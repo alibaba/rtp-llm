024116228971a3b15b0993e97f524b005e2536c6
rtp-llm <rtp-llm@alibaba>
2024-02-19 17:13:01 +0800

    refactor fast api server

--- maga_transformer/start_server.py
+++ maga_transformer/start_server.py
@@ -38,12 +38,111 @@ from maga_transformer.openai.api_datatype import ChatCompletionRequest, ChatComp
 from anyio.lowlevel import RunVar
 from anyio import CapacityLimiter
 
-# make buffer larger to avoid RemoteProtocolError Receive buffer too long
+# make buffer larger to avoid throw exception "RemoteProtocolError Receive buffer too long"
 MAX_INCOMPLETE_EVENT_SIZE = 1024 * 1024
 
 StreamObjectType = Union[Dict[str, Any], BaseModel]
 
-class FastApiServer(object):
+class InferenceApp(object):
+    def __init__(self, inference_server):
+        self.inference_server = inference_server
+
+    def start(self):
+        app = self.create_server()
+        self.inference_server.wait_all_worker_ready()
+
+        timeout_keep_alive = int(os.environ.get("TIMEOUT_KEEP_ALIVE", 5))
+        uvicorn.run(app, host="0.0.0.0", port=g_worker_info.server_port, log_config=UVICORN_LOGGING_CONFIG,
+                    timeout_keep_alive = timeout_keep_alive, h11_max_incomplete_event_size=MAX_INCOMPLETE_EVENT_SIZE)
+
+    def create_server(self):
+        middleware = [
+            Middleware(
+                CORSMiddleware,
+                allow_origins=['*'],
+                allow_credentials=True,
+                allow_methods=['*'],
+                allow_headers=['*']
+            )
+        ]
+        app = FastAPI(middleware=middleware)
+
+        @app.on_event("startup")
+        async def startup():
+            RunVar("_default_thread_limiter").set(CapacityLimiter(self.inference_server._controller.max_concurrency * 2))
+
+        @app.get("/health")
+        @app.post("/health")
+        @app.get("/GraphService/cm2_status")
+        @app.post("/GraphService/cm2_status")
+        @app.get("/SearchService/cm2_status")
+        @app.post("/SearchService/cm2_status")
+        @app.get("/status")
+        @app.post("/status")
+        @app.post("/health_check")
+        async def health():
+            return "ok"
+
+        @app.get("/")
+        async def health():
+            return {"status": "home"}
+
+        @app.get("/worker_status")
+        def worker_status():
+            return {
+                "available_concurrency": self.inference_server._controller.get_available_concurrency(),
+                "alive": True,
+            }
+
+        # entry for worker RANK != 0
+        @app.post("/inference_internal")
+        async def inference_internal(req: Union[str,Dict[Any, Any]]):
+            if g_parallel_info.is_master:
+                return InferenceServer.format_exception(ExceptionType.UNSUPPORTED_OPERATION,
+                                    "gang cluster is None or role is master, should not access /inference_internal!")
+            return await self.inference_server._infer_wrap(req)
+
+        # entry for worker RANK == 0
+        @app.post("/")
+        async def inference(req: Union[str,Dict[Any, Any]]):
+            if not g_parallel_info.is_master:
+                return InferenceServer.format_exception(ExceptionType.UNSUPPORTED_OPERATION,
+                                    "gang worker should not access this / api directly!")
+            return await self.inference_server._infer_wrap(req)
+
+        # update for worker RANK != 0
+        @app.post("/update_internal")
+        def update_internal(version_info: VersionInfo):
+            if g_parallel_info.is_master:
+                return InferenceServer.format_exception(ExceptionType.UNSUPPORTED_OPERATION,
+                                    "gang cluster is None or role is master, should not access /update_internal!")
+            return self.inference_server._update_wrap(version_info)
+
+        # update for worker RANK == 0
+        @app.post("/update")
+        def update(version_info: VersionInfo):
+            if not g_parallel_info.is_master:
+                return InferenceServer.format_exception(ExceptionType.UNSUPPORTED_OPERATION,
+                                    "gang worker should not access /update api directly!")
+            return self.inference_server._update_wrap(version_info)
+
+        @app.get("/v1/models")
+        async def list_models():
+            assert (self._openai_endpoint != None)
+            return await self.inference_server._openai_endpoint.list_models()
+
+        # entry for worker RANK == 0
+        @app.post("/chat/completions")
+        @app.post("/v1/chat/completions")
+        async def chat_completion(request: ChatCompletionRequest, raw_request: Request):
+            if not g_parallel_info.is_master:
+                return InferenceServer.format_exception(ExceptionType.UNSUPPORTED_OPERATION,
+                                    "gang worker should not access this completions api directly!")
+            return await self.inference_server._chat_completion_wrap(request, raw_request)
+
+        return app
+
+class InferenceServer(object):
     def __init__(self):
         if 'LOAD_CKPT_NUM_PROCESS' not in os.environ:
             os.environ['LOAD_CKPT_NUM_PROCESS'] = '0'
@@ -59,30 +158,27 @@ class FastApiServer(object):
     def start(self):
         self._system_reporter.start()
         self._gang_server.start()
-        # for debug online
         if os.environ.get('DEBUG_START_FAKE_PROCESS', None) is not None:
-            logging.info("DEBUG_START_FAKE_PROCESS is not None, start fake server")
+            # for debug online
+            logging.info("DEBUG_START_FAKE_PROCESS is set, start fake server")
             self._inference_worker = None
         else:
             self._inference_worker = InferenceWorker()
             self._openai_endpoint = OpenaiEndopoint(self._inference_worker.model)
         self._init_controller()
-        app = self.create_server()
+        app = InferenceApp(self)
+        app.start()
+
+    def wait_all_worker_ready(self):
         # master需要等其他所有机器都ready以后才能起服务，挂vipserver
         if g_parallel_info.is_master and g_parallel_info.world_size > 1:
-            self._wait_other_inference_worker_ready()
-
-        timeout_keep_alive = int(os.environ.get("TIMEOUT_KEEP_ALIVE", 5))
-        uvicorn.run(app, host="0.0.0.0", port=g_worker_info.server_port, log_config=UVICORN_LOGGING_CONFIG, timeout_keep_alive = timeout_keep_alive, h11_max_incomplete_event_size=MAX_INCOMPLETE_EVENT_SIZE)
-
-    def _wait_other_inference_worker_ready(self):
-        while True:
-            try:
-                self._gang_server.wait_infernece_server_ready()
-                break
-            except Exception as e:
-                logging.warn("worker not all ready, error_msg: " + str(e))
-                time.sleep(5)
+            while True:
+                try:
+                    self._gang_server.wait_infernece_server_ready()
+                    break
+                except Exception as e:
+                    logging.warn("worker not all ready, error_msg: " + str(e))
+                    time.sleep(5)
 
     def _init_controller(self):
         concurrency_with_block = json.loads(os.environ.get('CONCURRENCY_WITH_BLOCK', "False").lower())
@@ -118,7 +214,7 @@ class FastApiServer(object):
             self._access_logger.log_exception_access(request, e, id)
             kmonitor.report(AccMetrics.ERROR_QPS_METRIC, 1)
             yield response_data_prefix + \
-                json.dumps(FastApiServer.handler_exceptions(e), ensure_ascii=False) + "\r\n\r\n"
+                json.dumps(InferenceServer.handler_exceptions(e), ensure_ascii=False) + "\r\n\r\n"
 
     @staticmethod
     def format_exception(errcode: int, message: str) -> Dict[str, Any]:
@@ -127,16 +223,16 @@ class FastApiServer(object):
     @staticmethod
     def handler_exceptions(e: Exception):
         if isinstance(e, FtRuntimeException):
-            return FastApiServer.format_exception(e.expcetion_type, e.message)
+            return InferenceServer.format_exception(e.expcetion_type, e.message)
         elif isinstance(e, ConcurrencyException):
-            return FastApiServer.format_exception(ExceptionType.CONCURRENCY_LIMIT_ERROR, str(e))
+            return InferenceServer.format_exception(ExceptionType.CONCURRENCY_LIMIT_ERROR, str(e))
         elif isinstance(e, LoraCountException) or isinstance(e, LoraPathException):
-            return FastApiServer.format_exception(ExceptionType.UPDATE_ERROR, str(e))
+            return InferenceServer.format_exception(ExceptionType.UPDATE_ERROR, str(e))
         elif isinstance(e, Exception):
             error_msg = f'ErrorMsg: {str(e)} \n Traceback: {traceback.format_exc()}'
-            return FastApiServer.format_exception(ExceptionType.UNKNOWN_ERROR, error_msg)
+            return InferenceServer.format_exception(ExceptionType.UNKNOWN_ERROR, error_msg)
         else:
-            return FastApiServer.format_exception(ExceptionType.UNKNOWN_ERROR, str(e))
+            return InferenceServer.format_exception(ExceptionType.UNKNOWN_ERROR, str(e))
 
     def _update_wrap(self, version_info: VersionInfo):
         id = self._atomic_count.increment()
@@ -170,6 +266,42 @@ class FastApiServer(object):
                 kmonitor.report(AccMetrics.ERROR_QPS_METRIC, 1)
             rep = JSONResponse(self.handler_exceptions(e), status_code=error_code)
         return rep
+
+    #TODO(xinfei.sxf) refactor this
+    async def _chat_completion_wrap(self, request: ChatCompletionRequest, raw_request: Request):
+        try:
+            self._controller.increment()
+        except ConcurrencyException as e:
+            kmonitor.report(AccMetrics.CONFLICT_QPS_METRIC)
+            return JSONResponse(self.handler_exceptions(e), status_code=409)
+
+        try:
+            assert (self._openai_endpoint != None)
+            id = self._atomic_count.increment()
+            kmonitor.report(AccMetrics.QPS_METRIC, 1)
+            self._access_logger.log_query_access(request.model_dump(), id)
+
+            if request.stream:
+                def generate_call():
+                    assert (self._openai_endpoint != None)
+                    response = self._openai_endpoint.chat_completion(request, raw_request)
+                    assert (isinstance(response, AsyncGenerator))
+                    return response
+                reported_response = self._call_generate_with_report(generate_call)
+                return StreamingResponse(
+                    self.stream_response(request.model_dump(), reported_response, id),
+                    media_type="text/event-stream"
+                )
+            else:
+                response = self._openai_endpoint.chat_completion(request, raw_request)
+                assert (isinstance(response, Coroutine))
+                return (await response).model_dump(exclude_none=True)
+        except Exception as e:
+            kmonitor.report(AccMetrics.ERROR_QPS_METRIC)
+            logging.error(f'chat_completion error: {e}, trace: {traceback.format_exc()}')
+            return JSONResponse(self.handler_exceptions(e), status_code=500)
+        finally:
+            self._controller.decrement()
 
     async def _call_generate_with_report(
             self, generate_call: Callable[[], AsyncGenerator[StreamObjectType, None]]
@@ -213,7 +345,7 @@ class FastApiServer(object):
             res = self._call_generate_with_report(generate_call)
         finally:
             self._controller.decrement()
-        
+
         if is_streaming:
             return StreamingResponse(self.stream_response(req, res, id), media_type="text/event-stream")
         last_element = None
@@ -221,118 +353,6 @@ class FastApiServer(object):
             last_element = x
         self._access_logger.log_success_access(req, last_element, id)
         return JSONResponse(content=last_element)
-
-    def create_server(self):
-        app = FastAPI()
-
-        app.add_middleware(
-            CORSMiddleware,
-            allow_origins=["*"],
-            allow_credentials=True,
-            allow_methods=["POST,GET,OPTIONS"],
-            allow_headers=["*"]
-        )
-
-        @app.on_event("startup")
-        async def startup():
-            RunVar("_default_thread_limiter").set(CapacityLimiter(self._controller.max_concurrency * 2))
-
-        @app.get("/health")
-        @app.post("/health")
-        @app.get("/GraphService/cm2_status")
-        @app.post("/GraphService/cm2_status")
-        @app.get("/SearchService/cm2_status")
-        @app.post("/SearchService/cm2_status")
-        @app.get("/status")
-        @app.post("/status")
-        @app.post("/health_check")
-        async def health():
-            return "ok"
-
-        @app.get("/")
-        async def health():
-            return {"status": "home"}
-
-        @app.get("/worker_status")
-        def worker_status():
-            return {
-                "available_concurrency": self._controller.get_available_concurrency(),
-                "alive": True,
-            }
-
-        # entry for worker RANK != 0
-        @app.post("/inference_internal")
-        async def inference_internal(req: Union[str,Dict[Any, Any]]):
-            if g_parallel_info.is_master:
-                return FastApiServer.format_exception(ExceptionType.UNSUPPORTED_OPERATION, "gang cluster is None or role is master, should not access /inference_internal!")
-            return await self._infer_wrap(req)
-
-        # entry for worker RANK == 0
-        @app.post("/")
-        async def inference(req: Union[str,Dict[Any, Any]]):
-            if not g_parallel_info.is_master:
-                return FastApiServer.format_exception(ExceptionType.UNSUPPORTED_OPERATION, "gang worker should not access this / api directly!")
-            return await self._infer_wrap(req)
-
-        # update for worker RANK != 0
-        @app.post("/update_internal")
-        def update_internal(version_info: VersionInfo):
-            if g_parallel_info.is_master:
-                return FastApiServer.format_exception(ExceptionType.UNSUPPORTED_OPERATION, "gang cluster is None or role is master, should not access /inference_internal!")
-            return self._update_wrap(version_info)
-
-        # update for worker RANK == 0
-        @app.post("/update")
-        def update(version_info: VersionInfo):
-            if not g_parallel_info.is_master:
-                return FastApiServer.format_exception(ExceptionType.UNSUPPORTED_OPERATION, "gang worker should not access this / api directly!")
-            return self._update_wrap(version_info)
-
-
-        @app.get("/v1/models")
-        async def list_models():
-            assert (self._openai_endpoint != None)
-            return await self._openai_endpoint.list_models()
-
-        @app.post("/chat/completions")
-        @app.post("/v1/chat/completions")
-        async def chat_completion(request: ChatCompletionRequest, raw_request: Request):
-            try:
-                self._controller.increment()
-            except ConcurrencyException as e:
-                kmonitor.report(AccMetrics.CONFLICT_QPS_METRIC)
-                return JSONResponse(self.handler_exceptions(e), status_code=409)
-
-            try:
-                assert (self._openai_endpoint != None)
-                id = self._atomic_count.increment()
-                kmonitor.report(AccMetrics.QPS_METRIC, 1)
-                self._access_logger.log_query_access(request.model_dump(), id)
-
-                if request.stream:
-                    # async def async_generate_call():
-                    def generate_call():
-                        assert (self._openai_endpoint != None)
-                        response = self._openai_endpoint.chat_completion(request, raw_request)
-                        assert (isinstance(response, AsyncGenerator))
-                        return response
-                    reported_response = self._call_generate_with_report(generate_call)
-                    return StreamingResponse(
-                        self.stream_response(request.model_dump(), reported_response, id),
-                        media_type="text/event-stream"
-                    )
-                else:
-                    response = self._openai_endpoint.chat_completion(request, raw_request)
-                    assert (isinstance(response, Coroutine))
-                    return (await response).model_dump(exclude_none=True)
-            except Exception as e:
-                kmonitor.report(AccMetrics.ERROR_QPS_METRIC)
-                logging.error(f'chat_completion error: {e}, trace: {traceback.format_exc()}')
-                return JSONResponse(self.handler_exceptions(e), status_code=500)
-            finally:
-                self._controller.decrement()
-
-        return app
 
 def local_rank_main():
     server = None
@@ -344,7 +364,7 @@ def local_rank_main():
         g_parallel_info.reload()
         g_worker_info.reload()
         logging.info(f'start local {g_worker_info}, {g_parallel_info}')
-        server = FastApiServer()
+        server = InferenceServer()
         server.start()
     except BaseException as e:
         logging.error(f'start server error: {e}, trace: {traceback.format_exc()}')
