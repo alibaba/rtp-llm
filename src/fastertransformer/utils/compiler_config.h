b9975f5ea2971a77ac42c7dac3681e65164d1875
rtp-llm <rtp-llm@alibaba>
2024-03-26 15:55:14 +0800

    feat - rm useless

--- src/fastertransformer/utils/compiler_config.h
+++ src/fastertransformer/utils/compiler_config.h
@@ -8,11 +8,4 @@ static constexpr bool enable_bf16 = true;
 static constexpr bool enable_bf16 = false;
 #endif
 
-
-#ifdef USE_WEIGHT_ONLY
-static constexpr bool use_weight_only = true;
-#else
-static constexpr bool use_weight_only = false;
-#endif
-
 }  // namespace CompileConfig
