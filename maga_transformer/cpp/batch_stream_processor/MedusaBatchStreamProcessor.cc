ce3df7df564c13bd8c853940310e0884fbd4bf37
rtp-llm <rtp-llm@alibaba>
2024-04-22 10:51:14 +0800

    fix - tmp remove medusa

--- maga_transformer/cpp/batch_stream_processor/MedusaBatchStreamProcessor.cc
+++
@@ -1,9 +0,0 @@
-#include "maga_transformer/cpp/batch_stream_processor/MedusaBatchStreamProcessor.h"
-
-namespace rtp_llm {
-
-absl::Status MedusaBatchStreamProcessor::createAttentionMask(GptModelInputs& input) const {
-    return absl::UnimplementedError("not impl yet!");
-}
-
-}  // namespace rtp_llm
