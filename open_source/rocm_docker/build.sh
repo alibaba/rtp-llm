7a5c1e57501e378aeceb13a61b567061f38ba68c
rtp-llm <rtp-llm@alibaba>
2024-06-11 19:41:26 +0800

    feat: add rocm dockerfile

---
+++ open_source/rocm_docker/build.sh
@@ -0,0 +1,33 @@
+#! /bin/bash
+set +x;
+
+echo $0
+echo $(readlink -f "$0")
+DIR=$(dirname $(readlink -f "$0"))
+echo $DIR
+
+# These variables are to replace by your own values
+BASE_IMAGE="alibaba-cloud-linux-3-registry.cn-hangzhou.cr.aliyuncs.com/alinux3/alinux3:x86-3.220822.1"
+IMAGE_NAME=${IMAGE_NAME:-"reg.docker.alibaba-inc.com/isearch/maga_transformer_rocm_open_source_base"}
+IMAGE_TAG=${IMAGE_TAG:-"0.0.1"}
+CONDA_URL=${CONDA_URL:-"https://repo.anaconda.com/miniconda/Miniconda3-py310_23.1.0-1-Linux-x86_64.sh"}
+# alternative:
+# BAZEL_URL=https://mirrors.huaweicloud.com/bazel/5.2.0/bazel-5.2.0-installer-linux-x86_64.sh
+BAZEL_URL=${BAZEL_URL:-"https://github.com/bazelbuild/bazel/releases/download/5.2.0/bazel-5.2.0-installer-linux-x86_64.sh"}
+PYPI_URL=${PYPI_URL:-"https://mirrors.aliyun.com/pypi/simple"}
+
+mkdir deps && cp ../deps/requirements*.txt $DIR/deps/
+
+docker build \
+    --build-arg BASE_OS_IMAGE=$BASE_IMAGE \
+    --build-arg CONDA_URL=$CONDA_URL \
+    --build-arg BAZEL_URL=$BAZEL_URL \
+    --build-arg PYPI_URL=$PYPI_URL \
+    --build-arg AMD_BKC_URL=$AMD_BKC_URL \
+    -f rocm.Dockerfile \
+    --network=host \
+    -t $IMAGE_NAME:$IMAGE_TAG \
+    $DIR && \
+docker tag $IMAGE_NAME:$IMAGE_TAG $IMAGE_NAME:latest
+
+rm -rf $DIR/deps
---
+++ open_source/rocm_docker/rocm.Dockerfile
@@ -0,0 +1,39 @@
+ARG BASE_OS_IMAGE
+FROM $BASE_OS_IMAGE
+
+MAINTAINER wangyin.yx
+
+RUN echo "%sdev ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers && \
+    groupadd sdev
+
+RUN dnf install -y \
+        unzip wget which findutils rsync tar \
+        gcc gcc-c++ libstdc++-static gdb coreutils \
+        binutils bash glibc-devel libdb glibc glibc-langpack-en bison lld \
+        emacs-nox git git-lfs && \
+    rpm --rebuilddb
+
+ARG CONDA_URL
+RUN wget $CONDA_URL -O /tmp/conda.sh && \
+    sh /tmp/conda.sh -b -p /opt/conda310/ && \
+    rm /tmp/conda.sh -f
+
+ARG BAZEL_URL
+RUN wget $BAZEL_URL -O /tmp/bazel.sh && sh /tmp/bazel.sh && rm /tmp/bazel.sh -f
+
+RUN git config --system core.hooksPath .githooks && \
+    git lfs install
+
+ARG AMD_BKC_URL
+RUN wget $AMD_BKC_URL -O /tmp/bkc.tar && \
+    mkdir -p /tmp/bkc && \
+    tar -xvf /tmp/bkc.tar --strip-components=1 -C /tmp/bkc && \
+    cd /tmp/bkc && \
+    sed 's/amdgpu-install/amdgpu-install --no-dkms/g' -i install.sh && \
+    sh install.sh && \
+    rm -rf /tmp/bkc /tmp/bkc.tar
+
+ARG PYPI_URL
+ADD deps /tmp/deps
+RUN /opt/conda310/bin/pip install -r /tmp/deps/requirements_rocm.txt -i $PYPI_URL && \
+    rm /tmp/deps
