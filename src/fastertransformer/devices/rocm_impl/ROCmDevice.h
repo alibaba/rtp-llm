b89911ff2461ff5b3723740fc8fedc69bd4bf622
Xinya Zhang <Xinya.Zhang@amd.com>
2024-06-28 20:03:37 +0000

    Implement API ROCmDevice::transpose

--- src/fastertransformer/devices/rocm_impl/ROCmDevice.h
+++ src/fastertransformer/devices/rocm_impl/ROCmDevice.h
@@ -12,6 +12,17 @@ public:
     ~ROCmDevice();
 
     DeviceProperties getDeviceProperties() override;
+    IAllocator* getAllocator() override { return allocator_.get(); }
+    IAllocator* getHostAllocator() override { return hostAllocator_.get(); }
+    void copy(const CopyParams& params) override;
+    TransposeOutput transpose(const TransposeParams& params) override;
+    void syncAndCheck() override;
+    BufferPtr gemm(const GemmParams& params) override;
+    SelectOutput select(const SelectParams& params) override;
+    BufferPtr embeddingLookup(const EmbeddingLookupParams& params) override;
+    LayernormOutput layernorm(const LayernormParams& params) override;
+    void activation(const ActivationParams& params) override;
+    AttentionModuleOutput contextAttention(const AttentionModuleParams& params) override;
 
 public:
     BufferPtr        testVecAdd(const BufferPtr a, const BufferPtr b);
