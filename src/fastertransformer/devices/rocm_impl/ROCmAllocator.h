97e997b73934c8dc096491a690e019590916dcd5
Dragan Mladjenovic <Dragan.Mladjenovic@amd.com>
2024-07-01 09:28:50 +0000

    [ROCm] Implementent ROCmDevice memory allocator and copy operation

--- src/fastertransformer/devices/rocm_impl/ROCmAllocator.h
+++ src/fastertransformer/devices/rocm_impl/ROCmAllocator.h
@@ -1,32 +1,51 @@
 #pragma once
+
 #include "src/fastertransformer/core/allocator.h"
+#include "src/fastertransformer/devices/rocm_impl/ROCmUtils.h"
+#include <hip/hip_runtime.h>
 
 namespace fastertransformer {
 
-template<>
-class Allocator<AllocatorType::ROCM> : public TypedAllocator<AllocatorType::ROCM> {
+template<AllocatorType AType, MemoryType MType, hipError_t (*Alloc)(void**, size_t), hipError_t (*Free)(void*)>
+class AllocatorT: public TypedAllocator<AType> {
 public:
-    Allocator() {}
-    ~Allocator() {}
+    AllocatorT() {}
+    ~AllocatorT() {
+        FT_LOG_INFO("rocm allocator destroyed"); /* TODO(rocm): Free all memory? */
+    }
 
     MemoryType memoryType() const {
-        return MEMORY_CPU;
+        return MType;
     }
 
     void* malloc(size_t size) {
-        return nullptr;
+        void* ptr = nullptr;
+        HIP_CHECK(Alloc(&ptr, size));
+        return ptr;
     };
 
-    void  free(void** ptr) {
+    void free(void** ptr) {
+        HIP_CHECK(Free(*ptr));
         *ptr = nullptr;
     };
 
     // not expected to be called
     void* reMalloc(void* ptr, size_t size) {
+        FT_LOG_ERROR("rocm allocator doesn't support reMalloc");
+        fflush(stdout);
         abort();
     };
 };
 
+template<>
+class Allocator<AllocatorType::ROCM>: public AllocatorT<AllocatorType::ROCM, MEMORY_GPU, hipMalloc, hipFree> {};
 
-} // namespace fastertransformer
+static inline hipError_t hipHostMalloc(void** ptr, size_t size) {
+    return ::hipHostMalloc(ptr, size, 0);
+}
 
+template<>
+class Allocator<AllocatorType::ROCM_HOST>:
+    public AllocatorT<AllocatorType::ROCM_HOST, MEMORY_CPU_PINNED, hipHostMalloc, hipHostFree> {};
+
+}  // namespace fastertransformer
