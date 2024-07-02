aae931c3266a47dff80fae784ef960e5e69ff89a
nizhang <nizhang@amd.com>
2024-07-02 17:12:54 +0000

    select op

--- src/fastertransformer/devices/rocm_impl/test/RocmOpsTest.cc
+++ src/fastertransformer/devices/rocm_impl/test/RocmOpsTest.cc
@@ -71,3 +71,68 @@ TEST_F(RocmOpsTest, TestOp) {
 
     assertTensorClose(tensorC_rslt, tensorC_ref);
 }
+
+TEST_F(RocmOpsTest, testSelect) {
+    auto src = createBuffer<float>({6, 5}, {
+        0, 1, 2, 3, 4,
+        5, 6, 7, 8, 9,
+        10, 11, 12, 13, 14,
+        15, 16, 17, 18, 19,
+        20, 21, 22, 23, 24,
+        25, 26, 27, 28, 29
+    });
+    auto index = createBuffer<int32_t>({3}, {0, 2, 3});
+
+    auto result = device_->select({*src, *index});
+    auto expected = torch::tensor({
+        {0, 1, 2, 3, 4},
+        {10, 11, 12, 13, 14},
+        {15, 16, 17, 18, 19}
+    }, torch::kFloat32);
+    assertTensorClose(bufferToTensor(*result), expected, 1e-6, 1e-6);
+
+    auto src2 = device_->clone({*src, AllocationType::HOST});
+    auto index2 = device_->clone({*index, AllocationType::HOST});
+    auto result2 = device_->select({*src2, *index2});
+    assertTensorClose(bufferToTensor(*result2), expected, 1e-6, 1e-6);
+}
+
+TEST_F(RocmOpsTest, testSelect1d) {
+    auto src = createBuffer<float>({2, 6}, {
+        0, 1, 2, 3, 4, 5,
+        10, 11, 12, 13, 14, 15
+    });
+    auto index = createBuffer<int32_t>({3}, {0, 4, 5}, AllocationType::HOST);
+
+    auto result = device_->select({*src, *index, 1});
+    auto expected = torch::tensor({
+        {0, 4, 5},
+        {10, 14, 15}
+    }, torch::kFloat32);
+    assertTensorClose(bufferToTensor(*result), expected, 1e-6, 1e-6);
+
+    src = createBuffer<float>({2, 5, 3},{
+        0, 1, 2,
+        3, 4, 5,
+        6, 7, 8,
+        9, 10, 11,
+        12, 13, 14,
+        15, 16, 17,
+        18, 19, 20,
+        21, 22, 23,
+        24, 25, 26,
+        27, 28, 29
+    });
+    index = createBuffer<int32_t>({4}, {0, 1, 3, 4}, AllocationType::HOST);
+    result = device_->select({*src, *index, 1});
+    expected = torch::tensor({
+        {0, 1, 2},
+        {3, 4, 5},
+        {9, 10, 11},
+        {12, 13, 14},
+        {15, 16, 17},
+        {18, 19, 20},
+        {24, 25, 26},
+        {27, 28, 29}
+    }, torch::kFloat32);
+}
