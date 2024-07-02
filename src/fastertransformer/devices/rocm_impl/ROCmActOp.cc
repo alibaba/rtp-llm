1e6c9798ee7907b557f39bc3d944890697684353
mtgu0705 <mtgu@amd.com>
2024-07-02 10:52:01 +0000

    add unit test activation and FFN layer unit test(act + gemm func pass)

---
+++ src/fastertransformer/devices/rocm_impl/ROCmActOp.cc
@@ -0,0 +1,82 @@
+#include "src/fastertransformer/devices/rocm_impl/ROCmDevice.h"
+#include "src/fastertransformer/kernels/activation_kernels.h"
+#include "src/fastertransformer/utils/compiler_config.h"
+#include "src/fastertransformer/kernels/activation_kernels.h"
+#include "src/fastertransformer/cuda/Dispatch.h"
+
+
+using namespace std;
+
+namespace fastertransformer {
+
+
+#define ARGS_DISPATCH(Atype, Dtype, out, bias, gate, gate_bias, m, n, stream)                                          \
+    do {                                                                                                               \
+        invokeGenericActivation<Atype>((Dtype*)out,                                                                    \
+                                       (const Dtype*)bias,                                                             \
+                                       (const Dtype*)gate,                                                             \
+                                       (const Dtype*)gate_bias,                                                        \
+                                       (const int*)nullptr,                                                            \
+                                       (const Dtype*)nullptr,                                                          \
+                                       (int)m,                                                                         \
+                                       (int)n,                                                                         \
+                                       0,                                                                              \
+                                       (const float*)nullptr,                                                          \
+                                       (const float*)nullptr,                                                          \
+                                       (const Dtype*)nullptr,                                                          \
+                                       stream);                                                                        \
+    } while (0)
+
+#define ATYPE_DISPATCH(Dtype, Atype, KERNEL, ...)                                                                      \
+    do {                                                                                                               \
+        if (Atype == ActivationType::Silu) {                                                                           \
+            KERNEL(SiluActivation, Dtype, __VA_ARGS__);                                                                \
+        } else if (Atype == ActivationType::Gelu) {                                                                    \
+            KERNEL(GeluActivation, Dtype, __VA_ARGS__);                                                                \
+        } else if (Atype == ActivationType::Swiglu) {                                                                  \
+            KERNEL(SiluActivation, Dtype, __VA_ARGS__);                                                                \
+        } else {                                                                                                       \
+            throw OpException(OpErrorType::ERROR_UNIMPLEMENTED);                                                       \
+        }                                                                                                              \
+    } while (0)
+
+#define DTYPE_DISPATCH(Dtype, ...)                                                                                     \
+    do {                                                                                                               \
+        if (Dtype == DataType::TYPE_FP16) {                                                                            \
+            ATYPE_DISPATCH(half, __VA_ARGS__);                                                                         \
+        } else {                                                                                                       \
+            throw OpException(OpErrorType::ERROR_UNIMPLEMENTED);                                                       \
+        }                                                                                                              \
+    } while (0)
+
+#define DISPATCH(Dtype, Atype, ...)                                                                                    \
+    do {                                                                                                               \
+        DTYPE_DISPATCH(Dtype, Atype, ARGS_DISPATCH, __VA_ARGS__);                                                      \
+    } while (0)
+
+void ROCmDevice::activation(const ActivationParams& params) {
+    const auto& states = params.states;
+    size_t      m      = states.shape()[0];
+    size_t      n      = states.shape()[1];
+
+    void* bias      = nullptr;
+    void* gate      = nullptr;
+    void* gate_bias = nullptr;
+
+    if (params.bias) {
+        bias = params.bias.value().get().data();
+    }
+
+    if (params.gate) {
+        gate = params.gate.value().get().data();
+    }
+
+    if (params.gate_bias) {
+        gate_bias = params.gate_bias.value().get().data();
+    }
+
+    DISPATCH(states.type(), params.atype, states.data(), bias, gate, gate_bias, m, n, stream_);
+}
+
+
+} // namespace fastertransformer
---
+++ src/fastertransformer/devices/rocm_impl/test/ops/RocmFFnOpTest.cc
@@ -0,0 +1,29 @@
+#include "src/fastertransformer/devices/rocm_impl/RocmTestUtils.h"
+#include "src/fastertransformer/devices/base_tests/FfnLayerTest.hpp"
+#include "src/fastertransformer/devices/rocm_impl/ROCmDevice.h"
+#include "src/fastertransformer/cuda/Dispatch.h"
+
+
+class RocmFfnLayerTest: public FfnLayerTest {};
+
+TEST_F(RocmFfnLayerTest, Gate_Fp16_FfnOpTest) {
+    FfnOpTest(4, 16, 16, ActivationType::Swiglu, DataType::TYPE_FP16);
+    FfnOpTest(4, 32, 32, ActivationType::Swiglu, DataType::TYPE_FP16);
+    FfnOpTest(4, 2048, 128, ActivationType::Swiglu, DataType::TYPE_FP16);
+    FfnOpTest(4, 2048, 4096, ActivationType::Swiglu, DataType::TYPE_FP16);
+    FfnOpTest(128, 2048, 128, ActivationType::Swiglu, DataType::TYPE_FP16);
+    FfnOpTest(1000, 2048, 128, ActivationType::Swiglu, DataType::TYPE_FP16);
+    FfnOpTest(1, 2, 4096, ActivationType::Swiglu, DataType::TYPE_FP16);
+    FfnOpTest(1000, 2048, 128, ActivationType::Swiglu, DataType::TYPE_FP16);
+}
+
+TEST_F(RocmFfnLayerTest, NoGate_Fp16_FfnOpTest) {
+    FfnOpTest(4, 16, 16, ActivationType::Swiglu, DataType::TYPE_FP16);
+    FfnOpTest(4, 32, 32, ActivationType::Swiglu, DataType::TYPE_FP16);
+    FfnOpTest(4, 2048, 128, ActivationType::Silu, DataType::TYPE_FP16);
+    FfnOpTest(4, 2048, 4096, ActivationType::Silu, DataType::TYPE_FP16);
+    FfnOpTest(128, 2048, 128, ActivationType::Silu, DataType::TYPE_FP16);
+    FfnOpTest(1000, 2048, 128, ActivationType::Silu, DataType::TYPE_FP16);
+    FfnOpTest(1, 2, 4096, ActivationType::Silu, DataType::TYPE_FP16);
+    FfnOpTest(1000, 2048, 128, ActivationType::Silu, DataType::TYPE_FP16);
+}
