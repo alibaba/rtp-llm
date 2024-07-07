213a967020799e009fe692231be264dead13326e
rtp-llm <rtp-llm@alibaba>
2024-07-07 23:56:47 +0800

    feat：delete useless module in qwen agent

--- maga_transformer/openai/renderers/qwen_agent/__init__.py
+++ maga_transformer/openai/renderers/qwen_agent/__init__.py
@@ -1,8 +1,4 @@
 __version__ = '0.0.6'
-from .agent import Agent
-from .multi_agent_hub import MultiAgentHub
 
 __all__ = [
-    'Agent',
-    'MultiAgentHub',
 ]
--- maga_transformer/openai/renderers/qwen_agent/gui/__init__.py
+++
@@ -1,8 +0,0 @@
-from qwen_agent.gui.gradio import gr, mgr
-from qwen_agent.gui.web_ui import WebUI
-
-__all__ = [
-    'gr',
-    'mgr',
-    'WebUI',
-]
