1eced3a02d635e9c05b32ba7ef755d71ad7ca014
rtp-llm <rtp-llm@alibaba>
2024-07-08 17:05:08 +0800

    fix, nccl error exit->abort

--- src/fastertransformer/cuda/nccl/nccl_utils.h
+++ src/fastertransformer/cuda/nccl/nccl_utils.h
@@ -35,7 +35,7 @@ namespace fastertransformer {
         ncclResult_t r = cmd;                                                                                          \
         if (r != ncclSuccess) {                                                                                        \
             printf("Failed, NCCL error %s:%d '%s'\n", __FILE__, __LINE__, ncclGetErrorString(r));                      \
-            exit(EXIT_FAILURE);                                                                                        \
+            abort();                                                                                                   \
         }                                                                                                              \
     } while (0)
 
