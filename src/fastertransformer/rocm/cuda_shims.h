fa4d8bc62b97ef9eeb005c2485f9d98a9abdfc4b
feifei14119 <feiw@amd.com>
2024-07-08 20:25:32 +0000

    [temp]fix rebase to upstream

--- src/fastertransformer/rocm/cuda_shims.h
+++ src/fastertransformer/rocm/cuda_shims.h
@@ -43,3 +43,265 @@ __host__ __device__ inline float special_cast<float, __hip_bfloat16>(__hip_bfloa
 #define cudaMemcpy hipMemcpy
 #define sync_check_cuda_error sync_check_hip_error
 #define check_cuda_error check_hip_error
+
+#include "amd_bfloat16.h"
+#define __nv_bfloat16 __hip_bfloat16
+#define __nv_bfloat162 __hip_bfloat162
+inline int div_up(int a, int n) {
+    return (a + n - 1) / n;
+}
+template<typename T>
+__device__ inline T __shfl_xor_sync(unsigned mask, T var, int laneMask, int width = 32) {
+    (void)mask;
+    return __shfl_xor(var, laneMask, width);
+}
+
+static inline __device__ __host__ __nv_bfloat162 __ldg(const __nv_bfloat162* ptr) {
+    return *ptr;
+}
+static inline __device__ __host__ __nv_bfloat16 __ldg(const __nv_bfloat16* ptr) {
+    return *ptr;
+}
+
+
+typedef struct __attribute__((aligned(4))) {
+    half x, y, z, w;
+} half4;
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+struct __attribute__((aligned(16))) Float4_ {
+    float2 x;
+    float2 y;
+};
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+struct __attribute__((aligned(32))) Float8_ {
+    float2 x;
+    float2 y;
+    float2 z;
+    float2 w;
+};
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifdef ENABLE_BF16
+struct __attribute__((aligned(8))) bf16_4_t {
+    __nv_bfloat162 x;
+    __nv_bfloat162 y;
+};
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+struct __attribute__((aligned(16))) bf16_8_t {
+    __nv_bfloat162 x;
+    __nv_bfloat162 y;
+    __nv_bfloat162 z;
+    __nv_bfloat162 w;
+};
+#endif
+
+#ifdef ENABLE_FP8
+using fp8_2_t = __nv_fp8x2_e4m3;
+using fp8_4_t = __nv_fp8x4_e4m3;
+
+struct __align__(8) fp8_8_t {
+    __nv_fp8x2_e4m3 x;
+    __nv_fp8x2_e4m3 y;
+    __nv_fp8x2_e4m3 z;
+    __nv_fp8x2_e4m3 w;
+};
+#endif
+
+// clang-format off
+template<typename T> struct packed_type_2;
+template <>          struct packed_type_2<float>         { using type = float; }; // we don't need to pack float by default
+template <>          struct packed_type_2<half>          { using type = half2; };
+
+#ifdef ENABLE_BF16
+template<>
+struct packed_type_2<__nv_bfloat16> {
+    using type = __nv_bfloat162;
+};
+#endif
+
+template <typename T, int N>
+struct packed_type;
+
+template <typename T>
+struct packed_type<T, 1>
+{
+    using type = T;
+};
+
+template <>
+struct packed_type<int8_t, 1>
+{
+    using type = int8_t;
+};
+
+template <>
+struct packed_type<int8_t, 2>
+{
+    using type = int16_t;
+};
+
+template <>
+struct packed_type<int8_t, 4>
+{
+    using type = int32_t;
+};
+
+template <>
+struct packed_type<int8_t, 8>
+{
+    using type = int64_t;
+};
+
+#ifdef ENABLE_FP8
+
+template <>
+struct packed_type<__nv_fp8_e4m3, 1>
+{
+    using type = __nv_fp8_e4m3;
+};
+
+template <>
+struct packed_type<__nv_fp8_e4m3, 2>
+{
+    using type = fp8_2_t;
+};
+
+template <>
+struct packed_type<__nv_fp8_e4m3, 4>
+{
+    using type = fp8_4_t;
+};
+
+template <>
+struct packed_type<__nv_fp8_e4m3, 8>
+{
+    using type = fp8_8_t;
+};
+#endif // ENABLE_FP8
+
+template <>
+struct packed_type<uint16_t, 2>
+{
+    using type = uint32_t;
+};
+
+template <>
+struct packed_type<uint16_t, 4>
+{
+    using type = uint2;
+};
+
+template <>
+struct packed_type<uint16_t, 8>
+{
+    using type = uint4;
+};
+
+template <>
+struct packed_type<half, 2>
+{
+    using type = uint32_t;
+};
+
+template <>
+struct packed_type<half, 4>
+{
+    using type = uint2;
+};
+
+template <>
+struct packed_type<half, 8>
+{
+    using type = uint4;
+};
+
+#ifdef ENABLE_BF16
+template <>
+struct packed_type<__nv_bfloat16, 2>
+{
+    using type = __nv_bfloat162;
+};
+
+template <>
+struct packed_type<__nv_bfloat16, 4>
+{
+    using type = bf16_4_t;
+};
+
+template <>
+struct packed_type<__nv_bfloat16, 8>
+{
+    using type = bf16_8_t;
+};
+#endif
+template <>
+struct packed_type<float, 2>
+{
+    using type = float2;
+};
+
+template <>
+struct packed_type<float, 4>
+{
+    using type = float4;
+};
+
+template <>
+struct packed_type<float, 8>
+{
+    using type = Float8_;
+};
+
+
+template<typename T> struct num_elems;
+template <>          struct num_elems<float>           { static constexpr int value = 1; };
+template <>          struct num_elems<float2>          { static constexpr int value = 2; };
+template <>          struct num_elems<float4>          { static constexpr int value = 4; };
+template <>          struct num_elems<Float4_>          { static constexpr int value = 4; };
+template <>          struct num_elems<Float8_>          { static constexpr int value = 8; };
+
+template <>          struct num_elems<half>            { static constexpr int value = 1; };
+template <>          struct num_elems<half2>           { static constexpr int value = 2; };
+template <>          struct num_elems<uint32_t>           { static constexpr int value = 2; };
+template <>          struct num_elems<int32_t>           { static constexpr int value = 2; };
+template <>          struct num_elems<int64_t>           { static constexpr int value = 4; };
+template <>          struct num_elems<uint2>           { static constexpr int value = 4; };
+template <>          struct num_elems<uint4>           { static constexpr int value = 8; };
+
+#ifdef ENABLE_BF16
+template <>          struct num_elems<__nv_bfloat16>   { static constexpr int value = 1; };
+template <>          struct num_elems<__nv_bfloat162>  { static constexpr int value = 2; };
+template <>          struct num_elems<bf16_4_t>  { static constexpr int value = 4; };
+template <>          struct num_elems<bf16_8_t>  { static constexpr int value = 8; };
+#endif
+
+#ifdef ENABLE_FP8
+template <>          struct num_elems<__nv_fp8_e4m3>   { static constexpr int value = 1; };
+template <>          struct num_elems<fp8_2_t>   { static constexpr int value = 2; };
+template <>          struct num_elems<fp8_4_t>   { static constexpr int value = 4; };
+template <>          struct num_elems<fp8_8_t>   { static constexpr int value = 8; };
+#endif
+
+template<typename T, int num> struct packed_as;
+template<typename T>          struct packed_as<T, 1>              { using type = T; };
+template<>                    struct packed_as<half,  2>          { using type = half2; };
+template<>                    struct packed_as<float,  2>         { using type = float2; };
+template<>                    struct packed_as<int8_t, 2>         { using type = int16_t; };
+template<>                    struct packed_as<int32_t, 2>        { using type = int2; };
+template<>                    struct packed_as<half2, 1>          { using type = half; };
+template<>                    struct packed_as<float2, 1>         { using type = float; };
+#ifdef ENABLE_BF16
+template<> struct packed_as<__nv_bfloat16,  2> { using type = __nv_bfloat162; };
+template<> struct packed_as<__nv_bfloat162, 1> { using type = __nv_bfloat16;  };
+#endif
+
+inline __device__ float2 operator*(float2 a, float2 b) { return make_float2(a.x * b.x, a.y * b.y); }
+inline __device__ float2 operator*(float2 a, float  b) { return make_float2(a.x * b, a.y * b); }
+// clang-format on
