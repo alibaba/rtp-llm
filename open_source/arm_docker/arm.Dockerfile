c11565418d80dedf9de48ea233b97870fd64bda9
rtp-llm <rtp-llm@alibaba>
2024-07-18 14:55:59 +0800

    build: add arm docker

---
+++ open_source/arm_docker/arm.Dockerfile
@@ -0,0 +1,30 @@
+ARG BASE_OS_IMAGE
+FROM $BASE_OS_IMAGE
+
+MAINTAINER wangyin.yx
+
+RUN echo "%sdev ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers && \
+    groupadd sdev
+
+RUN dnf install -y \
+        unzip wget which findutils rsync tar \
+        gcc gcc-c++ libstdc++-static gdb coreutils \
+        binutils bash glibc-devel libdb glibc glibc-langpack-en bison lld \
+        emacs-nox git git-lfs
+
+
+ARG CONDA_URL
+RUN wget $CONDA_URL -O /tmp/conda.sh && \
+    sh /tmp/conda.sh -b -p /opt/conda310/ && \
+    rm /tmp/conda.sh -f
+
+ARG BAZEL_URL
+RUN wget $BAZEL_URL -O /usr/local/bin/bazel && chmod +x /usr/local/bin/bazel
+
+RUN git config --system core.hooksPath .githooks && \
+    git lfs install
+
+ARG PYPI_URL
+ADD deps /tmp/deps
+RUN /opt/conda310/bin/pip install -r /tmp/deps/requirements_base.txt -i $PYPI_URL && \
+    rm -rf /tmp/deps && /opt/conda310/bin/pip cache purge
---
+++ open_source/arm_docker/build.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+set +x;
+
+echo $0
+echo $(readlink -f "$0")
+DIR=$(dirname $(readlink -f "$0"))
+echo $DIR
+
+# These variables are to replace by your own values
+BASE_IMAGE="alibaba-cloud-linux-3-registry.cn-hangzhou.cr.aliyuncs.com/alinux3/alinux3:220901.1"
+IMAGE_NAME=${IMAGE_NAME:-"reg.docker.alibaba-inc.com/isearch/maga_transformer_arm_open_source_base"}
+IMAGE_TAG=${IMAGE_TAG:-"0.0.1"}
+CONDA_URL=${CONDA_URL:-"https://repo.anaconda.com/miniconda/Miniconda3-py310_23.1.0-1-Linux-aarch64.sh"}
+# alternative:
+BAZEL_URL=https://mirrors.huaweicloud.com/bazel/5.2.0/bazel-5.2.0-linux-arm64
+# BAZEL_URL=${BAZEL_URL:-"https://github.com/bazelbuild/bazel/releases/download/5.2.0/bazel-5.2.0-linux-arm64"}
+PYPI_URL=${PYPI_URL:-"https://mirrors.aliyun.com/pypi/simple"}
+
+mkdir deps && cp ../deps/requirements*.txt $DIR/deps/
+
+docker build \
+    --build-arg BASE_OS_IMAGE=$BASE_IMAGE \
+    --build-arg CONDA_URL=$CONDA_URL \
+    --build-arg BAZEL_URL=$BAZEL_URL \
+    --build-arg PYPI_URL=$PYPI_URL \
+    -f arm.Dockerfile \
+    --network=host \
+    -t $IMAGE_NAME:$IMAGE_TAG \
+    $DIR && \
+docker tag $IMAGE_NAME:$IMAGE_TAG $IMAGE_NAME:latest
+
+rm -rf $DIR/deps
