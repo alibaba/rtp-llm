9aa5aef9ba0feb0d4610b55131f14260769f3182
rtp-llm <rtp-llm@alibaba>
2024-07-31 17:34:56 +0800

    feat: add lora interface in cpp engine.

--- maga_transformer/cpp/engine_base/EngineBase.cc
+++ maga_transformer/cpp/engine_base/EngineBase.cc
@@ -9,6 +9,7 @@ namespace rtp_llm {
 EngineBase::EngineBase(const EngineInitParams& params) {
     EngineBase::initDevices(params);
     device_ = ft::DeviceFactory::getDefaultDevice();
+    lora_manager_ = std::make_shared<lora::LoraManager>();
 }
 
 void EngineBase::initDevices(const EngineInitParams& params) {
@@ -29,6 +30,26 @@ void EngineBase::initDevices(const EngineInitParams& params) {
     default_device_params.host_reserve_memory_bytes =
         EnvUtil::getEnv("HOST_RESERVE_MEMORY_BYTES", default_device_params.host_reserve_memory_bytes);
     ft::DeviceFactory::initDevices(global_params);
+}
+
+void EngineBase::addLora(int64_t lora_id,
+                         ft::lora::loraLayerWeightsMap lora_a,
+                         ft::lora::loraLayerWeightsMap lora_b)
+{
+    lora_manager_->addLora(lora_id, lora_a, lora_b);
+
+}
+
+void EngineBase::removeLora(int64_t lora_id) {
+    lora_manager_->removeLora(lora_id);
+}
+
+void EngineBase::releaseLoraResource() {
+    lora_manager_->releaseSignal();
+}
+
+std::shared_ptr<lora::LoraManager> EngineBase::getLoraManager() {
+    return lora_manager_;
 }
 
 }  // namespace rtp_llm
--- maga_transformer/cpp/engine_base/EngineBase.h
+++ maga_transformer/cpp/engine_base/EngineBase.h
@@ -4,6 +4,7 @@
 #include "maga_transformer/cpp/dataclass/GenerateStream.h"
 #include "maga_transformer/cpp/dataclass/EngineInitParameter.h"
 #include "src/fastertransformer/devices/DeviceBase.h"
+#include "maga_transformer/cpp/lora/LoraManager.h"
 
 namespace ft = fastertransformer;
 
@@ -18,7 +19,17 @@ public:
     ft::DeviceBase* getDevice() {
         return device_;
     }
+
+    void addLora(int64_t lora_id, ft::lora::loraLayerWeightsMap lora_a, ft::lora::loraLayerWeightsMap lora_b);
+
+    void removeLora(int64_t lora_id);
+
+    void releaseLoraResource();
+
+    std::shared_ptr<lora::LoraManager> getLoraManager();
+
     virtual std::shared_ptr<GenerateStream> enqueue(const std::shared_ptr<GenerateInput>& input) = 0;
+
     virtual absl::Status stop() = 0;
 
     virtual KVCacheInfo getKVCacheInfo() const {
@@ -26,6 +37,7 @@ public:
     }
 protected:
     ft::DeviceBase* device_;
+    std::shared_ptr<lora::LoraManager> lora_manager_;
 };
 
 }  // namespace rtp_llm
