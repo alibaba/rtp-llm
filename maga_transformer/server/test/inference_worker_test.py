d8528c4d57be06ff39ea8d9506b007ebe5b8a48a
rtp-llm <rtp-llm@alibaba>
2024-08-21 11:06:14 +0800

    update - update inference_worker_test

--- maga_transformer/server/test/inference_worker_test.py
+++ maga_transformer/server/test/inference_worker_test.py
@@ -1,13 +1,10 @@
 import os
-import torch
 import asyncio
 import logging
-import threading
-import time
-from concurrent.futures import ThreadPoolExecutor
 from unittest import TestCase, main
 from unittest import mock
 from maga_transformer.utils.weight_type import WEIGHT_TYPE
+from maga_transformer.utils.free_port_util import FreePortUtil
 from maga_transformer.config.log_config import LOGGING_CONFIG
 from maga_transformer.async_decoder_engine.async_model import AsyncModel
 from maga_transformer.server.inference_worker import InferenceWorker, BatchPipelineResponse
@@ -16,23 +13,24 @@ from maga_transformer.test.model_test.test_util.fake_model_loader import FakeMod
 from maga_transformer.structure.request_extractor import request_id_field_name
 from maga_transformer.distribute.worker_info import DEFAULT_START_PORT, update_master_info
 
-os.environ['KV_CACHE_MEM_MB'] = '100'
-
 class FakeInferenceWorker(InferenceWorker):
     def __init__(self, model, pipeline):
         self.model = model
         self.pipeline = pipeline
 
-@mock.patch.dict('os.environ', {'RESERVER_RUNTIME_MEM_MB': '1', 'DEVICE_RESERVE_MEMORY_BYTES': str(64 * 1024 * 1024)})
 class InferenceWorkerTest(TestCase):
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
-
+        os.environ['KV_CACHE_MEM_MB'] = '100'
+        os.environ['RESERVER_RUNTIME_MEM_MB'] = '1'
+        os.environ['DEVICE_RESERVE_MEMORY_BYTES'] = str(64 * 1024 * 1024)
         self.tokenizer_path = os.path.join(os.getcwd(), "maga_transformer/test/model_test/fake_test/testdata/llama/fake/hf_source")
         self.ckpt_path = os.path.join(os.getcwd(), "maga_transformer/test/model_test/fake_test/testdata/llama/fake/hf_source")
+        self.inference_worker = self.create_inference_worker()
 
     def create_inference_worker(self):
-        update_master_info('0.0.0.0', int(os.environ.get('START_PORT', DEFAULT_START_PORT)))
+        port_list = FreePortUtil.get_consecutive_free_ports(3)
+        update_master_info('0.0.0.0', int(os.environ.get('START_PORT', port_list[0])))
         self.fake_model_loader = FakeModelLoader(model_type='llama',
                                                  tokenizer_path=self.tokenizer_path,
                                                  ckpt_path=self.ckpt_path,
@@ -69,194 +67,122 @@ class InferenceWorkerTest(TestCase):
         return result, aux_info, finished
 
     def test_simple(self):
-        inference_worker = self.create_inference_worker()
-        try:
-            thread_count = 10
-            t = ThreadPoolExecutor(thread_count)
-            def func():
-                return asyncio.run(self._run(inference_worker, prompt="please write a story about dog", generate_config={"top_k":1, "max_new_tokens":3, "top_p": 1}))
-            result = []
-            for i in range(0, thread_count):
-                result.append(t.submit(func))
-            # just ensure every input has result
-            for i in range(0, thread_count):
-                result_text, aux_info, finished = result[i].result()
-                logging.info(f"{i} th : {result_text}, aux_info: {aux_info}, finished:{finished}")
-                self.assertTrue(len(result_text) > 0)
-        finally:
-            inference_worker.stop()
+        def func():
+            return asyncio.run(self._run(self.inference_worker, prompt="please write a story about dog", generate_config={"top_k":1, "max_new_tokens":3, "top_p": 1}))
+        # just ensure every input has result
+        result_text, aux_info, finished = func()
+        logging.info(f"result_text: {result_text}, aux_info: {aux_info}, finished:{finished}")
+        self.assertTrue(len(result_text) > 0)
 
     def test_text_input(self):
-        inference_worker = self.create_inference_worker()
-        try:
-            result_text, aux_info, finished = asyncio.run(self._run(inference_worker, text="please write a story about dog", generate_config={"top_k":1, "max_new_tokens":3, "top_p": 1}))
-            # logging.info(f"test text input : {result_text}, aux_info: {aux_info}, finished:{finished}")
-            self.assertTrue(len(result_text) > 0)
-        finally:
-            inference_worker.stop()
+        result_text, aux_info, finished = asyncio.run(self._run(self.inference_worker, text="please write a story about dog", generate_config={"top_k":1, "max_new_tokens":3, "top_p": 1}))
+        # logging.info(f"test text input : {result_text}, aux_info: {aux_info}, finished:{finished}")
+        self.assertTrue(len(result_text) > 0)
 
     def test_num_batch(self):
-        inference_worker = self.create_inference_worker()
-        try:
-            thread_count = 10
-            t = ThreadPoolExecutor(thread_count)
-            def func():
-                return asyncio.run(self._run(inference_worker,
-                                             prompt_batch=["please write a story about dog", "please write a story about dog"],
-                                             yield_generator=True,
-                                             generate_config={"top_k":1, "max_new_tokens":3, "top_p": 1, "return_incremental": True, "num_return_sequences":3}))
-            result = []
-            for i in range(0, thread_count):
-                result.append(t.submit(func))
-            # just ensure every input has result
-            for i in range(0, thread_count):
-                result_text, aux_info, finished = result[i].result()
-                logging.info(f"batch * num {i} th : {result_text}, aux_info: {aux_info}, finished:{finished}")
-                self.assertEqual(2, len(result_text))
-                self.assertEqual(3, len(result_text[0]))
-                self.assertEqual(3, len(result_text[1]))
-                self.assertEqual(2, len(aux_info))
-                self.assertEqual(3, len(aux_info[0]))
-                self.assertEqual(3, len(aux_info[1]))
-                self.assertEqual([True, True], finished)        
-                self.assertTrue(len(result_text) > 0)
-        finally:
-            inference_worker.stop()
+        def func():
+            return asyncio.run(self._run(self.inference_worker,
+                                         prompt_batch=["please write a story about dog",
+                                                       "please write a story about dog"],
+                                         yield_generator=True,
+                                         generate_config={"top_k": 1, "max_new_tokens": 3, "top_p": 1, "return_incremental": True, "num_return_sequences": 3}))
+        result_text, aux_info, finished = func()
+        logging.info(f"batch * num: {result_text}, aux_info: {aux_info}, finished:{finished}")
+        self.assertEqual(2, len(result_text))
+        self.assertEqual(3, len(result_text[0]))
+        self.assertEqual(3, len(result_text[1]))
+        self.assertEqual(2, len(aux_info))
+        self.assertEqual(3, len(aux_info[0]))
+        self.assertEqual(3, len(aux_info[1]))
+        self.assertEqual([True, True], finished)
+        self.assertTrue(len(result_text) > 0)
 
     def test_num_return_sequences_1(self):
-        inference_worker = self.create_inference_worker()
-        try:
-            result_text, aux_info, finished = asyncio.run(self._run(inference_worker,
-                                                                    prompt="please write a story about dog",
-                                                                    yield_generator=True,
-                                                                    generate_config={"top_k":1, "max_new_tokens":3, "top_p": 1, "return_incremental": True, "num_return_sequences": 1}))
-            # logging.info(f"no batch * num_return_sequences 1 : {result_text}, aux_info: {aux_info}, finished:{finished}")
-            self.assertEqual(1, len(result_text))
-            self.assertEqual(1, len(aux_info))
-            self.assertEqual(True, finished)
-        finally:
-            inference_worker.stop()
+        result_text, aux_info, finished = asyncio.run(self._run(self.inference_worker,
+                                                                prompt="please write a story about dog",
+                                                                yield_generator=True,
+                                                                generate_config={"top_k": 1, "max_new_tokens": 3, "top_p": 1, "return_incremental": True, "num_return_sequences": 1}))
+        # logging.info(f"no batch * num_return_sequences 1 : {result_text}, aux_info: {aux_info}, finished:{finished}")
+        self.assertEqual(1, len(result_text))
+        self.assertEqual(1, len(aux_info))
+        self.assertEqual(True, finished)
 
     def test_batch_num_return_sequences_1(self):
-        inference_worker = self.create_inference_worker()
-        try:
-            result_text, aux_info, finished = asyncio.run(self._run(inference_worker,
-                                                                    prompt_batch=["please write a story about dog", "please write a story about dog"],
-                                                                    yield_generator=True,
-                                                                    generate_config={"top_k":1, "max_new_tokens":3, "top_p": 1, "return_incremental": True, "num_return_sequences": 1}))
-            # logging.info(f"batch 2 * num_return_sequences 1: {result_text}, aux_info: {aux_info}, finished:{finished}")
-            self.assertEqual(2, len(result_text))
-            self.assertEqual(1, len(result_text[0]))
-            self.assertEqual(1, len(result_text[1]))
-            self.assertEqual(2, len(aux_info))
-            self.assertEqual(1, len(aux_info[0]))
-            self.assertEqual(1, len(aux_info[1]))
-            self.assertEqual([True, True], finished)
-        finally:
-            inference_worker.stop()
+        result_text, aux_info, finished = asyncio.run(self._run(self.inference_worker,
+                                                                prompt_batch=["please write a story about dog",
+                                                                              "please write a story about dog"],
+                                                                yield_generator=True,
+                                                                generate_config={"top_k": 1, "max_new_tokens": 3, "top_p": 1, "return_incremental": True, "num_return_sequences": 1}))
+        # logging.info(f"batch 2 * num_return_sequences 1: {result_text}, aux_info: {aux_info}, finished:{finished}")
+        self.assertEqual(2, len(result_text))
+        self.assertEqual(1, len(result_text[0]))
+        self.assertEqual(1, len(result_text[1]))
+        self.assertEqual(2, len(aux_info))
+        self.assertEqual(1, len(aux_info[0]))
+        self.assertEqual(1, len(aux_info[1]))
+        self.assertEqual([True, True], finished)
 
     def test_incremental(self):
-        inference_worker = self.create_inference_worker()
-        try:
-            thread_count = 10
-            t = ThreadPoolExecutor(thread_count)
-            def func():
-                return asyncio.run(self._run(inference_worker,
-                                                            prompt="please write a story about dog",
-                                                            yield_generator=True,
-                                                            generate_config={"top_k":1, "max_new_tokens":3, "top_p": 1, "return_incremental": True}))
-            result = []
-            for i in range(0, thread_count):
-                result.append(t.submit(func))
-            # just ensure every input has result
-            for i in range(0, thread_count):
-                result_text, aux_info, finished = result[i].result()
-                self.assertEqual(3, aux_info.get("iter_count"))
-                self.assertEqual(True, finished)
-                logging.info(f"incremental {i} th : {result_text}, aux_info: {aux_info}, finished:{finished}")    
-                self.assertTrue(len(result_text) > 0)
-        finally:
-            inference_worker.stop()
+        def func():
+            return asyncio.run(self._run(self.inference_worker,
+                                         prompt="please write a story about dog",
+                                         yield_generator=True,
+                                         generate_config={"top_k": 1, "max_new_tokens": 3, "top_p": 1, "return_incremental": True}))
+        result = []
+        result_text, aux_info, finished = func()
+        self.assertEqual(3, aux_info.get("iter_count"))
+        self.assertEqual(True, finished)
+        logging.info(f"incremental : {result_text}, aux_info: {aux_info}, finished:{finished}")
+        self.assertTrue(len(result_text) > 0)
 
     def test_batch_incremental(self):
-        inference_worker = self.create_inference_worker()
-        try:
-            thread_count = 10
-            t = ThreadPoolExecutor(thread_count)
-            def func():
-                return asyncio.run(self._run(inference_worker,
-                                                            prompt_batch=["please write a story about dog", "please write a story about dog"],
-                                                            yield_generator=True,
-                                                            generate_config={"top_k":1, "max_new_tokens":3, "top_p": 1, "return_incremental": True}))
-            result = []
-            for i in range(0, thread_count):
-                result.append(t.submit(func))
-            # just ensure every input has result
-            for i in range(0, thread_count):
-                result_text, aux_info, finished = result[i].result()
-                self.assertEqual(2, len(aux_info))
-                self.assertEqual([True, True], finished)
-                logging.info(f"batch incremental {i} th : {result_text}, aux_info: {aux_info}, finished:{finished}")    
-                self.assertTrue(len(result_text) > 0)
-        finally:
-            inference_worker.stop()
+        def func():
+            return asyncio.run(self._run(self.inference_worker,
+                                         prompt_batch=["please write a story about dog",
+                                                       "please write a story about dog"],
+                                         yield_generator=True,
+                                         generate_config={"top_k": 1, "max_new_tokens": 3, "top_p": 1, "return_incremental": True}))
+        result_text, aux_info, finished = func()
+        self.assertEqual(2, len(aux_info))
+        self.assertEqual([True, True], finished)
+        logging.info(f"batch incremental: {result_text}, aux_info: {aux_info}, finished:{finished}")
+        self.assertTrue(len(result_text) > 0)
 
 
     def test_num_return_incremental(self):
-        inference_worker = self.create_inference_worker()
-        try:
-            thread_count = 10
-            t = ThreadPoolExecutor(thread_count)
-            def func():
-                return asyncio.run(self._run(inference_worker,
-                                             prompt="please write a story about dog",
-                                             yield_generator=True,
-                                             generate_config={"top_k":1, "max_new_tokens":3, "top_p": 1, "return_incremental": True, "num_return_sequences":3}))
-            result = []
-            for i in range(0, thread_count):
-                result.append(t.submit(func))
-            # just ensure every input has result
-            for i in range(0, thread_count):
-                result_text, aux_info, finished = result[i].result()
-                logging.info(f"incremental num return {i} th : {result_text}, aux_info: {aux_info}, finished:{finished}")
-                self.assertEqual(3, len(result_text))
-                self.assertEqual(3, len(aux_info))
-                self.assertEqual(True, finished)    
-                self.assertTrue(len(result_text) > 0)
-        finally:
-            inference_worker.stop()
+        def func():
+            return asyncio.run(self._run(self.inference_worker,
+                                         prompt="please write a story about dog",
+                                         yield_generator=True,
+                                         generate_config={"top_k": 1, "max_new_tokens": 3, "top_p": 1, "return_incremental": True, "num_return_sequences": 3}))
+        result_text, aux_info, finished = func()
+        logging.info(f"incremental num return: {result_text}, aux_info: {aux_info}, finished:{finished}")
+        self.assertEqual(3, len(result_text))
+        self.assertEqual(3, len(aux_info))
+        self.assertEqual(True, finished)
+        self.assertTrue(len(result_text) > 0)
 
     def test_batch_num_return_incremental(self):
-        inference_worker = self.create_inference_worker()
-        try:
-            thread_count = 10
-            t = ThreadPoolExecutor(thread_count)
-            def func():
-                return asyncio.run(self._run(inference_worker,
-                                             prompt_batch=["please write a story about dog", "please write a story about dog"],
-                                             yield_generator=True,
-                                             generate_config={"top_k":1, "max_new_tokens":3, "top_p": 1, "return_incremental": True, "num_return_sequences":3}))
-            result = []
-            for i in range(0, thread_count):
-                result.append(t.submit(func))
-            # just ensure every input has result
-            for i in range(0, thread_count):
-                result_text, aux_info, finished = result[i].result()
-                logging.info(f"incremental batch * num return {i} th : {result_text}, aux_info: {aux_info}, finished:{finished}")
-                self.assertEqual(2, len(result_text))
-                self.assertEqual(3, len(result_text[0]))
-                self.assertEqual(3, len(result_text[1]))
-                self.assertEqual(2, len(aux_info))
-                self.assertEqual(3, len(aux_info[0]))
-                self.assertEqual(3, len(aux_info[1]))
-                self.assertEqual([True, True], finished)    
-                self.assertTrue(len(result_text) > 0)
-        finally:
-            inference_worker.stop()
+        def func():
+            return asyncio.run(self._run(self.inference_worker,
+                                         prompt_batch=["please write a story about dog",
+                                                       "please write a story about dog"],
+                                         yield_generator=True,
+                                         generate_config={"top_k": 1, "max_new_tokens": 3, "top_p": 1, "return_incremental": True, "num_return_sequences": 3}))
+
+        result_text, aux_info, finished = func()
+        logging.info(f"incremental batch * num return: {result_text}, aux_info: {aux_info}, finished:{finished}")
+        self.assertEqual(2, len(result_text))
+        self.assertEqual(3, len(result_text[0]))
+        self.assertEqual(3, len(result_text[1]))
+        self.assertEqual(2, len(aux_info))
+        self.assertEqual(3, len(aux_info[0]))
+        self.assertEqual(3, len(aux_info[1]))
+        self.assertEqual([True, True], finished)
+        self.assertTrue(len(result_text) > 0)
 
     def test_encode(self):
-        inference_worker = self.create_inference_worker()
-        token_ids, tokens = inference_worker.tokenizer_encode('a b c')
+        token_ids, tokens = self.inference_worker.tokenizer_encode('a b c')
         self.assertEqual(token_ids, [1, 263, 289, 274])
         self.assertEqual(tokens, ['<s>', 'a', 'b', 'c'])
 
