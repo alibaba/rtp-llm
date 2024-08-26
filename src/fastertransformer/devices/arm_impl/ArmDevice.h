af6d8c3721de710207e07b3ac4b2e9b36f795e02
Ruifeng Wang <ruifeng.wang@arm.com>
2024-08-26 09:48:38 +0800

    attention: add MHA parallel support
    Signed-off-by: Ruifeng Wang <ruifeng.wang@arm.com>

--- src/fastertransformer/devices/arm_impl/ArmDevice.h
+++ src/fastertransformer/devices/arm_impl/ArmDevice.h
@@ -7,6 +7,14 @@
 
 namespace fastertransformer {
 
+#include <omp.h>
+template <typename T0, typename F>
+void parallel_for(const T0& D0, const F& func) {
+    int nthr = omp_get_max_threads();
+#pragma omp parallel for num_threads(nthr)
+    for (T0 d0 = 0; d0 < D0; ++d0) func(d0);
+}
+
 class ArmCpuDevice : public DeviceBase {
 public:
     ArmCpuDevice(const DeviceInitParams& params);
@@ -31,10 +39,21 @@ public:
     void sampleBeamSearch(const BeamSearchParams& params) override;
     void broadcast(const BroadcastParams& params) override;
     void allReduceSum(const AllReduceParams& params);
+    void printStat();
 
 private:
     std::unique_ptr<IAllocator> allocator_;
     arm_compute::DataType getAclDataType(DataType type);
+    void contextAttentionStride(const AttentionModuleParams& params);
+    void decoderSelfAttentionStride(const AttentionModuleParams& params);
+    void contextAttentionFallback(const AttentionModuleParams& params);
+    void decoderSelfAttentionFallback(const AttentionModuleParams& params);
+    void logTime(std::chrono::microseconds diff, size_t index);
+    uint64_t  a_cnt_[16] = {0};
+    uint64_t a_tmin_[16] = {999999999, 999999999, 999999999, 999999999, 999999999, 999999999, 999999999, 999999999,
+                            999999999, 999999999, 999999999, 999999999, 999999999, 999999999, 999999999, 999999999};
+    uint64_t a_tmax_[16] = {0};
+    uint64_t a_tave_[16] = {0};
 };
 
 } // namespace fastertransformer
