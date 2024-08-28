80fb1ccb30cb1468bdab5f5a4db9bf731f834b12
rtp-llm <rtp-llm@alibaba>
2024-08-28 11:29:02 +0800

    build: update arm docker

--- open_source/arm_docker/arm.Dockerfile
+++ open_source/arm_docker/arm.Dockerfile
@@ -3,15 +3,25 @@ FROM $BASE_OS_IMAGE
 
 MAINTAINER wangyin.yx
 
+ADD functions /etc/rc.d/init.d/functions
+
 RUN echo "%sdev ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers && \
     groupadd sdev
 
 RUN dnf install -y \
         unzip wget which findutils rsync tar \
         gcc gcc-c++ libstdc++-static gdb coreutils \
-        binutils bash glibc-devel libdb glibc glibc-langpack-en bison lld \
-        emacs-nox git git-lfs openblas-devel
+        binutils bash glibc-devel libdb glibc glibc-langpack-en bison lld && \
+    dnf clean all
 
+RUN dnf install -y \
+        emacs-nox git git-lfs openblas-devel \
+    	docker nfs-utils && \
+    dnf clean all
+
+RUN dnf install -y \
+    	java-latest-openjdk-devel.aarch64 && \
+    dnf clean all
 
 ARG CONDA_URL
 RUN wget $CONDA_URL -O /tmp/conda.sh && \
@@ -26,9 +36,9 @@ RUN git config --system core.hooksPath .githooks && \
 
 ARG PYPI_URL
 ADD deps /tmp/deps
-RUN /opt/conda310/bin/pip install -r /tmp/deps/requirements_base.txt -i $PYPI_URL && \
+RUN /opt/conda310/bin/pip install -r /tmp/deps/requirements_torch_cpu.txt -r /tmp/deps/requirements_base.txt -i $PYPI_URL && \
     rm -rf /tmp/deps && /opt/conda310/bin/pip cache purge
 
 ARG BAZELISK_URL
-RUN wget -q $BAZELISK_URL -O /usr/local/bin/bazelisk && chmod a+x /usr/local/bin/bazelisk
+RUN wget $BAZELISK_URL -O /usr/local/bin/bazelisk && chmod a+x /usr/local/bin/bazelisk
 
--- open_source/arm_docker/build.sh
+++ open_source/arm_docker/build.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-set +x;
+set -x;
 
 echo $0
 echo $(readlink -f "$0")
@@ -7,17 +7,18 @@ DIR=$(dirname $(readlink -f "$0"))
 echo $DIR
 
 # These variables are to replace by your own values
-BASE_IMAGE="alibaba-cloud-linux-3-registry.cn-hangzhou.cr.aliyuncs.com/alinux3/alinux3:220901.1"
+BASE_IMAGE="alibaba-cloud-linux-3-registry.cn-hangzhou.cr.aliyuncs.com/alinux3/alinux3:231220.1"
 IMAGE_NAME=${IMAGE_NAME:-"reg.docker.alibaba-inc.com/isearch/maga_transformer_arm_open_source_base"}
-IMAGE_TAG=${IMAGE_TAG:-"0.0.1"}
+IMAGE_TAG=${IMAGE_TAG:-"0.0.3"}
 CONDA_URL=${CONDA_URL:-"https://repo.anaconda.com/miniconda/Miniconda3-py310_23.1.0-1-Linux-aarch64.sh"}
 # alternative:
 BAZEL_URL=https://mirrors.huaweicloud.com/bazel/5.2.0/bazel-5.2.0-linux-arm64
 # BAZEL_URL=${BAZEL_URL:-"https://github.com/bazelbuild/bazel/releases/download/5.2.0/bazel-5.2.0-linux-arm64"}
-BAZELISK_URL=http://search-ad.oss-cn-hangzhou-zmf-internal.aliyuncs.com/aios/bazelisk-1.20.0
+BAZELISK_URL=https://github.com/bazelbuild/bazelisk/releases/download/v1.20.0/bazelisk-linux-arm64
 PYPI_URL=${PYPI_URL:-"https://mirrors.aliyun.com/pypi/simple"}
 
 mkdir deps && cp ../deps/requirements*.txt $DIR/deps/
+cp ../rocm_docker/functions .
 
 docker build \
     --build-arg BASE_OS_IMAGE=$BASE_IMAGE \
@@ -32,3 +33,4 @@ docker build \
 docker tag $IMAGE_NAME:$IMAGE_TAG $IMAGE_NAME:latest
 
 rm -rf $DIR/deps
+rm -rf functions
