081131cde8030d9abf5c01c7e00f730a8a8d856a
rtp-llm <rtp-llm@alibaba>
2024-10-17 17:34:36 +0800

    fix: fix sp unit test
    Signed-off-by: zhaoxiaoyong <zhaoxiaoyong.zxy@alibaba-inc.com>

--- maga_transformer/cpp/speculative_engine/test/SpeculativeMockEngine.h
+++ maga_transformer/cpp/speculative_engine/test/SpeculativeMockEngine.h
@@ -13,7 +13,7 @@ createVanillaSpeculativeEngine(DeviceBase* device, const CustomConfig& config, G
     EngineInitParams                              score_params   = createEngineInitParams(device, config, params);
     EngineInitParams                              vanilla_params = createEngineInitParams(device, config, params);
     std::unique_ptr<ProposeModelEngineInitParams> propose_params = std::make_unique<ProposeModelEngineInitParams>(
-        "vanilla", 5, vanilla_params.gpt_init_parameter, std::move(vanilla_params.gpt_weights));
+        "vanilla", 1, vanilla_params.gpt_init_parameter, std::move(vanilla_params.gpt_weights));
     std::shared_ptr<SpeculativeEngine> engine = make_shared<SpeculativeEngine>(score_params, std::move(propose_params));
     THROW_IF_STATUS_ERROR(engine->init());
     return engine;
