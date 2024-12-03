5ff47fb6055ab5e107fc6a99b17b4a82d1443db9
rtp-llm <rtp-llm@alibaba>
2024-12-04 00:48:57 +0800

    test: real random by seed

--- maga_transformer/test/utils/port_util.py
+++ maga_transformer/test/utils/port_util.py
@@ -4,6 +4,7 @@ import random
 from contextlib import closing
 
 def get_consecutive_free_ports(num_port = 1):
+    random.seed()
     start_port = random.randint(12000, 20000) # 不要用随机端口，容易冲突
     for base_port in range(start_port, 65536 - num_port + 1):
         if all(is_port_free(port) for port in range(base_port, base_port + num_port)):
