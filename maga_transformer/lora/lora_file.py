3574be27648182662ca2942f3cd528f5daad6ef4
rtp-llm <rtp-llm@alibaba>
2025-01-16 15:43:23 +0800

    fix: fix lora load raise exception when load compact.

--- maga_transformer/lora/lora_file.py
+++ maga_transformer/lora/lora_file.py
@@ -20,7 +20,7 @@ class LoraConfig:
     is_merge: bool = False
 
     def __init__(self, name: str = '', json_path: str = '') -> None:
-        
+
         if json_path.endswith('.json'):
             with open(json_path, 'r') as f:
                 f = json.load(f)
@@ -30,7 +30,7 @@ class LoraConfig:
                 self.lora_dropout = f['lora_dropout']
                 self.target_modules = f['target_modules']
                 self.name = name
-            
+
         return
 
     def get_scale(self):
@@ -50,7 +50,7 @@ class LoraInfos:
 
     def __init__(self) -> None:
         self.lora_infos = []
-    
+
     def add_lora_infos(self, LoraFileList : Dict[LoraConfig, List[CkptFileInfo]]):
         for key, value in LoraFileList.items():
             lora_name       = key.name
@@ -65,10 +65,10 @@ class LoraInfos:
 
 class LoraCkpt:
     LoraFileList: Dict[LoraConfig, List[CkptFileInfo]]
-    
+
     def __init__(self):
         self.LoraFileList = {}
-    
+
     def get_lora_tensor_names(self, config_name: str) -> List[str]:
         tensor_names = []
         for key, value in self.LoraFileList.items():
@@ -76,7 +76,7 @@ class LoraCkpt:
                 for ckptfile in value:
                     tensor_names.extend(ckptfile.get_tensor_names())
         return tensor_names
-    
+
     def load_lora_tensor(self, lora_name: str, tensor_name: str) -> List[torch.Tensor]:
         tensors = []
         for key, value in self.LoraFileList.items():
@@ -102,33 +102,33 @@ class LoraCkpt:
             raise Exception(f"No lora model ckpt files")
         if len(lora_ckpt_paths) != 1:
             raise Exception(f"More than one model ckpt files")
-        
+
         lora_config_path = os.path.join(lora_path, "adapter_config.json")
         if not os.path.exists(lora_config_path):
             raise Exception(f"Error lora config path: {lora_config_path}")
-        
+
         logging.info(f"lora_config_path is {lora_config_path}")
         lora_config = LoraConfig(config_name, lora_config_path)
         ckpt = CkptFileInfo(file_name=str(lora_ckpt_paths[0]), finetune_type=FinetuneType.lora)
         self.LoraFileList[lora_config] = [ckpt]
         self.lora_check()
-    
+
     def lora_check(self) -> None:
         if len(self.LoraFileList) == 0:
             logging.info("The database has no lora ckpts")
             return
-        
+
         for key, _ in self.LoraFileList.items():
             tensor_names = self.get_lora_tensor_names(key.name)
             for name in tensor_names:
                 self.lora_tensor_check(name)
-    
+
     def lora_tensor_check(self, tensor_name: str):
-        # check lora tensor names. the lora tensor name should match 
+        # check lora tensor names. the lora tensor name should match
         # the format which is "base_model.model.{}.{}.weight"
         pattern = r'base_model\.model\.(.*)\.(lora_A|lora_B)\.weight'
         if re.fullmatch(pattern, tensor_name) == None:
-            logging.warn(f"invalid lora tensor name : {tensor_name}")
+            raise Exception(f"invalid lora tensor name : {tensor_name}")
 
     def remove_lora(self, name:str):
         for key, _ in self.LoraFileList.items():
@@ -136,21 +136,21 @@ class LoraCkpt:
                 del self.LoraFileList[key]
                 return True
         return False
-    
+
     def has_lora(self):
         return len(self.LoraFileList) == 1
-    
+
     def get_first_lora_name(self):
         if len(self.LoraFileList) == 1:
             return list(self.LoraFileList)[0].name
         return None
-    
+
     def get_lora(self, config_name: str) -> List[Any]:
         for key, value in self.LoraFileList.items():
             if key.name == config_name:
                 return value
         return []
-    
+
     def get_lora_config(self, config_name: str):
         for key, _ in self.LoraFileList.items():
             if key.name == config_name:
