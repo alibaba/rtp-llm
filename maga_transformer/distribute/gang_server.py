fix model stop, exit hang, broadcast failed rank info

maga_transformer/distribute/gang_server.py
@@ -14,19 +14,19 @@ from datetime import timedelta
 
 import torch.distributed as dist
 from maga_transformer.config.uvicorn_config import UVICORN_LOGGING_CONFIG
-from maga_transformer.distribute.worker_info import g_worker_info, g_parallel_info, g_master_info, update_master_info, DEFAULT_START_PORT
+from maga_transformer.distribute.worker_info import WorkerInfo, g_worker_info, g_parallel_info, g_master_info, update_master_info, DEFAULT_START_PORT
 from maga_transformer.distribute.gang_info import get_gang_info, GangInfo
 
 # for ut
 from maga_transformer.distribute.gang_test_util import create_store, store_based_barrier
 
-def http_post_with_retry(url: str, data: Dict[str, Any], retry_limit: int = 3):
+def http_post_with_retry(url: str, data: Dict[str, Any], retry_limit: int = 3, timeout: int = 150):
     retry_time = 0
     ret = None
     while retry_time < retry_limit:
         retry_time += 1
         try:
-            ret = requests.post(url, json=data, timeout=150)
+            ret = requests.post(url, json=data, timeout=timeout)
             if ret.status_code == 200:
                 return ret
         except:
@@ -34,12 +34,36 @@ def http_post_with_retry(url: str, data: Dict[str, Any], retry_limit: int = 3):
                 raise
     return ret
 
+class FailedRankInfo:
+    def __init__(self, failed_ip: str, failed_local_rank: int, failed_world_rank: int,
+                 timestamp: int, formatted_time: str, reporter: List[str]):
+        self.failed_ip = failed_ip
+        self.failed_world_rank = failed_world_rank
+        self.failed_local_rank = failed_local_rank
+        self.timestamp = timestamp
+        self.formatted_time = formatted_time
+        self.reporter = reporter
+        
+    def __str__(self):
+        return (f"FailedRankInfo(failed_ip='{self.failed_ip}', "
+                f"failed_local_rank={self.failed_local_rank}, "
+                f"failed_world_rank={self.failed_world_rank}, "
+                f"timestamp={self.timestamp}, "
+                f"formatted_time='{self.formatted_time}', "
+                f"reporter={self.reporter})")
+
+    def __repr__(self):
+        return self.__str__()
+
 class GangServer:
     def __init__(self):
         self._initialized: bool = False
         self._gang_status: Dict[str, str] = {}
         self._gang_info: GangInfo = None
         self._request_threadpool = ThreadPoolExecutor(g_parallel_info.world_size)
+        self._failure_events: Dict[int, FailedRankInfo] = {}
+        self._delay_exit_loops: int = 0
+        self._max_delay_times: int = 3
 
     def _start_server(self):
         app = FastAPI()
@@ -52,6 +76,18 @@ class GangServer:
                 logging.debug(f'server member recv: {name} {ip}')
                 self._gang_status[name] = ip
             return {'initializing':not self._initialized}
+
+        @app.post('/report_failure')
+        def handle_failure_report(req: Dict[str, Any]):
+            logging.info(f"receive handle_failure_report info {req}")
+            try:
+                failed_info = FailedRankInfo(req['failed_ip'], req['failed_local_rank'], req['failed_world_rank'], 
+                                            req['timestamp'], req['formatted_time'], [req['reporter']])
+                self._update_failure_table(failed_info)
+                return {"status": "ok"}
+            except Exception as e:
+                logging.error(f"Error while processing failure report: {e}")
+                return {"status": "error", "message": str(e)}, 500
 
         try:
             uvicorn.run(
@@ -69,6 +105,16 @@ class GangServer:
         t = Thread(target=self._start_server)
         t.daemon = True
         t.start()
+
+    def _update_failure_table(self, failed_info: FailedRankInfo):
+        rank = failed_info.failed_world_rank
+        if rank not in self._failure_events:
+            logging.info(f"add rank {rank} for {failed_info}")
+            self._failure_events[rank] = failed_info
+        else:
+            if failed_info.timestamp < self._failure_events[rank].timestamp:
+                self._failure_events[rank].timestamp = failed_info.timestamp
+                self._failure_events[rank].reporter.append(failed_info.reporter)
 
     def _check_gang_info(self, gang_info: GangInfo):
         if gang_info.master is None or gang_info.self is None:
@@ -152,23 +198,64 @@ class GangServer:
             wait(future_list)
 
     def _health_check_impl(self):
+        failed_member = None
         for member in self._gang_info.members:
             url = f'http://{member.ip}:{member.gang_hb_port}/heartbeat'
             try:
                 res = http_post_with_retry(url, {"name": "", "ip": ""})
             except:
-                logging.error(f"Gang server {member.ip}:{member.gang_hb_port} heartbeat loss, do abort")
-                os._exit(-1)
+                logging.error(f"Gang server {member.ip}:{member.gang_hb_port} heartbeat loss")
+                failed_member = member
+                break
             if res.status_code != 200:
-                logging.error(f"Gang server {member.ip} status code is not 200, do abort")
-                os._exit(-1)
+                logging.error(f"Gang server {member.ip} status code is not 200")
+                failed_member = member
+                break
             try:
                 if res.json()['initializing'] == True:
-                    logging.error(f"Gang server {member.ip}:{member.gang_hb_port} is restarted, do abort")
-                    os._exit(-1)
+                    logging.error(f"Gang server {member.ip}:{member.gang_hb_port} is restarted")
+                    failed_member = member
+                    break
             except Exception as e:
-                logging.error(f"Gang server {member.ip}:{member.gang_hb_port} hb failed {e}, do abort")
-                os._exit(-1)
+                logging.error(f"Gang server {member.ip}:{member.gang_hb_port} hb failed {e}")
+                failed_member = member
+                break
+        
+        if failed_member:
+            logging.info(f"broadcast for failed member [world rank {failed_member.world_rank}]")
+            self.broadcast_failure(failed_member)
+            self._delay_exit_loops += 1
+
+        if self._delay_exit_loops > self._max_delay_times:
+            logging.info(f"member [world rank {failed_member.world_rank}] heartbeat exception, do abort!")
+            os._exit(-1)
+
+    # use base model type, python3
+    def broadcast_failure(self, failed_member: WorkerInfo):
+        for member in self._gang_info.members:
+            if failed_member.world_rank != member.world_rank:
+                report_url = f'http://{member.ip}:{member.gang_hb_port}/report_failure'
+                try:
+                    now = datetime.datetime.now()
+                    formatted_time_ms = now.strftime('%Y-%m-%d %H:%M:%S') + f".{now.microsecond // 1000:03}"
+                    current_timestamp_ms = int(time.time() * 1000)
+                    http_post_with_retry(report_url, {
+                        "failed_ip": failed_member.ip,
+                        "failed_world_rank": failed_member.world_rank,
+                        "failed_local_rank": failed_member.local_rank,
+                        "formatted_time": formatted_time_ms,
+                        "timestamp": current_timestamp_ms,
+                        "reporter": str(-1)
+                    }, 1, 30)
+                except Exception as e:
+                    logging.warning(f"Failed to notify [world rank {member.world_rank}] {e}")
+                    continue
+        logging.info(f"broadcast success")
+
+    def _check_failed(self):
+        if len(self._failure_events) > 0:
+            first_failure_rank, first_failure_info = min(self._failure_events.items(), key=lambda x: x[1].timestamp)
+            logging.error(f"first failure node is {first_failure_info}")
 
     def start_health_check(self):
         sleep_time = int(os.environ.get('GANG_SLEEP_TIME', '10'))
@@ -177,8 +264,9 @@ class GangServer:
                 time.sleep(sleep_time)
                 try:
                     self._health_check_impl()
+                    self._check_failed()
                 except:
-                    os._exit(-1)              
+                    os._exit(-1)
         t = Thread(target=wrapper)
         t.daemon = True
         t.start()
@@ -203,19 +291,8 @@ class GangServer:
             self._gang_info.master.server_port)
         master_url = f"tcp://{g_master_info.ip}:{self._gang_info.master.server_port - 1}"
         logging.info(f'gang worker {g_parallel_info} exchange done')
-        # init_process_group会去检查gpu num > 0, 所以测试环境不希望init_process_group
 
         init_process_timeout = int(os.environ.get('DIST_BARRIER_TIMEOUT', 45))
-        # 使用ProcessGroupNCCL自带的health check进行一次同步+nccl检测
-        # if os.environ.get('FAKE_GANG_ENV', None) == None:
-        #     os.environ['ENABLE_NCCL_HEALTH_CHECK'] = '1'
-        #     dist.init_process_group(backend=dist.Backend.NCCL,
-        #                             init_method=master_url,
-        #                             rank=g_parallel_info.world_rank,
-        #                             world_size=g_parallel_info.world_size,
-        #                             timeout=timedelta(seconds=init_process_timeout))
-        # else:
-            # 由于后续会进行health check，且init_process_group默认不进行block，因此使用memory barrier进行一次同步
         self.memory_barrier(master_url, timeout=init_process_timeout)
 
         self.start_health_check()