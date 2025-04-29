64bb0c6e0350030d75f909660603c9a00baeb873
rtp-llm <rtp-llm@alibaba>
2025-04-30 00:28:23 +0800

    refactor: rm useless

--- src/fastertransformer/devices/cuda_impl/CudaMlaContextAttention.cc
+++
@@ -1,9 +0,0 @@
-#include "src/fastertransformer/devices/cuda_impl/CudaDevice.h"
-#include "src/fastertransformer/kernels/mla_kernels/mla_merge_transpose_kernel.h"
-#include "src/fastertransformer/devices/utils/DebugUtils.h"
-#include "src/fastertransformer/core/BufferHelper.h"
-#include "src/fastertransformer/cuda/Dispatch.h"
-
-namespace fastertransformer {
-
-}  // namespace fastertransformer
