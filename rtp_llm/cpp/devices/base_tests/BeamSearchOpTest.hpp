f393a90ec0f0bb2494d821b26a3f4d2fe02a3df5
rtp-llm <rtp-llm@alibaba>
2025-05-25 19:43:46 +0800

    test: add test for variable beam width search

--- rtp_llm/cpp/devices/base_tests/BeamSearchOpTest.hpp
+++ rtp_llm/cpp/devices/base_tests/BeamSearchOpTest.hpp
@@ -11,65 +11,78 @@ public:
     torch::TensorOptions int_options   = torch::TensorOptions(torch::kInt).device(torch::Device(torch::kCPU));
     torch::TensorOptions float_options = torch::TensorOptions(torch::kFloat).device(torch::Device(torch::kCPU));
 
+    // BS: batch_size, MSL: max_seq_len
+    // BMI: beam_width_in, BMO: beam_width_out
+
     struct TestBeamSearchInput {
-        // float [BS, BM]
+        // float [BS, BMI]
         torch::Tensor logits;
-        // int [BS, BM, MSL]
+        // int [BS, MBI, MSL]
         torch::Tensor token_ids;
-        // int [BS, BM] context + decoder
+        // int [BS, MBI] context + decoder
         torch::Tensor input_lengths;
-        // int [BS, BM] context + decoder
+        // int [BS, MBI] context + decoder
         torch::Tensor sequence_lengths;
-        // float [BS, BM]
+        // float [BS, MBI]
         torch::Tensor cum_log_probs;
-        // int [BS, BM]
-        torch::Tensor beam_index;
+        // int
+        int beam_width_out;
     };
 
     struct TestBeamSearchOutput {
-        // int [BS, BM, MSL]
+        // int [BS, BMO, MSL]
         torch::Tensor token_ids;
-        // float [BS, BM]
+        // int [BS, BMO] context + decoder
+        torch::Tensor input_lengths;
+        // int [BS, BMO] context + decoder
+        torch::Tensor sequence_lengths;
+        // float [BS, BMO]
         torch::Tensor cum_log_probs;
-        // int [BS, BM]
-        torch::Tensor beam_index;
+        // int [BS, BMO]
+        torch::Tensor beam_indices;
     };
 
-    TestBeamSearchInput prepareInput(int batch_size, int beam_width, int vocab_size, int max_seq_len) {
-        int rand_input_length    = 4;
-        int rand_length          = std::rand() % (max_seq_len - rand_input_length);
+    TestBeamSearchInput
+    prepareInput(int batch_size, int beam_width_in, int beam_width_out, int vocab_size, int max_seq_len) {
+        int rand_input_length    = torch::randint(1, max_seq_len, {1}, int_options).item<int>();
+        int rand_length          = torch::randint(0, max_seq_len - rand_input_length, {1}, int_options).item<int>();
         int rand_sequence_length = rand_length + rand_input_length;
 
-        auto input_lengths    = torch::ones({batch_size, beam_width}, int_options) * rand_input_length;
-        auto sequence_lengths = torch::ones({batch_size, beam_width}, int_options) * rand_sequence_length;
-        auto cum_log_probs    = torch::rand({batch_size, beam_width}, float_options);
-        auto token_ids        = torch::randint(0, vocab_size - 1, {batch_size, beam_width, max_seq_len}, int_options);
-        auto logits           = torch::rand({batch_size, beam_width, vocab_size}, float_options);
+        auto       input_lengths    = torch::full({batch_size, beam_width_in}, rand_input_length, int_options);
+        auto       sequence_lengths = torch::full({batch_size, beam_width_in}, rand_sequence_length, int_options);
+        at::Tensor cum_log_probs, token_ids, logits;
         if (rand_sequence_length == rand_input_length) {
-            logits = torch::rand({batch_size, 1, vocab_size}, float_options).repeat({1, beam_width, 1});
+            logits = torch::rand({batch_size, 1, vocab_size}, float_options).repeat({1, beam_width_in, 1});
             token_ids =
-                torch::randint(0, vocab_size - 1, {batch_size, 1, max_seq_len}, int_options).repeat({1, beam_width, 1});
-            cum_log_probs = torch::zeros({batch_size, beam_width}, float_options);
+                torch::randint(0, vocab_size, {batch_size, 1, max_seq_len}, int_options).repeat({1, beam_width_in, 1});
+            cum_log_probs = torch::zeros({batch_size, beam_width_in}, float_options);
+        } else {
+            logits        = torch::rand({batch_size, beam_width_in, vocab_size}, float_options);
+            token_ids     = torch::randint(0, vocab_size, {batch_size, beam_width_in, max_seq_len}, int_options);
+            cum_log_probs = torch::rand({batch_size, beam_width_in}, float_options);
         }
-        auto beam_index = torch::zeros({batch_size, beam_width}, int_options);
 
-        return TestBeamSearchInput({logits, token_ids, input_lengths, sequence_lengths, cum_log_probs, beam_index});
+        return TestBeamSearchInput({logits, token_ids, input_lengths, sequence_lengths, cum_log_probs, beam_width_out});
     };
 
     TestBeamSearchOutput opRun(TestBeamSearchInput& input) {
-
         auto logits           = tensorToBuffer(input.logits, AllocationType::DEVICE);
         auto input_lengths    = tensorToBuffer(input.input_lengths, AllocationType::DEVICE);
         auto sequence_lengths = tensorToBuffer(input.sequence_lengths, AllocationType::DEVICE);
         auto token_ids        = tensorToBuffer(input.token_ids, AllocationType::DEVICE);
         auto cum_log_probs    = tensorToBuffer(input.cum_log_probs, AllocationType::DEVICE);
-        auto beam_index       = tensorToBuffer(input.beam_index, AllocationType::DEVICE);
+
+        auto beam_index_t = torch::zeros_like(input.input_lengths, int_options);
+        auto beam_index   = tensorToBuffer(beam_index_t);
 
         device_->sampleBeamSearch(
             {*logits, *token_ids, *input_lengths, *sequence_lengths, *cum_log_probs, *beam_index});
 
-        return TestBeamSearchOutput(
-            {bufferToTensor(*token_ids), bufferToTensor(*cum_log_probs), bufferToTensor(*beam_index)});
+        return TestBeamSearchOutput({bufferToTensor(*token_ids),
+                                     bufferToTensor(*input_lengths),
+                                     bufferToTensor(*sequence_lengths),
+                                     bufferToTensor(*cum_log_probs),
+                                     bufferToTensor(*beam_index)});
     };
 
     TestBeamSearchOutput torchRef(TestBeamSearchInput& input) {
@@ -82,17 +95,25 @@ public:
                                             input.input_lengths,
                                             input.sequence_lengths,
                                             input.cum_log_probs,
-                                            input.beam_index});
+                                            input.beam_width_out});
 
-        return TestBeamSearchOutput({result.token_ids, result.cum_log_probs, result.beam_index});
+        return TestBeamSearchOutput({result.token_ids,
+                                     result.input_lengths,
+                                     result.sequence_lengths,
+                                     result.cum_log_probs,
+                                     result.beam_indices});
     };
 
     void simpleTest(int batch_size, int beam_width, int vocab_size, int max_seq_len) {
-        auto input  = prepareInput(batch_size, beam_width, vocab_size, max_seq_len);
+        variableBeamWidthTest(batch_size, beam_width, beam_width, vocab_size, max_seq_len);
+    }
+
+    void variableBeamWidthTest(int batch_size, int beam_width_in, int beam_width_out, int vocab_size, int max_seq_len) {
+        auto input  = prepareInput(batch_size, beam_width_in, beam_width_out, vocab_size, max_seq_len);
         auto result = opRun(input);
         auto ref    = torchRef(input);
         assertTensorClose(result.token_ids, ref.token_ids);
         assertTensorClose(result.cum_log_probs, ref.cum_log_probs);
-        assertTensorClose(result.beam_index, ref.beam_index);
+        assertTensorClose(result.beam_indices, ref.beam_indices);
     }
 };
--- rtp_llm/cpp/devices/torch_impl/BeamSearchOp.h
+++ rtp_llm/cpp/devices/torch_impl/BeamSearchOp.h
@@ -6,103 +6,123 @@ namespace rtp_llm {
 namespace torch_impl {
 
 struct BeamSearchOpInput {
-    // [batch_size, beam_num, vocab_size] float
+    // [batch_size, beam_width_in, vocab_size] float
     torch::Tensor logits;
-    // [batch_size, beam_num, max_seq_len] int
+    // [batch_size, beam_width_in, max_seq_len] int
     torch::Tensor token_ids;
-    // [batch_size, beam_num] int
+    // [batch_size, beam_width_in] int
     torch::Tensor input_lengths;
-    // [batch_size, beam_num] int
+    // [batch_size, beam_width_in] int
     torch::Tensor sequence_lengths;
-    // [batch_size, beam_num] float
+    // [batch_size, beam_width_in] float
     torch::Tensor cum_log_probs;
-    // [batch_size, beam_num] int
-    torch::Tensor beam_index;
+    int           beam_width_out;
 
-    // only index in first dim, split batch compute to multi single compute.
+    // only indexing on the first dim, split a batch computation into multiple single computations.
     BeamSearchOpInput operator[](int index) {
         return BeamSearchOpInput({logits[index],
                                   token_ids[index],
                                   input_lengths[index],
                                   sequence_lengths[index],
                                   cum_log_probs[index],
-                                  beam_index[index]});
+                                  beam_width_out});
     }
 };
 
 struct BeamSearchOpOutput {
-    // [batch_size, beam_num, max_seq_len] int
+    // [batch_size, beam_width_out, max_seq_len] int
     torch::Tensor token_ids;
-    // [batch_size, beam_num] float
+    // [batch_size, beam_width_out] int
+    torch::Tensor input_lengths;
+    // [batch_size, beam_width_out] int
+    torch::Tensor sequence_lengths;
+    // [batch_size, beam_width_out] float
     torch::Tensor cum_log_probs;
-    // [batch_size, beam_num] int
-    torch::Tensor beam_index;
+    // [batch_size, beam_width_out] int
+    torch::Tensor beam_indices;
 
-    void zeros(int batch_size, int beam_num, int max_seq_len) {
-        token_ids     = torch::zeros({batch_size, beam_num, max_seq_len},
+    void zeros(int batch_size, int beam_width_out, int max_seq_len) {
+        token_ids        = torch::zeros({batch_size, beam_width_out, max_seq_len},
                                  torch::TensorOptions(torch::kInt).device(torch::Device(torch::kCPU)));
-        cum_log_probs = torch::zeros({batch_size, beam_num},
+        input_lengths    = torch::zeros({batch_size, beam_width_out},
+                                     torch::TensorOptions(torch::kInt).device(torch::Device(torch::kCPU)));
+        sequence_lengths = torch::zeros({batch_size, beam_width_out},
+                                        torch::TensorOptions(torch::kInt).device(torch::Device(torch::kCPU)));
+        cum_log_probs    = torch::zeros({batch_size, beam_width_out},
                                      torch::TensorOptions(torch::kFloat).device(torch::Device(torch::kCPU)));
-        beam_index =
-            torch::zeros({batch_size, beam_num}, torch::TensorOptions(torch::kInt).device(torch::Device(torch::kCPU)));
+        beam_indices     = torch::zeros({batch_size, beam_width_out},
+                                    torch::TensorOptions(torch::kInt).device(torch::Device(torch::kCPU)));
     };
 
     // note output must has no batch dim.
     void set(int index, BeamSearchOpOutput output) {
         token_ids.index_put_({index}, output.token_ids);
+        input_lengths.index_put_({index}, output.input_lengths);
+        sequence_lengths.index_put_({index}, output.sequence_lengths);
         cum_log_probs.index_put_({index}, output.cum_log_probs);
-        beam_index.index_put_({index}, output.beam_index);
+        beam_indices.index_put_({index}, output.beam_indices);
     }
 };
 
 struct BeamSearchOpImpl: torch::nn::Module {
 
-    BeamSearchOpOutput oneBatchForward(BeamSearchOpInput input) {
-        // logits float[beam_width, vocab_size]
-        auto beam_width = input.logits.size(0);
+    BeamSearchOpOutput singleForward(BeamSearchOpInput input) {
+        // logits float[beam_width_in, vocab_size]
+        auto beam_width_in  = input.logits.size(0);
+        auto beam_width_out = input.beam_width_out;
+        auto max_seq_len    = input.token_ids.size(1);
         // first topk from log softmax logits
-        // log_probs: [beam_width, beam_width]
-        auto [log_probs, index] = input.logits.log_softmax(-1).topk(beam_width, -1);
+        // log_probs: [beam_width_in, beam_width_out]
+        auto [log_probs, index] = input.logits.log_softmax(-1).topk(beam_width_out, -1);
         // add cum_log_probs
         auto cum_log_probs = input.cum_log_probs;
         if (input.input_lengths.equal(input.sequence_lengths)) {
-            for (int i = 1; i < beam_width; i++) {
-                cum_log_probs.index_put_({i}, -1e9);
-            }
+            cum_log_probs.index_put_({torch::indexing::Slice(1)}, -1e9);
         }
-        log_probs = log_probs + input.cum_log_probs.reshape({-1, 1});
+        log_probs = log_probs + cum_log_probs.reshape({-1, 1});
 
-        auto new_token_ids =
-            torch::zeros({beam_width}, torch::TensorOptions(torch::kInt).device(torch::Device(torch::kCPU)));
-        auto beam_index =
-            torch::zeros({beam_width}, torch::TensorOptions(torch::kInt).device(torch::Device(torch::kCPU)));
-        // first beam search
+        auto input_lengths =
+            torch::zeros({beam_width_out}, torch::TensorOptions(torch::kInt).device(torch::Device(torch::kCPU)));
+        auto sequence_lengths =
+            torch::zeros({beam_width_out}, torch::TensorOptions(torch::kInt).device(torch::Device(torch::kCPU)));
+        auto token_ids = torch::zeros({beam_width_out, max_seq_len},
+                                      torch::TensorOptions(torch::kInt).device(torch::Device(torch::kCPU)));
 
-        std::tie(log_probs, beam_index) = log_probs.flatten(0, -1).topk(beam_width, -1);
-        new_token_ids                   = torch::gather(index.flatten(0, -1), 0, beam_index);
-        beam_index                      = torch::div(beam_index, beam_width, "floor").squeeze();
+        // start beam search
+        at::Tensor beam_indices;
+        std::tie(log_probs, beam_indices) = log_probs.flatten(0, -1).topk(beam_width_out, -1);
+        auto new_token_ids                = torch::gather(index.flatten(0, -1), 0, beam_indices);
+
+        // keep track of beam indices
+        auto beam_indices_in = torch::div(beam_indices, beam_width_out, "floor").squeeze();
 
         // according beam index to update input token ids
-        auto token_ids = input.token_ids.clone();
-        for (int i = 0; i < beam_width; i++) {
-            // update sequence lengths
-            auto sequence_lengths_index = input.sequence_lengths[i].item<int>() + 1;
-            auto select_beam_index      = beam_index[i].item<int>();
-            auto tmp                    = input.token_ids[select_beam_index];
-            token_ids.index_put_({i}, tmp);
-            token_ids.index_put_({i, sequence_lengths_index - 1}, new_token_ids[i]);
+        for (int beam_idx_out = 0; beam_idx_out < beam_width_out; ++beam_idx_out) {
+            auto beam_idx_in = beam_indices_in[beam_idx_out].item<int>();
+
+            // keep track of input lengths
+            auto input_len = input.input_lengths[beam_idx_in].item<int>();
+            input_lengths.index_put_({beam_idx_out}, input_len);
+
+            // keep track of sequence lengths
+            auto seq_len = input.sequence_lengths[beam_idx_in].item<int>();
+            sequence_lengths.index_put_({beam_idx_out}, seq_len + 1);
+
+            // update token_ids
+            token_ids.index_put_({beam_idx_out}, input.token_ids[beam_idx_in]);
+            token_ids.index_put_({beam_idx_out, seq_len}, new_token_ids[beam_idx_out]);
         }
-        return BeamSearchOpOutput({token_ids, log_probs, beam_index});
+        return BeamSearchOpOutput({token_ids, input_lengths, sequence_lengths, log_probs, beam_indices_in});
     };
 
     BeamSearchOpOutput forward(BeamSearchOpInput input) {
-        int                batch_size  = input.logits.size(0);
-        int                beam_num    = input.logits.size(1);
-        int                max_seq_len = input.token_ids.size(2);
+        int                batch_size     = input.logits.size(0);
+        int                beam_width_out = input.beam_width_out;
+        int                max_seq_len    = input.token_ids.size(2);
         BeamSearchOpOutput output;
-        output.zeros(batch_size, beam_num, max_seq_len);
+        output.zeros(batch_size, beam_width_out, max_seq_len);
         for (int i = 0; i < batch_size; i++) {
-            auto result = oneBatchForward(input[i]);
+            auto result = singleForward(input[i]);
             output.set(i, result);
         }
         return output;
