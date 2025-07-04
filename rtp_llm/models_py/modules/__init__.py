87ef28b6690936852d56a576584784cffd1c9d3f
rtp-llm <rtp-llm@alibaba>
2025-07-04 10:53:57 +0800

    fix rocm rmsnorm ut

--- rtp_llm/models_py/modules/__init__.py
+++ rtp_llm/models_py/modules/__init__.py
@@ -7,7 +7,7 @@ if device_type == DeviceType.ROCm:
 else:
     from rtp_llm.models_py.modules.norm import RMSNorm
 
-from rtp_llm.models_py.modules.norm import LayerNorm, AddBiasResLayerNorm
+from rtp_llm.models_py.modules.norm import LayerNorm, AddBiasResLayerNorm, RMSNormTorch
 from rtp_llm.models_py.modules.embedding import Embedding
 from rtp_llm.models_py.modules.linear import Linear
 from rtp_llm.models_py.modules.mlp import DenseMLP
--- rtp_llm/models_py/test/rocm_norm_test.py
+++ rtp_llm/models_py/test/rocm_norm_test.py
@@ -1,7 +1,7 @@
 import torch
 import itertools
 from unittest import TestCase, main, SkipTest
-from rtp_llm.models_py.modules.norm import RMSNorm, RMSNormTorch
+from rtp_llm.models_py.modules import RMSNorm, RMSNormTorch
 from torch import dtype as _dtype
 
 
