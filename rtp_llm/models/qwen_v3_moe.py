c6bb4aa7a1dfb75d9bf0d446d9d682361ed148cc
rtp-llm <rtp-llm@alibaba>
2025-08-26 19:16:54 +0800

    fix: handle ImportError in Qwen3Moe model creation

--- rtp_llm/models/qwen_v3_moe.py
+++ rtp_llm/models/qwen_v3_moe.py
@@ -8,7 +8,6 @@ from rtp_llm.model_loader.weight_module import AtomicWeight, WeightModule
 from rtp_llm.models.qwen_v2 import QWenV2, QWenV2Weight
 from rtp_llm.models.qwen_v2_moe import Qwen2Moe, QWenV2MoeWeight
 from rtp_llm.models_py.model_desc.module_base import GptModelBase
-from rtp_llm.models_py.model_desc.qwen3_moe import Qwen3MoeModel
 from rtp_llm.utils.model_weight import (
     CkptWeightInfo,
     W,
@@ -89,8 +88,13 @@ class Qwen3Moe(Qwen2Moe):
         return config
 
     def _create_python_model(self) -> Optional[GptModelBase]:
-        self.py_model = Qwen3MoeModel(self.config, self.weight)
-        return self.py_model
+        try:
+            from rtp_llm.models.qwen_v3_moe import Qwen3MoeModel
+
+            self.py_model = Qwen3MoeModel(self.config, self.weight)
+            return self.py_model
+        except ImportError:
+            return None
 
 
 class Qwen3MoeEagle3Weight(QWenV2Weight):
