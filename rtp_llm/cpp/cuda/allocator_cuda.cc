533aff9bf839db8951360e863c8ff9c253e64fa3
zhangzhi <1801213824@pku.edu.cn>
2025-10-15 20:10:58 +0800

    feat: replace cuda allocator with virtual memory allocator

--- rtp_llm/cpp/cuda/allocator_cuda.cc
+++ rtp_llm/cpp/cuda/allocator_cuda.cc
@@ -89,30 +89,167 @@ void PurePointerCudaAllocator::free(void** ptr) {
     return;
 }
 
-Allocator<AllocatorType::CUDA>::Allocator(int device_id): PurePointerCudaAllocator(device_id) {}
+Allocator<AllocatorType::CUDA>::Allocator(int device_id):
+    PurePointerCudaAllocator(device_id),
+    IVirtualMemAllocator() {    
+}
 
 Allocator<AllocatorType::CUDA>::~Allocator() {
     destroy();
 }
 
+void* Allocator<AllocatorType::CUDA>::mallocPhysical(size_t size) {
+    RTP_LLM_LOG_DEBUG("Malloc Pin memory with size %lu\n", size);
+    auto address = doMallocSync(size);
+    if (!address) {
+        return nullptr;
+    }
+    CUdeviceptr                 dptr = reinterpret_cast<CUdeviceptr>(address);
+    std::lock_guard<std::mutex> lock(lock_);
+    auto it = pointer_mapping_->find(dptr);
+    if (it == pointer_mapping_->end()) {
+        RTP_LLM_LOG_ERROR("Unexpected allocation, pointer mapping missing.");
+        return address;
+    }
+    auto& block = it->second;
+    block.pin   = true;
+    return address;
+}
+
 void* Allocator<AllocatorType::CUDA>::doMalloc(size_t size) {
-    void* ptr = nullptr;
-    check_cuda_value(cudaMalloc(&ptr, (size_t)(ceil(size / 128.)) * 128));
-    return ptr;
+    RTP_LLM_LOG_DEBUG("Malloc virtual memory with size %lu\n", size);
+    size_t                      granularity = 0;
+    std::lock_guard<std::mutex> lock(lock_);
+
+    CUmemAllocationProp prop{};
+    prop.type          = CU_MEM_ALLOCATION_TYPE_PINNED;
+    prop.location.type = CU_MEM_LOCATION_TYPE_DEVICE;
+    prop.location.id   = device_id_;
+
+    check_cuda_value(cuMemGetAllocationGranularity(&granularity, &prop, CU_MEM_ALLOC_GRANULARITY_MINIMUM));
+
+    if (granularity > 0) {
+        const size_t padded_size = (size + granularity - 1) / granularity * granularity;
+
+        RTP_LLM_LOG_DEBUG("Malloc virtual memory with padded size %lu\n", padded_size);
+
+        // 1. 先保留虚拟地址
+        CUdeviceptr dptr = 0;
+        check_cuda_value(cuMemAddressReserve(&dptr, padded_size, 0, 0, 0));
+
+        // 2. 创建物理显存
+        CUmemGenericAllocationHandle handle{};
+        check_cuda_value(cuMemCreate(&handle, padded_size, &prop, 0));
+
+        // 3. 映射
+        check_cuda_value(cuMemMap(dptr, padded_size, 0, handle, 0));
+
+        // 4. 设置访问权限
+        CUmemAccessDesc access{};
+        access.location.type = CU_MEM_LOCATION_TYPE_DEVICE;
+        access.location.id   = device_id_;
+        access.flags         = CU_MEM_ACCESS_FLAGS_PROT_READWRITE;
+        check_cuda_value(cuMemSetAccess(dptr, padded_size, &access, 1));
+
+        (*pointer_mapping_)[dptr] = {false, true, padded_size, handle};
+        return reinterpret_cast<void*>(dptr);
+
+    } else {
+        RTP_LLM_LOG_ERROR("Get system granularity failed\n");
+        return nullptr;
+    }
 }
 
 void* Allocator<AllocatorType::CUDA>::doMallocSync(size_t size) {
-    void* ptr = nullptr;
-    check_cuda_value(cudaMalloc(&ptr, (size_t)(ceil(size / 128.)) * 128));
-    return ptr;
+    return doMalloc(size);
 }
 
 void Allocator<AllocatorType::CUDA>::doFree(void* address) {
+
+    if (!address) {
+        RTP_LLM_LOG_WARNING("Try to free an empty pointer\n");
+        return;
+    }
+
+    CUdeviceptr dptr = reinterpret_cast<CUdeviceptr>(address);
+
+    std::lock_guard<std::mutex> lock(lock_);
+    auto                        it = pointer_mapping_->find(dptr);
+    if (it == pointer_mapping_->end()) {
+        RTP_LLM_LOG_ERROR("Free Pointer Failed, Pointer is not managed by this alloctor %p\n", address);
+        return;
+    }
+
+    RTP_LLM_LOG_DEBUG("Vmem allocator free pointer %p\n", address);
     // tmp sync to avoid memory free before kernel run. cudaFree will not perform any implicit synchronization when the
     // pointer was allocated with cudaMallocAsync or cudaMallocFromPoolAsync
     check_cuda_value(cudaStreamSynchronize(stream_));
-    check_cuda_value(cudaFree(address));
+    const auto& block = it->second;
+    check_cuda_value(cuMemUnmap(dptr, block.size));
+    check_cuda_value(cuMemRelease(block.handle));
+    check_cuda_value(cuMemAddressFree(dptr, block.size));
+
+    RTP_LLM_LOG_DEBUG("Vmem allocator free pointer %p successfully\n", address);
     return;
+}
+
+void Allocator<AllocatorType::CUDA>::unmap() {
+
+    std::lock_guard<std::mutex> lock(lock_);
+    RTP_LLM_LOG_DEBUG("Vmem allocator unmap all allocated buffer\n");
+
+    for (auto& [dptr, block] : *pointer_mapping_) {
+
+        if (block.pin || !block.mapped) {
+            continue;
+        }
+
+        RTP_LLM_LOG_DEBUG("Vmem allocator unmap %p[%lu]\n", dptr, block.size);
+
+        // 1. 解除映射
+        check_cuda_value(cuMemUnmap(dptr, block.size));
+        // 2. 释放对应的物理显存
+        check_cuda_value(cuMemRelease(block.handle));
+        block.mapped = false;
+    }
+}
+
+void Allocator<AllocatorType::CUDA>::map() {
+    std::lock_guard<std::mutex> lock(lock_);
+
+    RTP_LLM_LOG_DEBUG("Vmem allocator map all allocated buffer\n");
+
+    for (auto& [dptr, block] : *pointer_mapping_) {
+
+        if (block.pin || block.mapped) {
+            continue;
+        }
+
+        RTP_LLM_LOG_DEBUG("Vmem allocator map %p[%lu]\n", dptr, block.size);
+
+        size_t              padded_size = block.size;  // 沿用之前对齐后的大小
+        CUmemAllocationProp prop{};
+        prop.type          = CU_MEM_ALLOCATION_TYPE_PINNED;
+        prop.location.type = CU_MEM_LOCATION_TYPE_DEVICE;
+        prop.location.id   = device_id_;
+
+        // 1. 重新创建一块新的物理显存句柄
+        CUmemGenericAllocationHandle new_handle{};
+        check_cuda_value(cuMemCreate(&new_handle, padded_size, &prop, 0));
+
+        // 2. 重新映射到新物理显存
+        check_cuda_value(cuMemMap(dptr, padded_size, 0, new_handle, 0));
+
+        // 3. 重新设置访问权限
+        CUmemAccessDesc access{};
+        access.location.type = CU_MEM_LOCATION_TYPE_DEVICE;
+        access.location.id   = device_id_;
+        access.flags         = CU_MEM_ACCESS_FLAGS_PROT_READWRITE;
+        check_cuda_value(cuMemSetAccess(dptr, padded_size, &access, 1));
+
+        // 4. 更新 mapping 中的 handle
+        block.handle = new_handle;
+    }
 }
 
 Allocator<AllocatorType::CUDA_HOST>::Allocator(int device_id): PurePointerCudaAllocator(device_id) {}
