692560131879b82db0c116f2ca3612a0f5726c26
wht21 <wuhaotian.wht@alibaba-inc.com>
2025-10-24 14:36:41 +0800

    feat: add sub par

--- rtp_llm/cli/serve.py
+++ rtp_llm/cli/serve.py
@@ -27,8 +27,7 @@ class ServeSubcommand(CLISubcommand):
             help="Start the RTP-LLM OpenAI Compatible API server.",
             description="Start the RTP-LLM OpenAI Compatible API server.",
             usage="rtp-llm serve [options]")
-        serve_sub_parser = serve_parser.add_subparsers(required=False, dest="options")
-        init_all_group_args(serve_sub_parser)
+        init_all_group_args(serve_parser)
 
         return serve_parser
 
