87a492041eaa7aa8d10ae49d2c72923c60140878
蝉吟 <liudu.ld@alibaba-inc.com>
2025-10-28 10:12:48 +0800

    feat: singleTypedKVCacheAllocator init

--- rtp_llm/cpp/cache_new/KVCacheAllocator.h
+++ rtp_llm/cpp/cache_new/KVCacheAllocator.h
@@ -6,29 +6,37 @@
 #include <string>
 #include <unordered_map>
 
-#include "rtp_llm/cpp/cache_new/KVCacheGroup.h"
+#include "rtp_llm/cpp/cache_new/CacheConfig.h"
 #include "rtp_llm/cpp/cache_new/types.h"
+#include "rtp_llm/cpp/devices/DeviceBase.h"
+#include "rtp_llm/cpp/cache_new/KVCacheGroup.h"
 
 namespace rtp_llm {
 
+
+
 class KVCacheAllocator {
 public:
-    KVCacheAllocator(const CacheConfig& config, rtp_llm::DeviceBase* device, AllocationType atype = AllocationType::DEVICE);
-    bool init() {}
+    KVCacheAllocator(const CacheConfig& config, rtp_llm::DeviceBase* device, AllocationType atype = AllocationType::DEVICE)
+        : config_(config), device_(device), atype_(atype) {}
+    
+    virtual ~KVCacheAllocator() = default;
 
-    MallocResult malloc(const MallocInfo& malloc_info) {}
+    virtual bool init() = 0;
+    virtual MallocResult malloc(const MallocInfo& malloc_info) = 0;
+    virtual FreeResult free(const FreeInfo& free_info) = 0;
+    virtual InsertResult insertIntoCache(const InsertInfo& insert_info) = 0;
+    virtual BlockAddrInfo convertIndexToAddr(int layer_id, int block_id) const = 0;
+    virtual BlockBufferInfo convertIndexToBuffer(int layer_id, int block_id) const = 0;
+    virtual CacheLayerLayout layerCacheBase() const = 0;
 
-    FreeResult free(const FreeInfo& free_info) {
-        // only consider the scenario of full fallback.
-    }
-    InsertResult insertIntoCache(const InsertInfo& insert_info){
-        // insert blocks in stream that have been cached in block_cache into block_cache   
-    }
-
-    CacheLayerLayout layerCacheBase() const {}
-
-private:
-    std::vector<std::shared_ptr<KVCacheGroup>> kv_cache_groups_;
+protected:
+    CacheConfig config_;
+    rtp_llm::DeviceBase* device_;
+    AllocationType atype_;
+    // std::vector<std::shared_ptr<KVCacheGroup>> kv_cache_groups_;
 };
+
+using KVCacheAllocatorPtr = std::shared_ptr<KVCacheAllocator>;
 
 }  // namespace rtp_llm
