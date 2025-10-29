d5d80e6bd00f9d5b827efce327d8bb961c023922
silu.zsl <silu.zsl@alibaba-inc.com>
2025-10-29 17:23:00 +0800

    feat: raw request support close aux_info

--- rtp_llm/cpp/engine_base/stream/GenerateConfig.h
+++ rtp_llm/cpp/engine_base/stream/GenerateConfig.h
@@ -63,6 +63,7 @@ public:
     bool                          force_sp_accept          = false;
     bool                          return_all_probs         = false;
     bool                          return_softmax_probs     = false;
+    bool                          aux_info                 = false;
     std::vector<std::vector<int>> stop_words_list;
     std::vector<std::string>      stop_words_str;
     bool                          print_stop_words = false;
@@ -76,11 +77,11 @@ public:
     bool             in_think_mode       = false;
     int              max_thinking_tokens = 0;
     std::vector<int> end_think_token_ids;
-    bool             gen_timeline = false;
-    int              profile_step = 3;
-    bool             ignore_eos   = false;
-    bool             reuse_cache  = true;
-    bool             enable_3fs   = true;
+    bool             gen_timeline              = false;
+    int              profile_step              = 3;
+    bool             ignore_eos                = false;
+    bool             reuse_cache               = true;
+    bool             enable_3fs                = true;
     bool             enable_memory_block_cache = true;
     std::string      trace_id;
 
@@ -135,7 +136,8 @@ public:
                      << ", in_think_mode: " << in_think_mode << ", max_thinking_tokens: " << max_thinking_tokens
                      << ", end_think_token_ids: " << vectorToString(end_think_token_ids)
                      << ", gen_timeline: " << gen_timeline << ", profile_step: " << profile_step
-                     << ", reuse_cache: " << reuse_cache << ", enable_3fs: " << enable_3fs << ", enable_memory_block_cache: " << enable_memory_block_cache << "}";
+                     << ", reuse_cache: " << reuse_cache << ", enable_3fs: " << enable_3fs
+                     << ", enable_memory_block_cache: " << enable_memory_block_cache << "}";
         return debug_string.str();
     }
 
