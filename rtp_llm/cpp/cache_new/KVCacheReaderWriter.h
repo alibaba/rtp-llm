e9c3f217748e6aabdea1a49f9ed376efb6a2f44a
lxq <lxq271332@alibaba-inc.com>
2025-10-29 20:22:25 +0800

    feat: update connector

--- rtp_llm/cpp/cache_new/KVCacheReaderWriter.h
+++ rtp_llm/cpp/cache_new/KVCacheReaderWriter.h
@@ -4,26 +4,26 @@
 
 #include "rtp_llm/cpp/cache_new/KVCacheConnector.h"
 #include "rtp_llm/cpp/cache_new/KVCacheGroup.h"
-#include "rtp_llm/cpp/engine_base/stream/GenerateStream.h"
+#include "rtp_llm/cpp/engine_base/resource/BatchKVCacheResource.h"
 
 namespace rtp_llm {
 
-class ReadCallBack {
-public:
-    virtual void onSuccess() = 0;
-    virtual void onFailure() = 0;
-};
+// class ReadCallBack {
+// public:
+//     virtual readDone(bool success) = 0;
+// };
 
-struct WriteCallback {
-public:
-    virtual void onSuccess() = 0;
-    virtual void onFailure() = 0;
-};
+// struct WriteCallback {
+// public:
+//     virtual writeDone(bool success) = 0;
+// };
 
 struct MatchResult {
     std::vector<int64_t> cache_keys;
     std::vector<int> block_ids;
 };
+
+using CallBack = std::function<void(bool)>;
 
 class KVCacheReaderWriter {
 public:
@@ -31,29 +31,40 @@ public:
     virtual ~KVCacheReaderWriter() = default;
 
 public:
-    virtual bool init() = 0;
-    virtual void read(const std::shared_ptr<GenerateStream> &stream, const std::shared_ptr<ReadCallBack& callback) = 0;
-    virtual void readByLayer(const std::shared_ptr<GenerateStream> &stream, int layer_id, const std::shared_ptr<ReadCallBack& callback) = 0;
-    virtual void write(const std::shared_ptr<GenerateStream> &stream, const std::shared_ptr<WriteCallBack& callback) = 0;
-    virtual void writeByLayer(const std::shared_ptr<GenerateStream> &stream, int layer_id, const std::shared_ptr<WriteCallBack& callback) = 0;
-    virtual MatchResult match(const std::shared_ptr<GenerateStream> &stream) = 0;
-    virtual MatchResult prefixMatch(const std::shared_ptr<GenerateStream> &stream) = 0;
+    virtual bool init();
+    virtual void asyncRead(const std::shared_ptr<BatchKVCacheResource> &resource, const CallBack &callback);
+    virtual void asyncReadByLayer(const std::shared_ptr<BatchKVCacheResource> &resource, int layer_id, const CallBack &callback);
+    virtual void asyncWrite(const std::shared_ptr<BatchKVCacheResource> &resource, const CallBack &callback);
+    virtual void asyncWriteByLayer(const std::shared_ptr<BatchKVCacheResource> &resource, int layer_id, const CallBack &callback);
+    virtual MatchResult match(const std::shared_ptr<BatchKVCacheResource> &resource);
+    virtual MatchResult prefixMatch(const std::shared_ptr<BatchKVCacheResource> &resource);
 
 private:
-    std::shared_ptr<KVCacheConnector> connector_;
+    // TODO(LXQ): 还是按照继承的方式实现, 不要放在一个基类中
+    std::shared_ptr<KVCacheConnector> memory_connector_;
+    // TODO(LXQ): 不应该感知group, 只感知layer
     std::vector<std::shared_ptr<KVCacheGroup>> groups_;
 };
 
-// for memory cache
-class MemoryKVCacheReaderWriter : public KVCacheReaderWriter {};
+// // for memory cache
+// class MemoryKVCacheReaderWriter : public KVCacheReaderWriter {
+// public:
+//     void bool match(const int64_t& cache_key) override {
+//         return connector_->match(cache_key);
+//     }
+//     void write(const std::vector<int64_t>& cache_keys, const std::vector<int>& block_indices) override {
+//         // 1. connector->match(cache_keys);
+//         // 2. if not fully match, reader_writer_->write(cache_keys, block_indices);
+//     }
+// };
 
-// for pd sep
-class P2PKVCacheReaderWriter : public KVCacheReaderWriter {};
+// // for pd sep
+// class P2PKVCacheReaderWriter : public KVCacheReaderWriter {};
 
-// for remote cache
-class RemoteKVCacheReaderWriter : public KVCacheReaderWriter {};
+// // for remote cache
+// class RemoteKVCacheReaderWriter : public KVCacheReaderWriter {};
 
-// for block meta data, eg: first token, loss etc
-class BlockMetaDataReaderWriter : public KVCacheReaderWriter {};
+// // for block meta data, eg: first token, loss etc
+// class BlockMetaDataReaderWriter : public KVCacheReaderWriter {};
 
 }  // namespace rtp_llm
