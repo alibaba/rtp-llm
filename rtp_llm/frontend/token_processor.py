a6f49dc2c2439ab1d6e4707cd842a617bdcd069a
silu.zsl <silu.zsl@alibaba-inc.com>
2025-10-29 17:23:00 +0800

    feat: raw request support close aux_info

--- rtp_llm/frontend/token_processor.py
+++ rtp_llm/frontend/token_processor.py
@@ -124,6 +124,7 @@ class TokenProcessorPerStream:
                         (self.ouput_tokens_list[i], tokens), axis=1
                     )
                 tokens = self.ouput_tokens_list[i]
+
             tokens = remove_padding_eos_with_numpy(
                 tokens, self.special_tokens.eos_token_id
             ).tolist()
