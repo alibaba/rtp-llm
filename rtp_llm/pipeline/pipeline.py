0f1a5e396de5e26d49c8699119335a0ef00d2d88
silu.zsl <silu.zsl@alibaba-inc.com>
2025-10-30 16:46:41 +0800

    feat: optimize python decode_tokens

--- rtp_llm/pipeline/pipeline.py
+++ rtp_llm/pipeline/pipeline.py
@@ -26,9 +26,10 @@ from rtp_llm.utils.multimodal_util import MultimodalInput
 from rtp_llm.utils.time_util import current_time_ms
 from rtp_llm.utils.util import AtomicCounter
 from rtp_llm.utils.word_util import (
+    batch_remove_padding_eos,
     get_stop_word_slices,
     match_stop_words,
-    remove_padding_eos,
+    remove_padding_eos_with_numpy,
     truncate_response_with_stop_words,
     truncate_token_with_stop_word_id,
 )
@@ -232,7 +233,75 @@ class Pipeline(object):
 
         return text, token_buffer
 
-    def decode_tokens(
+    def decode_non_incremental_tokens(
+        self,
+        generate_config: GenerateConfig,
+        generate_outputs: GenerateOutputs,
+        stop_word_str_list: List[str],
+        stop_word_str_slices: List[str],
+        stop_word_ids: List[int],
+        stop_word_id_slices: List[int],
+        **kwargs: Any
+    ) -> Tuple[List[str], List[int]]:
+        all_output_ids = torch.cat(
+            [go.output_ids for go in generate_outputs.generate_outputs], dim=0
+        )
+        all_output_ids_np = all_output_ids.cpu().numpy()
+        token_lists_to_decode = []
+        output_lens = []
+        if not generate_config.ignore_eos:
+            processed_tokens_np_list = batch_remove_padding_eos(
+                all_output_ids_np, self._special_tokens.eos_token_id
+            )
+            tokens_lists_for_decode_input = [
+                tokens.tolist() for tokens in processed_tokens_np_list
+            ]
+        else:
+            tokens_lists_for_decode_input = all_output_ids_np.tolist()
+        for i, generate_output in enumerate(generate_outputs.generate_outputs):
+            tokens_list = tokens_lists_for_decode_input[i]
+            output_lens.append(len(tokens_list))
+            processed_tokens = self.process_stop_id(
+                generate_config,
+                generate_output,
+                tokens_list,
+                stop_word_ids,
+                stop_word_id_slices,
+            )
+            token_lists_to_decode.append(processed_tokens)
+
+        decoded_batch = self.tokenizer.batch_decode(
+            token_lists_to_decode,
+            skip_special_tokens=generate_config.skip_special_tokens,
+            **kwargs
+        )
+        newly_decoded_texts = [text.rstrip("\uFFFD") for text in decoded_batch]
+        all_texts = newly_decoded_texts
+
+        final_texts = []
+        for i in range(len(all_texts)):
+            processed_text, _ = self.process_stop_str(
+                generate_config,
+                generate_outputs.generate_outputs[i],
+                newly_decoded_texts[i],
+                all_texts[i],
+                stop_word_str_list,
+                stop_word_str_slices,
+                "",
+                **kwargs
+            )
+
+            if generate_config.out_prefix:
+                processed_text = generate_config.out_prefix + processed_text
+
+            final_texts.append(processed_text)
+
+        return (
+            final_texts,
+            output_lens,
+        )
+
+    def decode_incremental_tokens(
         self,
         generate_config: GenerateConfig,
         generate_outputs: GenerateOutputs,
@@ -244,90 +313,59 @@ class Pipeline(object):
         token_buffers: List[str],
         ouput_tokens_list: List[torch.Tensor],
         **kwargs: Any
-    ) -> Tuple[
-        List[str], List[int], List[DecodingState], List[str], List[torch.Tensor]
-    ]:
-        texts = []
-        all_texts = []
-        output_lens = []
-        if len(decoding_states) == 0:
-            if not generate_config.has_num_beams() and generate_config.is_streaming:
-                decoding_states = [
-                    DecodingState()
-                    for _ in range(len(generate_outputs.generate_outputs))
-                ]
-            else:
-                # num_beams不等于1的情况下，不能进行增量decode，因为过去的token id会变化
-                decoding_states = [None] * len(generate_outputs.generate_outputs)
-
+    ) -> Tuple[List[str], List[int]]:
+        """处理增量解码的逻辑。"""
+        num_outputs = len(generate_outputs.generate_outputs)
         if len(token_buffers) == 0:
-            token_buffers = [""] * len(generate_outputs.generate_outputs)
+            token_buffers = [""] * num_outputs
+
+        if len(decoding_states) == 0:
+            decoding_states = [DecodingState() for _ in range(num_outputs)]
 
         if len(ouput_tokens_list) == 0:
             ouput_tokens_list = [
-                torch.empty(0, dtype=torch.int32)
-                for _ in range(len(generate_outputs.generate_outputs))
+                torch.empty(0, dtype=torch.int32) for _ in range(num_outputs)
             ]
 
-        is_incremental = bool(decoding_states and decoding_states[0] is not None)
-
-        # Preprocess
-        token_lists_to_decode = []
+        newly_decoded_texts = []
+        all_texts = []
         output_lens = []
+        ignore_eos = generate_config.ignore_eos
         for i, generate_output in enumerate(generate_outputs.generate_outputs):
-            if not generate_config.has_num_beams():
-                ouput_tokens_list[i] = torch.cat(
-                    (ouput_tokens_list[i], generate_output.output_ids), dim=1
-                )
-                generate_output.output_ids = ouput_tokens_list[i]
-
-            tokens = generate_output.output_ids
-            if not generate_config.ignore_eos:
-                tokens = remove_padding_eos(tokens, self._special_tokens.eos_token_id)
+            ouput_tokens_list[i] = torch.cat(
+                (ouput_tokens_list[i], generate_output.output_ids), dim=1
+            )
+            full_tokens_tensor = ouput_tokens_list[i]
+            tokens_np = full_tokens_tensor.cpu().numpy().flatten()
+            if not ignore_eos:
+                tokens_list = remove_padding_eos_with_numpy(
+                    tokens_np, self._special_tokens.eos_token_id
+                ).tolist()
             else:
-                tokens = tokens.reshape(-1)
+                tokens_list = tokens_np.tolist()
 
-            output_lens.append(tokens.nelement())
+            output_lens.append(len(tokens_list))
 
             processed_tokens = self.process_stop_id(
                 generate_config,
                 generate_output,
-                tokens.tolist(),
+                tokens_list,
                 stop_word_ids,
                 stop_word_id_slices,
             )
-            token_lists_to_decode.append(processed_tokens)
-
-        # Decode
-        newly_decoded_texts: List[str]
-        all_texts: List[str]
-
-        if is_incremental:
-            newly_decoded_texts = []
-            all_texts = []
-            for i, tokens in enumerate(token_lists_to_decode):
-                new_text = IncrementDecodingUtils.detokenize_incrementally(
-                    self.tokenizer, tokens, decoding_states[i]
-                )
-                decoding_states[i].all_text += new_text
-
-                text_to_return = (
-                    new_text
-                    if generate_config.return_incremental
-                    else decoding_states[i].all_text
-                )
-                newly_decoded_texts.append(text_to_return)
-                all_texts.append(decoding_states[i].all_text)
-        else:
-            decoded_batch = self.tokenizer.batch_decode(
-                token_lists_to_decode,
-                skip_special_tokens=generate_config.skip_special_tokens,
-                **kwargs
+            new_text = IncrementDecodingUtils.detokenize_incrementally(
+                self.tokenizer, processed_tokens, decoding_states[i]
             )
-            newly_decoded_texts = [text.rstrip("\uFFFD") for text in decoded_batch]
-            all_texts = newly_decoded_texts
+            decoding_states[i].all_text += new_text
 
-        # PostProcess
+            text_to_return = (
+                new_text
+                if generate_config.return_incremental
+                else decoding_states[i].all_text
+            )
+            newly_decoded_texts.append(text_to_return)
+            all_texts.append(decoding_states[i].all_text)
+
         final_texts = []
         for i in range(len(all_texts)):
             processed_text, token_buffers[i] = self.process_stop_str(
@@ -349,7 +387,7 @@ class Pipeline(object):
         return (
             final_texts,
             output_lens,
-            decoding_states,
+            decoding_stales,
             token_buffers,
             ouput_tokens_list,
         )
@@ -405,24 +443,41 @@ class Pipeline(object):
                 generate_outputs.generate_outputs
             )
             begin_time = current_time_ms()
-            (
-                generate_texts,
-                output_lens,
-                decoding_states,
-                token_buffers,
-                ouput_tokens_list,
-            ) = self.decode_tokens(
-                generate_config,
-                generate_outputs_cache,
-                stop_word_strs,
-                stop_word_str_slices,
-                stop_word_ids,
-                stop_word_id_slices,
-                decoding_states,
-                token_buffers,
-                ouput_tokens_list,
-                **kwargs
+            is_incremental = (
+                not generate_config.has_num_beams() and generate_config.is_streaming
             )
+            if is_incremental:
+                (
+                    generate_texts,
+                    output_lens,
+                    decoding_states,
+                    token_buffers,
+                    ouput_tokens_list,
+                ) = self.decode_incremental_tokens(
+                    generate_config,
+                    generate_outputs_cache,
+                    stop_word_strs,
+                    stop_word_str_slices,
+                    stop_word_ids,
+                    stop_word_id_slices,
+                    decoding_states,
+                    token_buffers,
+                    ouput_tokens_list,
+                    **kwargs
+                )
+            else:
+                (
+                    generate_texts,
+                    output_lens,
+                ) = self.decode_non_incremental_tokens(
+                    generate_config,
+                    generate_outputs_cache,
+                    stop_word_strs,
+                    stop_word_str_slices,
+                    stop_word_ids,
+                    stop_word_id_slices,
+                    **kwargs
+                )
 
             kmonitor.report(
                 GaugeMetrics.POST_PIPELINE_RT_METRIC, current_time_ms() - begin_time
@@ -438,6 +493,9 @@ class Pipeline(object):
                     GaugeMetrics.FT_ITERATE_COUNT_METRIC,
                     generate_outputs_cache.generate_outputs[0].aux_info.iter_count,
                 )
-                for l in output_lens:
-                    kmonitor.report(GaugeMetrics.OUTPUT_TOKEN_SIZE_METRIC, l)
+                if len(output_lens) > 0:
+                    kmonitor.report(
+                        GaugeMetrics.OUTPUT_TOKEN_SIZE_METRIC,
+                        sum(output_lens) / len(output_lens),
+                    )
                 break
