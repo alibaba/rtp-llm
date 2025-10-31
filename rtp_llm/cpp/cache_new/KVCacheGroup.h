42938c1c2a95e02996df4861841833854c0da3d3
xinfei.sxf <xinfei.sxf@alibaba-inc.com>
2025-10-31 11:17:04 +0800

    refactor kv cache group api, add ut

--- rtp_llm/cpp/cache_new/KVCacheGroup.h
+++ rtp_llm/cpp/cache_new/KVCacheGroup.h
@@ -8,49 +8,67 @@
 
 #include <torch/torch.h>
 
+#include "rtp_llm/cpp/cache_new/types.h"
 #include "rtp_llm/cpp/cache_new/CacheConfig.h"
 #include "rtp_llm/cpp/cache_new/BlockPool.h"
+#include "rtp_llm/cpp/cache_new/BlockCacheV1.h"
 #include "rtp_llm/cpp/core/Buffer.h"
 
 namespace rtp_llm {
 
+// first call:
+// match, alloc, insert to cache
+
+// other call:
+// insert to cache, removeSkippedBlocks, alloc
+
+// query finish:
+// free
+
 class KVCacheGroup {
 public:
-    KVCacheGroup(const std::vector<int>& layer_ids, 
-                 std::shared_ptr<KVCacheSpec> group_spec,
-                 BlockPoolPtr block_pool)
-        : layer_ids_(layer_ids), group_spec_(std::move(group_spec)), 
-          block_pool_(block_pool) {}
+    // TODO, KVCacheSpec 也包含了layer ids.
+    KVCacheGroup(const LayerIdsType& layer_ids, std::shared_ptr<KVCacheSpec> group_spec, BlockPoolPtr block_pool):
+        layer_ids_(layer_ids),
+        group_spec_(std::move(group_spec)),
+        block_pool_(block_pool),
+        block_cache_(block_pool_->blockCache()),
+        seq_size_per_block_(group_spec_->seq_size_per_block) {}
 
     virtual ~KVCacheGroup() = default;
 
     virtual bool init() = 0;
-    virtual std::vector<BlockIdxType> alloc(int needed_blocks) = 0;
-    virtual MatchResult match(std::vector<CacheKeyType> cache_keys) const = 0;
-    virtual void free(std::vector<BlockIdxType> block_indices) = 0;
-    virtual void insertIntoCache(std::vector<CacheKeyType> cache_keys,
-                                 std::vector<BlockIdxType> block_indices,
-                                 std::vector<std::vector<float>> loss) = 0;
-    virtual std::unordered_map<int, torch::Tensor> layerCacheBase() const = 0;
-    virtual BlockAddrInfo convertIndexToAddr(int layer_id, int block_id) const = 0;
-    virtual BlockBufferInfo convertIndexToBuffer(int layer_id, int block_id) const = 0;
+    // TODO, add error status
+    virtual void malloc(CacheKeysType& cache_keys, BlockIndicesType& block_indices, int seq_len) = 0;
+    // TODO, add error status for remote.
+    virtual MatchResult match(CacheKeysType& cache_keys)                                                       = 0;
+    virtual void        free(const BlockIndicesType& block_indices)                                            = 0;
+    virtual void insertIntoCache(CacheKeysType& cache_keys, BlockIndicesType& block_indices, bool is_resident) = 0;
+    virtual void removeSkippedBlocks(BlockIndicesType& block_indices)                                          = 0;
+    virtual int  needBlocksNum(int seq_len, int current_blocks) const                                          = 0;
 
-    virtual size_t freeBlockNums() const = 0;
-    virtual bool evict(int need_evict_len) = 0;
-    virtual int seqSizePerBlock() const = 0;
+    virtual std::unordered_map<int, torch::Tensor> layerCacheBase() const                                 = 0;
+    virtual BlockAddrInfo                          convertIndexToAddr(int layer_id, int block_id) const   = 0;
+    virtual BlockBufferInfo                        convertIndexToBuffer(int layer_id, int block_id) const = 0;
+
+    virtual size_t freeBlockNums() const     = 0;
+    virtual bool   evict(int need_evict_len) = 0;
+
+    int seqSizePerBlock() const {
+        return seq_size_per_block_;
+    }
 
 protected:
-    std::vector<int> layer_ids_;
+    LayerIdsType                 layer_ids_;
     std::shared_ptr<KVCacheSpec> group_spec_;
-    // BlockCachePtr block_cache_;
-    BlockPoolPtr block_pool_;
+    BlockPoolPtr                 block_pool_;
+    BlockCacheV1Ptr              block_cache_;
 
-    int seq_size_per_block_;
+    int                                    seq_size_per_block_;
     std::unordered_map<int, torch::Tensor> gloabl_layer_to_kv_tensors;
-    std::unordered_map<int, int> gloabl_layer_to_local_layer;
+    std::unordered_map<int, int>           gloabl_layer_to_local_layer;
 };
 
 using KVCacheGroupPtr = std::shared_ptr<KVCacheGroup>;
 
 }  // namespace rtp_llm
-
