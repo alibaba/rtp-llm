e6f5e4d9b2cf132d9fd3fd68f7c9f401f9f2be80
xinfei.sxf <xinfei.sxf@alibaba-inc.com>
2025-10-31 11:17:04 +0800

    refactor kv cache group api, add ut

--- rtp_llm/cpp/cache_new/HybridPoolKVCacheAllocator.h
+++ rtp_llm/cpp/cache_new/HybridPoolKVCacheAllocator.h
@@ -17,8 +17,8 @@
 
 // class HybridPoolKVCacheAllocator public KVCacheAllocator {
 // public:
-//     HybridPoolKVCacheAllocator(const CacheConfig& config, rtp_llm::DeviceBase* device, AllocationType atype = AllocationType::DEVICE);
-//     bool init() {
+//     HybridPoolKVCacheAllocator(const CacheConfig& config, rtp_llm::DeviceBase* device, AllocationType atype =
+//     AllocationType::DEVICE); bool init() {
 //         // 1. build block_pool for each kind of attentions.
 //         // 2. build kv_cache_groups_ by CacheConfig for each kind of attentions with its block_pool
 //     };
@@ -35,24 +35,24 @@
 //         // only consider the scenario of full fallback.
 //     }
 //     InsertResult insertIntoCache(const InsertInfo& insert_info){
-//         // insert blocks in stream that have been cached in block_cache into block_cache   
+//         // insert blocks in stream that have been cached in block_cache into block_cache
 //     };
 
 //     CacheLayerLayout layerCacheBase() const {
-        
+
 //     };
 
-// private:    
+// private:
 //     MallocResult mallocWithCache(const MallocInfo& malloc_info) {
 //         // MallocResult malloc_result;
 //         // int full_reuse_len = INT_MAX;
 //         // std::vector<MatchResult> match_results;
 //         // auto cache_keys = malloc_info.stream->kvCache().cache_keys;
 
-//         // for (auto& kv_cache_group : kv_cache_groups_) {   
+//         // for (auto& kv_cache_group : kv_cache_groups_) {
 //         //     auto match_result = kv_cache_group->match(cache_keys);
 //         //     match_results.push_back(match_result);
-            
+
 //         //     if (kv_cache_group->type() == KVCacheType::FULL && match_result.reuse_length < full_reuse_len) {
 //         //         full_reuse_len = match_result.reuse_length;
 //         //     }
@@ -65,13 +65,13 @@
 //         //         break;
 //         //     }
 //         // }
-        
+
 //         // for (auto& match_result : match_results) {
 //         //     // update stream's BatchKVCacheResource
 //         // }
 
 //         // for (auto& kv_cache_group : kv_cache_groups_) {
-//         //     auto block_indices = kv_cache_group->alloc(cache_keys, reuse_len);
+//         //     auto block_indices = kv_cache_group->malloc(cache_keys, reuse_len);
 //         //     // update stream's BatchKVCacheResource
 //         // }
 
@@ -81,7 +81,7 @@
 //     MallocResult mallocSimple(const MallocInfo& malloc_info) {
 //         // for(auto& kv_cache_group : kv_cache_groups_) {
 //         //     // cache_keys = cache_keys that are not allocated blocks;
-//         //     auto block_indices = kv_cache_group->alloc(cache_keys, 0);
+//         //     auto block_indices = kv_cache_group->malloc(cache_keys, 0);
 //         //     // update stream's BatchKVCacheResource
 //         //     if (kv_cache_group->type() == KVCacheType::LINEAR) {
 //         //         // insert previous blocks into block_cache and free it
@@ -93,4 +93,3 @@
 // using HybridLayerKVCacheAllocatorPtr = std::shared_ptr<HybridLayerKVCacheAllocatorPtr>;
 
 // }  // namespace rtp_llm
-
