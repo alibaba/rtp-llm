21cc50aeff3eba3f5721325c58a8e77e7f461a53
zw193905 <zw193905@alibaba-inc.com>
2025-11-06 14:08:03 +0800

    feat - tmp add code

--- rtp_llm/models_py/modules/fmha.py
+++ rtp_llm/models_py/modules/fmha.py
@@ -12,6 +12,12 @@ from rtp_llm.models_py.modules.mla import (
     MlaRotaryEmbeddingOp,
     TrtV2PrefillAttentionOp,
 )
+from rtp_llm.models_py.modules.flashinfer_python import (
+    FlashInferPythonParams,
+    FlashInferPythonPrefillOp,
+    FlashInferPythonDecodeOp
+)
+
 from rtp_llm.ops import FMHAType
 from rtp_llm.ops.compute_ops import KVCache, ParamsBase, PyAttentionInputs
 from rtp_llm.utils.model_weight import W
@@ -109,6 +115,128 @@ DECODE_MHA_IMPS: List[type[FMHADecodeImplBase]] = []
 
 PREFILL_MLA_IMPS: List[type[FMHAPrefillImplBase]] = []
 DECODE_MLA_IMPS: List[type[FMHADecodeImplBase]] = []
+
+try:
+    from rtp_llm.ops.compute_ops import FlashInferPrefillOp, FusedRopeKVCachePrefillOp
+
+    class FlashInferPythonPrefillImpl(FMHAPrefillImplBase):
+
+        def __init__(
+            self, config: GptInitModelParameters, attn_inputs: PyAttentionInputs
+        ) -> None:
+            super().__init__(
+                FlashInferPythonPrefillOp(config.gpt_init_params),
+                FusedRopeKVCachePrefillOp(config.gpt_init_params),
+                attn_inputs,
+            )
+            self.support_ = self.support_ and (config.use_mla == False)
+
+        @staticmethod
+        def fmha_type() -> FMHAType:
+            return FMHAType.FLASH_INFER
+
+        def support_cuda_graph(self) -> bool:
+            return True
+
+    PREFILL_MHA_IMPS.append(FlashInferPythonPrefillImpl)
+    
+    class FlashInferPrefillImpl(FMHAPrefillImplBase):
+
+        def __init__(
+            self, config: GptInitModelParameters, attn_inputs: PyAttentionInputs
+        ) -> None:
+            super().__init__(
+                FlashInferPrefillOp(config.gpt_init_params),
+                FusedRopeKVCachePrefillOp(config.gpt_init_params),
+                attn_inputs,
+            )
+            self.support_ = self.support_ and (config.use_mla == False)
+
+        @staticmethod
+        def fmha_type() -> FMHAType:
+            return FMHAType.FLASH_INFER
+
+        def support_cuda_graph(self) -> bool:
+            return True
+
+    PREFILL_MHA_IMPS.append(FlashInferPrefillImpl)
+
+    class MlaFlashInferPrefillImpl(FMHAPrefillImplBase):
+
+        def __init__(
+            self,
+            config: GptInitModelParameters,
+            attn_inputs: PyAttentionInputs,
+            weights: List[Dict[str, torch.Tensor]],
+            cos_sin_cache: torch.Tensor,
+        ) -> None:
+
+            super().__init__(
+                MlaFlashInferPrefillOp(
+                    config,
+                    config.head_num,
+                    config.kv_lora_rank,
+                    config.rope_head_dim,
+                    config.nope_head_dim,
+                    config.seq_size_per_block,
+                    config.softmax_extra_scale,
+                    config.use_mla,
+                    weights,
+                ),
+                # TrtV2PrefillAttentionOp(
+                #     config,
+                #     config.head_num,
+                #     config.kv_lora_rank,
+                #     config.rope_head_dim,
+                #     config.nope_head_dim,
+                #     config.use_mla,
+                #     weights,
+                # ),
+                MlaRotaryEmbeddingOp(
+                    head_size=config.nope_head_dim,
+                    cos_sin_cache=cos_sin_cache,
+                    kv_lora_rank=config.kv_lora_rank,
+                    rope_head_dim=config.rope_head_dim,
+                    token_per_block=config.seq_size_per_block,
+                    is_neox_style=False,
+                ),
+                attn_inputs,
+            )
+
+        @staticmethod
+        def fmha_type() -> FMHAType:
+            return FMHAType.FLASH_INFER
+
+        def forward(
+            self,
+            q: torch.Tensor,
+            compressed_kv: torch.Tensor,
+            k_pe: torch.Tensor,
+            kv_cache: Optional[KVCache],
+            layer_id: int,
+        ):
+            assert self.rope_kvcache_impl is not None and self.rope_params is not None
+            q_pe = q[:, :, self.fmha_impl.qk_nope_head_dim :]
+            self.rope_kvcache_impl.forward(
+                q_pe, k_pe, compressed_kv, self.rope_params, kv_cache
+            )
+
+            if (
+                self.attn_inputs.is_prefill
+                and self.attn_inputs.cache_store_inputs
+                and self.write_cache_store_impl is not None
+            ):
+                self.write_cache_store_impl(kv_cache)
+            assert self.fmha_impl is not None
+            res = self.fmha_impl.forward(
+                q, compressed_kv, k_pe, self.fmha_params, layer_id
+            )
+            return res
+
+    PREFILL_MLA_IMPS.append(MlaFlashInferPrefillImpl)
+
+except ImportError:
+    logging.info("FlashInferPrefillImpl not available, skipped.")
 
 try:
     from rtp_llm.ops.compute_ops import FlashInferPrefillOp, FusedRopeKVCachePrefillOp
@@ -214,6 +342,27 @@ except ImportError:
 
 try:
     from rtp_llm.ops.compute_ops import FlashInferDecodeOp, FusedRopeKVCacheDecodeOp
+
+    class FlashInferPythonDecodeImpl(FMHADecodeImplBase):
+
+        def __init__(
+            self, config: GptInitModelParameters, attn_inputs: PyAttentionInputs
+        ) -> None:
+            super().__init__(
+                FlashInferPythonDecodeOp(config.gpt_init_params),
+                FusedRopeKVCacheDecodeOp(config.gpt_init_params),
+                attn_inputs,
+            )
+            self.support_ = self.support_ and (config.use_mla == False)
+
+        @staticmethod
+        def fmha_type() -> FMHAType:
+            return FMHAType.FLASH_INFER
+
+        def support_cuda_graph(self) -> bool:
+            return True
+
+    DECODE_MHA_IMPS.append(FlashInferPythonDecodeImpl)
 
     class FlashInferDecodeImpl(FMHADecodeImplBase):
 
