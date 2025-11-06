92be8d1f8ad1bf809960833a8c620f57625aecfa
凌葭 <lvjiang.lj@alibaba-inc.com>
2025-11-06 15:14:13 +0800

    feat:support load huggingface model in standalone mode and improve qwen3_test

--- rtp_llm/models_py/standalone/test/qwen3_test.py
+++ rtp_llm/models_py/standalone/test/qwen3_test.py
@@ -1,23 +1,24 @@
-# if you haven't installed the rtp-llm wheel, you need to add the dev directory to the python path
 import sys
 from pathlib import Path
-
-rtp_opensouce_path = Path(__file__).resolve().parent.parent.parent.parent.parent
-print(f"rtp_opensouce_path: {rtp_opensouce_path}")
-sys.path.append(str(rtp_opensouce_path))
 
 # only need to import the rtp_simple_model
 from rtp_llm.models_py.standalone.rtp_simple_model import RtpSimplePyModel
 
-# create simplemodel
+# if you want to run the script directly and haven't installed the rtp-llm wheel, you need to add the dev directory to the python path
+# rtp_opensouce_path = Path(__file__).resolve().parent.parent.parent.parent.parent
+# print(f"rtp_opensouce_path: {rtp_opensouce_path}")
+# sys.path.append(str(rtp_opensouce_path))
+
+
+# create simplemodel, support load model from huggingface
 my_model = RtpSimplePyModel(
     model_type="qwen_3",
-    model_path="/home/lvjiang.lj/models/qwen3-0.6B",
+    model_path_or_name="Qwen/Qwen3-0.6B",
     act_type="FP16",
 )
 
 # generate text and token_ids
-messages = [{"role": "user", "content": "你好，请用较长篇幅介绍一下自己"}]
+messages = [{"role": "user", "content": "你好，请用较长篇幅介绍自己"}]
 input_text = my_model.tokenizer.apply_chat_template(
     messages, tokenize=False, add_generation_prompt=True, enable_thinking=False
 )
