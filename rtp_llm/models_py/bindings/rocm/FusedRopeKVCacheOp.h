1d6aa85d2c3685f43809b11a96bcce5093932245
moudi.mou <moudi.mou@alibaba-inc.com>
2025-11-10 19:41:12 +0800

    feat: [rocm] support variable-length sequence multi-batch inference

--- rtp_llm/models_py/bindings/rocm/FusedRopeKVCacheOp.h
+++ rtp_llm/models_py/bindings/rocm/FusedRopeKVCacheOp.h
@@ -10,11 +10,12 @@ namespace rtp_llm {
 class FusedRopeKVCachePrefillOp: public FMHARocmBase {
 public:
     FusedRopeKVCachePrefillOp(const GptInitParameter& gpt_init_parameter);
-    CKAttnPtr                                               prepare(torch_ext::PyAttentionInputs attn_inputs);
-    std::tuple<torch::Tensor, torch::Tensor, torch::Tensor> forward(const torch::Tensor&              qkv,
-                                                                    FMHAType                          fmha_type,
-                                                                    std::optional<torch_ext::KVCache> kv_cache_base,
-                                                                    const CKAttnPtr&                  params);
+    CKAttnPtr prepare(torch_ext::PyAttentionInputs attn_inputs);
+    // std::tuple<torch::Tensor, torch::Tensor, torch::Tensor, torch::Tensor>
+    torch::Tensor forward(const torch::Tensor&              qkv,
+                          FMHAType                          fmha_type,
+                          std::optional<torch_ext::KVCache> kv_cache_base,
+                          const CKAttnPtr&                  params);
 };
 
 class FusedRopeKVCacheDecodeOp: public FMHARocmBase {
