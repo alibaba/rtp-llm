3445b0427cd81f46c8bb4e4891d0593daadc3ca8
xiebaijie <xiebaijie.xbj@alibaba-inc.com>
2025-11-12 18:07:40 +0800

    fix: remove old AMD sampler

--- rtp_llm/cpp/devices/rocm_impl/ROCmDevice.cc
+++ rtp_llm/cpp/devices/rocm_impl/ROCmDevice.cc
@@ -153,7 +153,6 @@ ROCmDevice::~ROCmDevice() {
     ROCM_CHECK(hipStreamDestroy(assist_stream_));
     ROCM_CHECK(hipblasDestroy(hipblas_handle_));
     ROCM_CHECK(hipblasLtDestroy(hipblaslt_handle_));
-    curandstate_buf_.reset();
 
     if (stream_ != nullptr) {
         ROCM_CHECK(hipStreamDestroy(stream_));
@@ -175,7 +174,6 @@ ROCmDevice::~ROCmDevice() {
 
 void ROCmDevice::init() {
     DeviceBase::init();
-    RTP_LLM_LOG_INFO("max batch size: %d", init_params_.max_batch_size);
 #ifdef ENABLE_DEEP_EP
     if (init_params_.use_deepep_moe) {
         if (!initDeepEPBuffer()) {
