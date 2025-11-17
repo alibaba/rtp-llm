e0504c194dc0f20a85380d71eaaddbaeb69ad2c6
serina.wzq <serina.wzq@alibaba-inc.com>
2025-11-17 12:21:11 +0800

    refactor: refactor fp8 cutlass groupgemm

--- rtp_llm/cpp/cuda/cutlass/cutlass_kernels/gemm_configs.h
+++ rtp_llm/cpp/cuda/cutlass/cutlass_kernels/gemm_configs.h
@@ -16,14 +16,11 @@
 
 #pragma once
 
-namespace tensorrt_llm
-{
-namespace cutlass_extensions
-{
+namespace tensorrt_llm {
+namespace cutlass_extensions {
 // Note: The shapes are in the format MxNxK. The K shape of the runtime config MUST match the K shape
 //       in the kernel layout details when doing weight only quantization.
-enum class CutlassTileConfig
-{
+enum class CutlassTileConfig {
     // Signals that we should run heuristics do choose a config
     Undefined,
 
@@ -71,17 +68,13 @@ enum class CutlassTileConfig
     CtaShape256x128x128_WarpShape64x64x128,
 };
 
-
-
-enum class SplitKStyle
-{
+enum class SplitKStyle {
     NO_SPLIT_K,
     SPLIT_K_SERIAL,
     // SPLIT_K_PARALLEL // Not supported yet
 };
 
-enum class CutlassTileConfigSM90
-{
+enum class CutlassTileConfigSM90 {
     // Signals that we should run heuristics do choose a config
     Undefined,
 
@@ -104,22 +97,22 @@ enum class CutlassTileConfigSM90
 
     // CTA configs for M=128
     CtaShape256x128x128B,
+
+    CtaShape128x16x256B,
 };
 
-enum class MainloopScheduleType
-{
-    AUTO // Automatically selects between pingpong and cooperative schedules on Hopper. On older architectures, this
-         // defaults to the "legacy" main loop schedule.
+enum class MainloopScheduleType {
+    AUTO  // Automatically selects between pingpong and cooperative schedules on Hopper. On older architectures, this
+          // defaults to the "legacy" main loop schedule.
 };
 
-enum class EpilogueScheduleType
-{
-    AUTO // Automatically chooses an epilogue schedule compatible with the selected main loop schedule for Hopper. For
-         // architectures older than hopper, the epilogue is always performed by the same thread block as the main loop.
+enum class EpilogueScheduleType {
+    AUTO  // Automatically chooses an epilogue schedule compatible with the selected main loop schedule for Hopper. For
+          // architectures older than hopper, the epilogue is always performed by the same thread block as the main
+          // loop.
 };
 
-enum class ClusterShape
-{
+enum class ClusterShape {
     ClusterShape_1x1x1,
     ClusterShape_2x1x1,
     ClusterShape_1x2x1,
@@ -128,61 +121,54 @@ enum class ClusterShape
     ClusterShape_8x1x1
 };
 
-struct CutlassGemmConfig
-{
-    enum CandidateConfigTypeParam : int
-    {
-        NONE = 0,
-        WEIGHT_ONLY = 1u << 0,
-        SIMT_ONLY = 1u << 1,
-        INT8_ONLY = 1u << 2,
-        HOPPER = 1u << 3,
+struct CutlassGemmConfig {
+    enum CandidateConfigTypeParam : int {
+        NONE         = 0,
+        WEIGHT_ONLY  = 1u << 0,
+        SIMT_ONLY    = 1u << 1,
+        INT8_ONLY    = 1u << 2,
+        HOPPER       = 1u << 3,
         GROUPED_GEMM = 1u << 4,
-        FP8_ONLY = 1u << 5,
+        FP8_ONLY     = 1u << 5,
     };
 
-    CutlassTileConfig tile_config = CutlassTileConfig::ChooseWithHeuristic;
-    SplitKStyle split_k_style = SplitKStyle::NO_SPLIT_K;
-    int split_k_factor = -1;
-    int stages = -1;
+    CutlassTileConfig tile_config    = CutlassTileConfig::ChooseWithHeuristic;
+    SplitKStyle       split_k_style  = SplitKStyle::NO_SPLIT_K;
+    int               split_k_factor = -1;
+    int               stages         = -1;
 
-    CutlassTileConfigSM90 tile_config_sm90 = CutlassTileConfigSM90::ChooseWithHeuristic;
-    MainloopScheduleType mainloop_schedule = MainloopScheduleType::AUTO;
-    EpilogueScheduleType epilogue_schedule = EpilogueScheduleType::AUTO;
-    ClusterShape cluster_shape = ClusterShape::ClusterShape_1x1x1;
-    bool is_sm90 = false;
+    CutlassTileConfigSM90 tile_config_sm90  = CutlassTileConfigSM90::ChooseWithHeuristic;
+    MainloopScheduleType  mainloop_schedule = MainloopScheduleType::AUTO;
+    EpilogueScheduleType  epilogue_schedule = EpilogueScheduleType::AUTO;
+    ClusterShape          cluster_shape     = ClusterShape::ClusterShape_1x1x1;
+    bool                  is_sm90           = false;
 
     CutlassGemmConfig() = default;
 
-    CutlassGemmConfig(
-        CutlassTileConfig tile_config, SplitKStyle split_k_style, int split_k_factor, int stages):
-        tile_config(tile_config),
-        split_k_style(split_k_style),
-        split_k_factor(split_k_factor),
-        stages(stages){}
+    CutlassGemmConfig(CutlassTileConfig tile_config, SplitKStyle split_k_style, int split_k_factor, int stages):
+        tile_config(tile_config), split_k_style(split_k_style), split_k_factor(split_k_factor), stages(stages) {}
 
-    CutlassGemmConfig(
-        CutlassTileConfig tile_config, int split_k_factor, int stages):
+    CutlassGemmConfig(CutlassTileConfig tile_config, int split_k_factor, int stages):
         tile_config(tile_config),
         split_k_style(split_k_factor > 1 ? SplitKStyle::SPLIT_K_SERIAL : SplitKStyle::NO_SPLIT_K),
         split_k_factor(split_k_factor),
         stages(stages) {}
 
-    CutlassGemmConfig(CutlassTileConfigSM90 tile_config_sm90, MainloopScheduleType mainloop_schedule,
-        EpilogueScheduleType epilogue_schedule, ClusterShape cluster_shape)
-        : tile_config_sm90(tile_config_sm90)
-        , mainloop_schedule(mainloop_schedule)
-        , epilogue_schedule(epilogue_schedule)
-        , cluster_shape(cluster_shape)
-        , is_sm90(true)
-    {
-    }
+    CutlassGemmConfig(CutlassTileConfigSM90 tile_config_sm90,
+                      MainloopScheduleType  mainloop_schedule,
+                      EpilogueScheduleType  epilogue_schedule,
+                      ClusterShape          cluster_shape):
+        tile_config_sm90(tile_config_sm90),
+        mainloop_schedule(mainloop_schedule),
+        epilogue_schedule(epilogue_schedule),
+        cluster_shape(cluster_shape),
+        is_sm90(true) {}
 
     bool operator==(CutlassGemmConfig const& r) const {
-        return tile_config == r.tile_config
-        && split_k_style == r.split_k_style && split_k_factor == r.split_k_factor && stages == r.stages;
+        return tile_config == r.tile_config && split_k_style == r.split_k_style && split_k_factor == r.split_k_factor
+               && stages == r.stages;
     }
 };
 
-} // namespace cutlass_extensions
-} // namespace tensorrt_llm
+}  // namespace cutlass_extensions
+}  // namespace tensorrt_llm
