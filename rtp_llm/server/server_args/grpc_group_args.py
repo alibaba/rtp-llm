72cd43e4417ad6ccf27552c85a1026f82e8b9f93
yanxi.wln <yanxi.wln@alibaba-inc.com>
2025-12-05 15:32:14 +0800

    fix: fix grpc default config

--- rtp_llm/server/server_args/grpc_group_args.py
+++ rtp_llm/server/server_args/grpc_group_args.py
@@ -13,6 +13,6 @@ def init_grpc_group_args(parser):
         "--grpc_config_json",
         env_name="GRPC_CONFIG_JSON",
         type=str,
-        default='{"client_config": {"grpc.max_receive_message_length": 1073741824, "grpc.max_metadata_size": 1073741824}, "server_config": {"grpc.max_concurrent_streams": 100000, "grpc.max_connection_idle_ms": 600000, "grpc.http2.min_recv_ping_interval_without_data_ms": 1000, "grpc.http2.max_ping_strikes": 1000}}',
+        default='{"client_config": {"grpc.max_receive_message_length": 1073741824, "grpc.max_metadata_size": 1073741824}, "server_config": {"grpc.max_metadata_size": 1073741824,"grpc.max_concurrent_streams": 100000, "grpc.max_connection_idle_ms": 600000, "grpc.http2.min_recv_ping_interval_without_data_ms": 1000, "grpc.http2.max_ping_strikes": 1000}}',
         help="gRPC configuration as JSON string.",
     )
