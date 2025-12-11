f13cc4b155b24ec660dd56a055a47cc7222a7dda
xiebaijie.xbj <xiebaijie.xbj@alibaba-inc.com>
2025-12-11 11:16:39 +0800

    auto

--- rtp_llm/models_py/modules/moe/executors/test/trtllm_fp4_executor_test.py
+++ rtp_llm/models_py/modules/moe/executors/test/trtllm_fp4_executor_test.py
@@ -187,12 +187,12 @@ def _generate_payload_and_weights(
     w13_input_scale = torch.empty((NUM_EXPERTS,), dtype=torch.float32, device='cuda:0').fill_(0.0014)
 
     # w13 = torch.empty((NUM_EXPERTS, MOE_INTERMEDIATE_SIZE * 2, HIDDEN_SIZE / 2), torch.uint8, device='cuda:0')
-    # w13_scale = torch.empty((NUM_EXPERTS, MOE_INTERMEDIATE_SIZE * 2, HIDDEN_SIZE / NVFP4_BLOCK_SIZE), torch.float8_e4m3fn, device='cuda:0')
+    w13_scale = torch.empty((NUM_EXPERTS, MOE_INTERMEDIATE_SIZE * 2, HIDDEN_SIZE / NVFP4_BLOCK_SIZE), torch.float32, device='cuda:0').normal_(76.1, 36.3).clamp_(0.1, 448.0).to(torch.float8_e4m3fn)
 
     w13_scale_2 = torch.empty((NUM_EXPERTS,), dtype=torch.float32, device='cuda:0').normal_(9e-05, 3.6e-05).clamp_(4.8e-05, 0.0002)
     w2_input_scale = torch.empty((NUM_EXPERTS,), dtype=torch.float32, device='cuda:0').fill_(0.0028)
     # w2 = torch.empty((NUM_EXPERTS, HIDDEN_SIZE, MOE_INTERMEDIATE_SIZE / 2), torch.uint8, device='cuda:0')
-    # w2_scale = torch.empty((NUM_EXPERTS, HIDDEN_SIZE, MOE_INTERMEDIATE_SIZE / NVFP4_BLOCK_SIZE), torch.float8_e4m3fn, device='cuda:0')
+    w2_scale = torch.empty((NUM_EXPERTS, HIDDEN_SIZE, MOE_INTERMEDIATE_SIZE / NVFP4_BLOCK_SIZE), torch.float32, device='cuda:0').normal_(53.2, 20.7).clamp_(4.5, 448.0).to(torch.float8_e4m3fn)
     w2_scale_2 = torch.empty((NUM_EXPERTS,), dtype=torch.float32, device='cuda:0').normal_(0.0001, 4.2e-05).clamp_(8.5e-05, 0.0003)
 
     permute_info, topk_weights = routing_reference_renormalize(
