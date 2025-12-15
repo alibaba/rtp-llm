b6da698d17ecfa6b983485003fcf221f8455c7da
fuyu.zh <fuyu.zh@alibaba-inc.com>
2025-12-15 19:37:35 +0800

    add vocab weight for tree logits processor

---
+++ rtp_llm/cpp/kernels/weight_logits.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <stdint.h>
+#if USING_CUDA
+#include <cuda_runtime.h>
+#endif
+#if USING_ROCM
+#include <hip/hip_runtime.h>
+#endif
+
+namespace rtp_llm {
+
+template<typename T>
+void invokeWeightLogits(T*           logits_batch,
+                        const float* weight_batch,
+                        const int    batch_size,
+                        const int    vocab_size,
+#if USING_CUDA
+                        cudaStream_t stream);
+#elif USING_ROCM
+                        hipStream_t stream);
+#endif
+
+}  // namespace rtp_llm
