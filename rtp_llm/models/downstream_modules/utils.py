fa86a36e5f1db1ecb7d19f23b6131776d6e00b00
luoli.hn <luoli.hn@taobao.com>
2025-12-17 13:43:51 +0800

    fix - some qwen3_reranker have no lm_weight fix - support qwen3 rerank after refactor embedding endpoint

--- rtp_llm/models/downstream_modules/utils.py
+++ rtp_llm/models/downstream_modules/utils.py
@@ -2,7 +2,6 @@ import logging
 from typing import Optional
 
 from rtp_llm.config.model_config import ModelConfig
-from rtp_llm.ops import TaskType
 from rtp_llm.frontend.tokenizer_factory.tokenizers import BaseTokenizer
 from rtp_llm.models.downstream_modules import (
     ALLEmbeddingModule,
@@ -13,27 +12,32 @@ from rtp_llm.models.downstream_modules import (
     RerankerModule,
     SparseEmbeddingModule,
 )
+from rtp_llm.models.downstream_modules.reranker.qwen3_reranker import (
+    Qwen3RerankerModule,
+)
+from rtp_llm.ops import TaskType
 
 
 def create_custom_module(
     config: ModelConfig,
     tokenizer: Optional[BaseTokenizer],
-):  
+):
     # try import internal module
     try:
         from internal_source.rtp_llm.models.downstream_modules.utils import (
             create_custom_module,
         )
+
         internal_module = create_custom_module(config, tokenizer)
         if internal_module is not None:
             return internal_module
     except ImportError:
         logging.exception("internal module not found, using external module")
 
-
     task_type = config.task_type
     if task_type == TaskType.LANGUAGE_MODEL:
         return None
+    model_type = config.model_type
     assert tokenizer is not None, "tokenizer should not be None"
     if task_type == TaskType.DENSE_EMBEDDING:
         return DenseEmbeddingModule(config, tokenizer)
@@ -47,6 +51,8 @@ def create_custom_module(
         return ClassifierModule(config, tokenizer)
     elif task_type == TaskType.BGE_M3:
         return BgeM3EmbeddingModule(config, tokenizer)
+    elif model_type == "qwen_3":
+        return Qwen3RerankerModule(config, tokenizer)
     elif task_type == TaskType.RERANKER:
         return RerankerModule(config, tokenizer)
     raise Exception(f"unknown task_type: {task_type}")
