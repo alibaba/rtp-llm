8f59ec2b1d2cf2c4046fb3b145ad6a880573dbbb
silu.zsl <silu.zsl@alibaba-inc.com>
2025-12-18 17:56:57 +0800

    feat: freeze gc

--- rtp_llm/frontend/frontend_app.py
+++ rtp_llm/frontend/frontend_app.py
@@ -1,4 +1,5 @@
 import asyncio
+import gc
 import logging
 import socket
 import threading
@@ -106,6 +107,9 @@ class FrontendApp(object):
         try:
             server = GracefulShutdownServer(config)
             server.set_server(self.frontend_server)
+            # freeze all current tracked objects to reduce gc cost
+            gc.collect()
+            gc.freeze()
             server.run()
         except BaseException as e:
             raise e
