711a47427caf007c97e3fcd2d378f7bf70828fe2
蝉吟 <liudu.ld@alibaba-inc.com>
2025-12-28 18:15:47 +0800

    fix kvcache group cache reuse

--- rtp_llm/cpp/cache/FullKVCacheGroup.cc
+++ rtp_llm/cpp/cache/FullKVCacheGroup.cc
@@ -35,10 +35,13 @@ MatchResult FullKVCacheGroup::match(const CacheKeysType& cache_keys) {
 
     for (auto& cache_key : cache_keys) {
         auto result = block_cache_->match(cache_key, group_id_);
-        if (!isNullBlockIdx(result.matched_index)) {
-            final_result.reuse_blocks++;
-            final_result.block_indices.push_back(result.matched_index);
+
+        if (isNullBlockIdx(result.matched_index)) {
+            break;
         }
+
+        final_result.reuse_blocks++;
+        final_result.block_indices.push_back(result.matched_index);
     }
 
     final_result.reuse_length = final_result.reuse_blocks * seqSizePerBlock();
