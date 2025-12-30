95b0a3ca0dd7f0055e01c2c7de8deee4c0d93653
xiebaijie.xbj <xiebaijie.xbj@alibaba-inc.com>
2025-12-30 11:38:16 +0800

    auto

--- rtp_llm/models_py/modules/factory/fused_moe/impl/cuda/executors/test/trtllm_fp4_executor_test.py
+++ rtp_llm/models_py/modules/factory/fused_moe/impl/cuda/executors/test/trtllm_fp4_executor_test.py
@@ -756,6 +756,7 @@ class moe_args:
     intermediate_size: int = None
     top_k: int = None
     padding: int = None
+    n_groups : int = None
     permute_info: torch.Tensor = None
     use_routing_scales_on_input: bool = None
     gated_act_type: GatedActType = None
