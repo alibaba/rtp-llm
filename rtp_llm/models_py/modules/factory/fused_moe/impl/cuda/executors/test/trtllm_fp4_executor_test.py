596669e07a4ba91831889e3741ee23a093ea1b9d
xiebaijie.xbj <xiebaijie.xbj@alibaba-inc.com>
2025-12-30 17:47:30 +0800

    auto

--- rtp_llm/models_py/modules/factory/fused_moe/impl/cuda/executors/test/trtllm_fp4_executor_test.py
+++ rtp_llm/models_py/modules/factory/fused_moe/impl/cuda/executors/test/trtllm_fp4_executor_test.py
@@ -343,8 +343,8 @@ class FP4MoeExecutor(FP4Moe):
         weights = {
             W.moe_w1: args.gemm1_weights.view(torch.float8_e4m3fn),
             W.moe_w2: args.gemm2_weights.view(torch.float8_e4m3fn),
-            W.moe_s1: args.gemm1_scales_linear.view(torch.float8_e4m3fn),
-            W.moe_s2: args.gemm2_scales_linear.view(torch.float8_e4m3fn),
+            W.moe_s1: args.gemm1_scales.view(torch.float8_e4m3fn),
+            W.moe_s2: args.gemm2_scales.view(torch.float8_e4m3fn),
             "w13_input_scale": 1.0 / args.hidden_states_scale_global,
             "w13_weight_scale_2": 1.0 / args.gemm1_scales_global,
             "w2_input_scale": 1.0 / args.c_global_sf,
