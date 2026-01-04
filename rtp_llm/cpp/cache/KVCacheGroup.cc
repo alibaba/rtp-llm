92166116602f5264dad5a374f2d7a019876f81bf
蝉吟 <liudu.ld@alibaba-inc.com>
2026-01-04 18:59:53 +0800

    fix typos and refine

--- rtp_llm/cpp/cache/KVCacheGroup.cc
+++ rtp_llm/cpp/cache/KVCacheGroup.cc
@@ -13,16 +13,16 @@ bool KVCacheGroup::init() {
                                 "global_layer_id out of range in KVCacheGroup::init: id=%d tensors_size=%zu",
                                 global_layer_id,
                                 layer_tensors.size());
-        gloabl_layer_to_kv_tensors[global_layer_id] = layer_tensors[static_cast<size_t>(global_layer_id)];
+        global_layer_to_kv_tensors[global_layer_id] = layer_tensors[static_cast<size_t>(global_layer_id)];
 
         if (!scale_tensors.empty()) {
             RTP_LLM_CHECK_WITH_INFO(static_cast<size_t>(global_layer_id) < scale_tensors.size(),
                                     "global_layer_id out of range in scale_tensors: id=%d tensors_size=%zu",
                                     global_layer_id,
                                     scale_tensors.size());
-            gloabl_layer_to_kv_scale_tensors[global_layer_id] = scale_tensors[static_cast<size_t>(global_layer_id)];
+            global_layer_to_kv_scale_tensors[global_layer_id] = scale_tensors[static_cast<size_t>(global_layer_id)];
         }
-        gloabl_layer_to_local_layer[layer_ids_[i]] = i;
+        global_layer_to_local_layer[layer_ids_[i]] = i;
     }
 
     return true;
@@ -59,31 +59,31 @@ int KVCacheGroup::seqSizePerBlock() const {
 }
 
 std::unordered_map<int, torch::Tensor> KVCacheGroup::layerCacheBase() const {
-    return gloabl_layer_to_kv_tensors;
+    return global_layer_to_kv_tensors;
 }
 
 std::unordered_map<int, torch::Tensor> KVCacheGroup::layerScaleCacheBase() const {
-    return gloabl_layer_to_kv_scale_tensors;
+    return global_layer_to_kv_scale_tensors;
 }
 
 BlockAddrInfo KVCacheGroup::convertIndexToAddr(int layer_id, int block_id) const {
-    auto it = gloabl_layer_to_local_layer.find(layer_id);
-    RTP_LLM_CHECK_WITH_INFO(it != gloabl_layer_to_local_layer.end(), "invalid layer_id: " + std::to_string(layer_id));
+    auto it = global_layer_to_local_layer.find(layer_id);
+    RTP_LLM_CHECK_WITH_INFO(it != global_layer_to_local_layer.end(), "invalid layer_id: " + std::to_string(layer_id));
     int local_layer_id = it->second;
     return block_pool_->convertIndexToAddr(local_layer_id, block_id);
 }
 
 BlockBufferPtrInfo KVCacheGroup::convertIndexToBuffer(int layer_id, int block_id) const {
-    auto it = gloabl_layer_to_local_layer.find(layer_id);
-    RTP_LLM_CHECK_WITH_INFO(it != gloabl_layer_to_local_layer.end(), "invalid layer_id: " + std::to_string(layer_id));
+    auto it = global_layer_to_local_layer.find(layer_id);
+    RTP_LLM_CHECK_WITH_INFO(it != global_layer_to_local_layer.end(), "invalid layer_id: " + std::to_string(layer_id));
     int local_layer_id = it->second;
     return block_pool_->convertIndexToBuffer(local_layer_id, block_id);
 }
 
 std::vector<BufferPtr>
 KVCacheGroup::convertIndexToBuffer(int layer_id, int block_id, int partition_count, int partition_id) const {
-    auto it = gloabl_layer_to_local_layer.find(layer_id);
-    RTP_LLM_CHECK_WITH_INFO(it != gloabl_layer_to_local_layer.end(), "invalid layer_id: " + std::to_string(layer_id));
+    auto it = global_layer_to_local_layer.find(layer_id);
+    RTP_LLM_CHECK_WITH_INFO(it != global_layer_to_local_layer.end(), "invalid layer_id: " + std::to_string(layer_id));
     int local_layer_id = it->second;
     return block_pool_->convertIndexToBuffer(local_layer_id, block_id, partition_count, partition_id);
 }
