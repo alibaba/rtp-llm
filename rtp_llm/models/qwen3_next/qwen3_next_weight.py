647d28af4d4fd9f3c7ff062f34d6b0c2685945d4
baowending.bwd <baowending.bwd@alibaba-inc.com>
2026-01-06 15:34:21 +0800

    feat - support qwen3next mtp model

--- rtp_llm/models/qwen3_next/qwen3_next_weight.py
+++ rtp_llm/models/qwen3_next/qwen3_next_weight.py
@@ -1,5 +1,5 @@
 import functools
-from typing import List
+from typing import Any, Dict, List
 
 import torch
 
@@ -48,6 +48,7 @@ def split_q_gate(ts: List[torch.Tensor], head_num: int, head_dim: int, part: int
         return t[:, 1, :, :].reshape(-1, dim1)
 
 
+# Qwen3Next use gemma_rms_norm
 def plus_one(ts: List[torch.Tensor]):
     return ts[0] + 1
 
@@ -144,6 +145,10 @@ def reorder_qkvz(
 
 
 class Qwen3NextWeight(ModelDeployWeightInfo):
+    def __init__(self, *args: List[Any], **kwargs: Dict[str, Any]):
+        super().__init__(*args, **kwargs)
+        self.prefix = "model."
+
     def _get_weight_info(self):
         weights: List[WeightModule] = [
             AtomicWeight(
@@ -156,45 +161,47 @@ class Qwen3NextWeight(ModelDeployWeightInfo):
             ),
             AtomicWeight(
                 W.final_ln_gamma,
-                [CkptWeightInfo("model.norm.weight", plus_one)],
+                [CkptWeightInfo(self.prefix + "norm.weight", plus_one)],
             ),
         ]
         all_layer_weights: List[List[WeightModule]] = []
         for idx in range(self._num_layers):
             layer_weight: List[WeightModule] = []
-            layer_weight.append(
-                AtomicWeight(
-                    W.pre_ln_gamma,
-                    [
-                        CkptWeightInfo(
-                            "model.layers.{i}.input_layernorm.weight",
-                            plus_one,
-                        )
-                    ],
-                )
-            )
+            layer_weight.extend(self._create_layer_norm_weight())
             if (
                 self.model_config.hybrid_attention_config.hybrid_attention_types[idx]
                 == HybridAttentionType.LINEAR
             ):
-                layer_weight.extend(self._create_linear_attention_weight(idx))
+                layer_weight.extend(self._create_linear_attention_weight())
             else:
-                layer_weight.extend(self._create_mqa_weight(idx))
-            layer_weight.append(
-                AtomicWeight(
-                    W.post_ln_gamma,
-                    [
-                        CkptWeightInfo(
-                            "model.layers.{i}.post_attention_layernorm.weight", plus_one
-                        ),
-                    ],
-                )
-            )
-            layer_weight.extend(self._create_ffn_weight(idx))
+                layer_weight.extend(self._create_mqa_weight())
+            layer_weight.extend(self._create_ffn_weight())
             all_layer_weights.append(layer_weight)
         return ModelWeightInfo(layer_weights=all_layer_weights, weights=weights)
 
-    def _create_ffn_weight(self, idx: int) -> List[WeightModule]:
+    def _create_layer_norm_weight(self) -> List[WeightModule]:
+        return [
+            AtomicWeight(
+                W.post_ln_gamma,
+                [
+                    CkptWeightInfo(
+                        self.prefix + "layers.{i}.post_attention_layernorm.weight",
+                        plus_one,
+                    ),
+                ],
+            ),
+            AtomicWeight(
+                W.pre_ln_gamma,
+                [
+                    CkptWeightInfo(
+                        self.prefix + "layers.{i}.input_layernorm.weight",
+                        plus_one,
+                    )
+                ],
+            ),
+        ]
+
+    def _create_ffn_weight(self) -> List[WeightModule]:
         moe_config = MoeConfig(
             expert_num=self.expert_num_,
             align_size=self._align_size,
@@ -212,7 +219,11 @@ class Qwen3NextWeight(ModelDeployWeightInfo):
                 sub_weights=[
                     MoeAtomicWeight(
                         W.moe_gate,
-                        [CkptWeightInfo("model.layers.{i}.mlp.gate.weight", identity)],
+                        [
+                            CkptWeightInfo(
+                                self.prefix + "layers.{i}.mlp.gate.weight", identity
+                            )
+                        ],
                         process_fun=transpose,
                         config=moe_config,
                     ),
@@ -220,7 +231,8 @@ class Qwen3NextWeight(ModelDeployWeightInfo):
                         W.ffn_w1,
                         [
                             CkptWeightInfo(
-                                "model.layers.{i}.mlp.shared_expert.gate_proj.weight",
+                                self.prefix
+                                + "layers.{i}.mlp.shared_expert.gate_proj.weight",
                                 identity,
                             )
                         ],
@@ -231,7 +243,8 @@ class Qwen3NextWeight(ModelDeployWeightInfo):
                         W.ffn_w2,
                         [
                             CkptWeightInfo(
-                                "model.layers.{i}.mlp.shared_expert.down_proj.weight",
+                                self.prefix
+                                + "layers.{i}.mlp.shared_expert.down_proj.weight",
                                 identity,
                             )
                         ],
@@ -242,7 +255,8 @@ class Qwen3NextWeight(ModelDeployWeightInfo):
                         W.ffn_w3,
                         [
                             CkptWeightInfo(
-                                "model.layers.{i}.mlp.shared_expert.up_proj.weight",
+                                self.prefix
+                                + "layers.{i}.mlp.shared_expert.up_proj.weight",
                                 identity,
                             )
                         ],
@@ -253,7 +267,8 @@ class Qwen3NextWeight(ModelDeployWeightInfo):
                         W.shared_expert_gate,
                         [
                             CkptWeightInfo(
-                                "model.layers.{i}.mlp.shared_expert_gate.weight",
+                                self.prefix
+                                + "layers.{i}.mlp.shared_expert_gate.weight",
                                 identity,
                             )
                         ],
@@ -263,7 +278,8 @@ class Qwen3NextWeight(ModelDeployWeightInfo):
                         W.moe_w2,
                         [
                             CkptWeightInfo(
-                                "model.layers.{i}.mlp.experts.{expert_id}.down_proj.weight",
+                                self.prefix
+                                + "layers.{i}.mlp.experts.{expert_id}.down_proj.weight",
                                 identity,
                             )
                         ],
@@ -274,13 +290,15 @@ class Qwen3NextWeight(ModelDeployWeightInfo):
                         W.moe_w1,
                         [
                             CkptWeightInfo(
-                                "model.layers.{i}.mlp.experts.{expert_id}.up_proj.weight",
+                                self.prefix
+                                + "layers.{i}.mlp.experts.{expert_id}.up_proj.weight",
                                 identity,
                             )
                         ]
                         + [
                             CkptWeightInfo(
-                                "model.layers.{i}.mlp.experts.{expert_id}.gate_proj.weight",
+                                self.prefix
+                                + "layers.{i}.mlp.experts.{expert_id}.gate_proj.weight",
                                 identity,
                             )
                         ],
@@ -292,13 +310,13 @@ class Qwen3NextWeight(ModelDeployWeightInfo):
             )
         ]
 
-    def _create_linear_attention_weight(self, idx: int):
+    def _create_linear_attention_weight(self):
         return [
             LinearAttnAtomicWeight(
                 W.linear_attn_qkvz_w,
                 [
                     CkptWeightInfo(
-                        "model.layers.{i}.linear_attn.in_proj_qkvz.weight",
+                        self.prefix + "layers.{i}.linear_attn.in_proj_qkvz.weight",
                         functools.partial(
                             reorder_qkvz,
                             linear_attention_config=self.model_config.linear_attention_config,
@@ -312,7 +330,7 @@ class Qwen3NextWeight(ModelDeployWeightInfo):
                 W.linear_attn_ba_w,
                 [
                     CkptWeightInfo(
-                        "model.layers.{i}.linear_attn.in_proj_ba.weight",
+                        self.prefix + "layers.{i}.linear_attn.in_proj_ba.weight",
                         functools.partial(
                             reorder_ba,
                             linear_attention_config=self.model_config.linear_attention_config,
@@ -324,13 +342,21 @@ class Qwen3NextWeight(ModelDeployWeightInfo):
             ),
             LinearAttnAtomicWeight(
                 W.linear_attn_norm_w,
-                [CkptWeightInfo("model.layers.{i}.linear_attn.norm.weight", identity)],
+                [
+                    CkptWeightInfo(
+                        self.prefix + "layers.{i}.linear_attn.norm.weight", identity
+                    )
+                ],
                 identity,
                 LinearAttnConfig(self.model_config.linear_attention_config),
             ),
             LinearAttnAtomicWeight(
                 W.linear_attn_dt_b,
-                [CkptWeightInfo("model.layers.{i}.linear_attn.dt_bias", identity)],
+                [
+                    CkptWeightInfo(
+                        self.prefix + "layers.{i}.linear_attn.dt_bias", identity
+                    )
+                ],
                 identity,
                 LinearAttnConfig(self.model_config.linear_attention_config),
             ),
@@ -338,7 +364,7 @@ class Qwen3NextWeight(ModelDeployWeightInfo):
                 W.linear_attn_conv1d_w,
                 [
                     CkptWeightInfo(
-                        "model.layers.{i}.linear_attn.conv1d.weight", identity
+                        self.prefix + "layers.{i}.linear_attn.conv1d.weight", identity
                     )
                 ],
                 identity,
@@ -346,7 +372,11 @@ class Qwen3NextWeight(ModelDeployWeightInfo):
             ),
             LinearAttnAtomicWeight(
                 W.linear_attn_alog,
-                [CkptWeightInfo("model.layers.{i}.linear_attn.A_log", identity)],
+                [
+                    CkptWeightInfo(
+                        self.prefix + "layers.{i}.linear_attn.A_log", identity
+                    )
+                ],
                 identity,
                 LinearAttnConfig(self.model_config.linear_attention_config),
             ),
@@ -354,7 +384,7 @@ class Qwen3NextWeight(ModelDeployWeightInfo):
                 W.linear_attn_out_w,
                 [
                     CkptWeightInfo(
-                        "model.layers.{i}.linear_attn.out_proj.weight", identity
+                        self.prefix + "layers.{i}.linear_attn.out_proj.weight", identity
                     )
                 ],
                 transpose,
@@ -362,13 +392,13 @@ class Qwen3NextWeight(ModelDeployWeightInfo):
             ),
         ]
 
-    def _create_mqa_weight(self, idx: int):
+    def _create_mqa_weight(self) -> List[WeightModule]:
         return [
             AttnAtomicWeight(
                 W.attn_qkv_w,
                 [
                     CkptWeightInfo(
-                        "model.layers.{i}.self_attn.q_proj.weight",
+                        self.prefix + "layers.{i}.self_attn.q_proj.weight",
                         functools.partial(
                             split_q_gate,
                             head_num=self.model_config.attn_config.head_num,
@@ -377,11 +407,11 @@ class Qwen3NextWeight(ModelDeployWeightInfo):
                         ),
                     ),
                     CkptWeightInfo(
-                        "model.layers.{i}.self_attn.k_proj.weight",
+                        self.prefix + "layers.{i}.self_attn.k_proj.weight",
                         identity,
                     ),
                     CkptWeightInfo(
-                        "model.layers.{i}.self_attn.v_proj.weight",
+                        self.prefix + "layers.{i}.self_attn.v_proj.weight",
                         identity,
                     ),
                 ],
@@ -392,7 +422,7 @@ class Qwen3NextWeight(ModelDeployWeightInfo):
                 W.attn_o_w,
                 [
                     CkptWeightInfo(
-                        "model.layers.{i}.self_attn.o_proj.weight",
+                        self.prefix + "layers.{i}.self_attn.o_proj.weight",
                         identity,
                     )
                 ],
@@ -403,7 +433,7 @@ class Qwen3NextWeight(ModelDeployWeightInfo):
                 W.attn_gate_w,
                 [
                     CkptWeightInfo(
-                        "model.layers.{i}.self_attn.q_proj.weight",
+                        self.prefix + "layers.{i}.self_attn.q_proj.weight",
                         functools.partial(
                             split_q_gate,
                             head_num=self.model_config.attn_config.head_num,
@@ -417,10 +447,18 @@ class Qwen3NextWeight(ModelDeployWeightInfo):
             ),
             AtomicWeight(
                 W.q_ln_gamma,
-                [CkptWeightInfo("model.layers.{i}.self_attn.q_norm.weight", plus_one)],
+                [
+                    CkptWeightInfo(
+                        self.prefix + "layers.{i}.self_attn.q_norm.weight", plus_one
+                    )
+                ],
             ),
             AtomicWeight(
                 W.k_ln_gamma,
-                [CkptWeightInfo("model.layers.{i}.self_attn.k_norm.weight", plus_one)],
+                [
+                    CkptWeightInfo(
+                        self.prefix + "layers.{i}.self_attn.k_norm.weight", plus_one
+                    )
+                ],
             ),
         ]
