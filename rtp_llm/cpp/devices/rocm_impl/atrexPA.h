92597b3621703550c38d97415c512a31d3c53dc6
beiyuan <beiyuan.ylf@alibaba-inc.com>
2026-01-12 12:39:47 +0800

    feat: optimize low concurrency pa

---
+++ rtp_llm/cpp/devices/rocm_impl/atrexPA.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <torch/extension.h>
+#include <hip/hip_runtime.h>
+#include "rtp_llm/cpp/kernels/rocm/pa_decode_dot_kernel.h"
+#include "rtp_llm/cpp/kernels/rocm/pa_decode_reduce_kernel.h"
+void paged_attention_atrex(torch::Tensor&                      out,
+                           torch::Tensor&                      exp_sums,
+                           torch::Tensor&                      max_logits,
+                           torch::Tensor&                      tmp_out,
+                           torch::Tensor&                      query,
+                           torch::Tensor&                      key_cache,
+                           torch::Tensor&                      value_cache,
+                           torch::Tensor&                      context_lens,
+                           torch::Tensor&                      block_tables,
+                           float                               scale,
+                           int64_t                             max_context_len,
+                           const std::optional<torch::Tensor>& alibi_slopes);
