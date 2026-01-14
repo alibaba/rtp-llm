272f223aaaf2c2993197e9b8d50e0da0c909d09a
Linfeng Yang <yanglf1121@163.com>
2026-01-14 11:52:00 +0800

    add rmsresnorm

--- rtp_llm/models_py/modules/base/rocm/not_implemented_ops.py
+++ rtp_llm/models_py/modules/base/rocm/not_implemented_ops.py
@@ -2,14 +2,6 @@
 
 from rtp_llm.models_py.modules.base.not_implemented import NotImplementedOp
 
-
-class RMSResNorm(NotImplementedOp):
-    """RMSResNorm is not implemented for ROCm."""
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(op_name="RMSResNorm", device_type="ROCm")
-
-
 class GroupTopK(NotImplementedOp):
     """GroupTopK is not implemented for ROCm."""
 
