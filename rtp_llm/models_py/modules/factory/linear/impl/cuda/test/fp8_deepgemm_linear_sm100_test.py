a08d546cb41b242f75e95a2f23ea5ba52c7057c3
rtp-llm <rtp-llm@alibaba-inc.com>
2026-01-16 10:29:21 +0800

    fix - fix cu12_9 flashinfer deps and add sm100 ut

---
+++ rtp_llm/models_py/modules/factory/linear/impl/cuda/test/fp8_deepgemm_linear_sm100_test.py
@@ -0,0 +1,19 @@
+import unittest
+
+from rtp_llm.models_py.kernels.cuda.deepgemm_wrapper import (
+    has_deep_gemm,
+    is_deep_gemm_e8m0_used,
+)
+from rtp_llm.models_py.modules.factory.linear.impl.cuda.test.fp8_deepgemm_linear_test import (
+    CudaFp8DeepGEMMLinearTestBase,
+)
+
+
+class CudaFp8DeepGEMMLinearSM100Test(CudaFp8DeepGEMMLinearTestBase, unittest.TestCase):
+    def test_sm100(self):
+        self.assertTrue(has_deep_gemm())
+        self.assertTrue(is_deep_gemm_e8m0_used())
+
+
+if __name__ == "__main__":
+    unittest.main()
