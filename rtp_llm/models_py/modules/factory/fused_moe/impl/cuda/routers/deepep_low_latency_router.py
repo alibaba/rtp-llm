df6d66e490f82ed868466dc105c832cc78069c75
lyh437841 <lyh437841@alibaba-inc.com>
2026-01-20 21:47:25 +0800

    feat: init deepep before load weight

--- rtp_llm/models_py/modules/factory/fused_moe/impl/cuda/routers/deepep_low_latency_router.py
+++ rtp_llm/models_py/modules/factory/fused_moe/impl/cuda/routers/deepep_low_latency_router.py
@@ -70,8 +70,10 @@ class DeepEpLowLatencyRouter(FusedMoeDataRouter):
 
         # DeepEpLowLatency-specific initialization
         self._num_experts = config.expert_num
-        self._ll_num_max_token_per_rank = self._calc_low_latency_max_token_per_rank(
-            config.max_generate_batch_size, config.tp_size, quant_config
+        self._ll_num_max_token_per_rank = (
+            DeepepWrapperConfig.calc_low_latency_max_token_per_rank(
+                config.max_generate_batch_size, config.tp_size, quant_config
+            )
         )
         deepep_config = DeepepWrapperConfig.from_config_adapter(
             self.config, self._ll_num_max_token_per_rank
@@ -316,42 +318,3 @@ class DeepEpLowLatencyRouter(FusedMoeDataRouter):
         self._handle = None
 
         return combined_x
-
-    def _calc_low_latency_max_token_per_rank(
-        self,
-        max_generate_batch_size: int,
-        tp_size: int,
-        quant_config: FusedMoEQuantConfig,
-    ) -> int:
-        ll_num_max_token_per_rank = (max_generate_batch_size + tp_size - 1) // tp_size
-        # deepgemm masked with max_m < 64 get incorrect result, related: https://github.com/deepseek-ai/DeepGEMM/issues/268
-        if not quant_config.is_quantized or quant_config.is_block_quantized:
-            matched_tokens = [64, 128]
-        elif quant_config.is_per_act_token:
-            matched_tokens = [
-                16,
-                24,
-                32,
-                40,
-                48,
-                56,
-                64,
-                72,
-                80,
-                88,
-                96,
-                104,
-                112,
-                120,
-                128,
-            ]
-        else:
-            raise ValueError("Unsupported quantization config")
-        if ll_num_max_token_per_rank > 128:
-            ll_num_max_token_per_rank = ((ll_num_max_token_per_rank + 127) // 128) * 128
-            return ll_num_max_token_per_rank
-        for t in matched_tokens:
-            if ll_num_max_token_per_rank <= t:
-                ll_num_max_token_per_rank = t
-                return ll_num_max_token_per_rank
-        return 128
