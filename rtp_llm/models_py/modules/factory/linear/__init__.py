f886360f692cd39ab51141cf32c82894a880372e
weike.chw <caihaowen.chw@alibaba-inc.com>
2026-01-22 19:56:57 +0800

    Revert "fix: fix frontend deps"
    This reverts commit 2d760004ad576d9f707c7752067c97892fec8c8b.

--- rtp_llm/models_py/modules/factory/linear/__init__.py
+++ rtp_llm/models_py/modules/factory/linear/__init__.py
@@ -3,8 +3,6 @@
 Uses strategy pattern for creating Linear layers.
 """
 
-import logging
-
 from rtp_llm.ops.compute_ops import DeviceType, get_device
 
 from .factory import LinearFactory
@@ -17,12 +15,10 @@ __all__ = ["LinearFactory", "LinearBase"]
 # ============================================================================
 
 device_type = get_device().get_device_type()
-try:
-    if device_type == DeviceType.ROCm:
-        # Import to trigger ROCm Linear strategy registration
-        import rtp_llm.models_py.modules.factory.linear.impl.rocm  # noqa: F401
-    elif device_type == DeviceType.Cuda:
-        # Import to trigger CUDA Linear strategy registration
-        import rtp_llm.models_py.modules.factory.linear.impl.cuda  # noqa: F401
-except Exception as e:
-    logging.warning(f"Failed to import Linear implementation: {e}")
+
+if device_type == DeviceType.ROCm:
+    # Import to trigger ROCm Linear strategy registration
+    import rtp_llm.models_py.modules.factory.linear.impl.rocm  # noqa: F401
+else:
+    # Import to trigger CUDA Linear strategy registration
+    import rtp_llm.models_py.modules.factory.linear.impl.cuda  # noqa: F401
