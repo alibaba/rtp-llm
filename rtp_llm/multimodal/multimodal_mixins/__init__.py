a69b4126ba49940459eff7c35b37adf9e24301a0
xieshui.yyx <xieshui.yyx@alibaba-inc.com>
2026-01-22 17:13:35 +0800

    fix: rebase conflicts

--- rtp_llm/multimodal/multimodal_mixins/__init__.py
+++ rtp_llm/multimodal/multimodal_mixins/__init__.py
@@ -10,6 +10,7 @@ from .llava.llava_mixin import LlavaMixin
 from .qwen2_5_vl.qwen2_5_vl_mixin import Qwen2_5_VLMixin
 from .qwen2_audio.qwen2_audio_mixin import Qwen2_AudioMixin
 from .qwen2_vl.qwen2_vl_mixin import Qwen2_VLMixin
+from .qwen3_vl_mixin import Qwen3_VLMixin
 from .qwen_vl.qwen_vl_mixin import QwenVLMixin
 
 if has_internal_source():
