2935f9cca5ce191e8ea7e0b75aef4b8c8326d45e
lxq <lxq271332@alibaba-inc.com>
2026-01-27 15:22:41 +0800

    feat: add memory connector implementation

---
+++ rtp_llm/cpp/cache/connector/memory/MemoryBlockCache.cc
@@ -0,0 +1,90 @@
+#include "rtp_llm/cpp/cache/connector/memory/MemoryBlockCache.h"
+
+#include <cassert>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <set>
+
+#include "rtp_llm/cpp/utils/AssertUtils.h"
+#include "rtp_llm/cpp/utils/StringUtil.h"
+
+using namespace std;
+
+namespace rtp_llm {
+
+MemoryBlockCache::MatchResult MemoryBlockCache::match(CacheKeyType cache_key) {
+    std::lock_guard<std::mutex> lock(mutex_);
+    const auto& [success, item] = lru_cache_.get(cache_key);
+    if (success) {
+        return {item.block_index, item.block_size};
+    } else {
+        return {NULL_BLOCK_IDX, 0};
+    }
+}
+
+bool MemoryBlockCache::contains(CacheKeyType cache_key) const {
+    std::lock_guard<std::mutex> lock(mutex_);
+    return lru_cache_.contains(cache_key);
+}
+
+std::pair<bool, std::optional<MemoryBlockCache::CacheItem>> MemoryBlockCache::put(const CacheItem& item) {
+    RTP_LLM_CHECK_WITH_INFO(!isNullBlockIdx(item.block_index), "put block id should not be null");
+
+    std::lock_guard<std::mutex> lock(mutex_);
+    if (lru_cache_.contains(item.cache_key)) {
+        // Increase old matched item's popularity
+        lru_cache_.get(item.cache_key);
+        return {false, std::nullopt};
+    }
+
+    std::optional<CacheItem> popped_item;
+    if (lru_cache_.full()) {
+        auto [success, popped_cache_item] = lru_cache_.pop();
+        if (!success) {
+            RTP_LLM_LOG_ERROR("put item failed, cache is full but pop item failed, cache key: %ld, cache size: %lu",
+                              item.cache_key,
+                              lru_cache_.size());
+            return {false, std::nullopt};
+        }
+        popped_item = popped_cache_item;
+    }
+
+    lru_cache_.put(item.cache_key, item);
+    return {true, popped_item};
+}
+
+std::vector<BlockIdxType> MemoryBlockCache::pop(int n) {
+    RTP_LLM_CHECK_WITH_INFO(n > 0, "pop n should > 0, n = " + std::to_string(n));
+    std::vector<BlockIdxType> pop_blocks;
+
+    std::lock_guard<std::mutex> lock(mutex_);
+
+    auto cond = [&](const CacheKeyType& /*key*/, const CacheItem& item) { return !item.is_resident; };
+
+    while (!lru_cache_.empty()) {
+        auto [success, cache_item] = lru_cache_.popWithCond(cond);
+        if (!success) {
+            break;
+        }
+        pop_blocks.push_back(cache_item.block_index);
+        --n;
+        if (n == 0) {
+            break;
+        }
+    }
+
+    return pop_blocks;
+}
+
+bool MemoryBlockCache::empty() const {
+    std::lock_guard<std::mutex> lock(mutex_);
+    return lru_cache_.empty();
+}
+
+size_t MemoryBlockCache::size() const {
+    std::lock_guard<std::mutex> lock(mutex_);
+    return lru_cache_.size();
+}
+
+}  // namespace rtp_llm
---
+++ rtp_llm/cpp/cache/connector/memory/MemoryBlockCache.h
@@ -0,0 +1,52 @@
+#pragma once
+
+#include <memory>
+#include <vector>
+#include <cstdint>
+#include <mutex>
+#include <optional>
+
+#include "rtp_llm/cpp/cache/KVCacheResource.h"
+#include "rtp_llm/cpp/utils/LRUCache.h"
+#include "rtp_llm/cpp/cache/Types.h"
+
+namespace rtp_llm {
+
+class MemoryBlockCache {
+public:
+    struct CacheItem {
+        CacheKeyType cache_key{0};
+        BlockIdxType block_index{-1};
+        size_t       block_size{0};
+        bool         is_resident{false};
+    };
+
+    struct MatchResult {
+        BlockIdxType matched_index{NULL_BLOCK_IDX};
+        size_t       block_size{0};
+    };
+
+public:
+    static const size_t kCacheMaxCapacity = 10000000;
+    explicit MemoryBlockCache(): lru_cache_(kCacheMaxCapacity) {}
+
+    MatchResult match(CacheKeyType cache_key);
+
+    bool contains(CacheKeyType cache_key) const;
+
+    std::pair<bool, std::optional<CacheItem>> put(const CacheItem& cache_item);
+
+    std::vector<BlockIdxType> pop(int n);
+
+    bool empty() const;
+
+    size_t size() const;
+
+private:
+    mutable LRUCache<CacheKeyType, CacheItem> lru_cache_;
+    mutable std::mutex                        mutex_;
+};
+
+using MemoryBlockCachePtr = std::shared_ptr<MemoryBlockCache>;
+
+}  // namespace rtp_llm
