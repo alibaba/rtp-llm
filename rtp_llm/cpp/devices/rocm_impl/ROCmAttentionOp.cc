6e2afe065e8f4c48c9c8a37fae38fcd613775ceb
Zhao An <zhao.an@amd.com>
2026-01-27 21:35:12 +0800

    fix: change asm_pa to gluon_pa for stress test acc issue

--- rtp_llm/cpp/devices/rocm_impl/ROCmAttentionOp.cc
+++ rtp_llm/cpp/devices/rocm_impl/ROCmAttentionOp.cc
@@ -1361,7 +1361,8 @@ AttentionModuleOutput ROCmDevice::decoderSelfAttention(const AttentionModulePara
             check_cuda_error();
             DEBUG_PRINT_PARAMS(params, this, "decode_writeKVCache", q_output);
             if (init_params_.use_asm_pa) {
-                runAiterAsmPA(params, this, *q_output);
+                // runAiterAsmPA(params, this, *q_output);
+                aiter_wrapper_->runTritonPA(params, this, *q_output, stream_);
             } else {
                 runAiterPA(params, this, *q_output);
             }
