551275df996e3d663d7c0299cb4483db768e4ca5
lxq <lxq271332@alibaba-inc.com>
2026-01-29 16:26:42 +0800

    feat: add connector coordinator implementation

--- rtp_llm/cpp/cache/KVCacheManager.cc
+++ rtp_llm/cpp/cache/KVCacheManager.cc
@@ -5,6 +5,7 @@
 
 #include "rtp_llm/cpp/cache/SingleTypeKVCacheAllocator.h"
 #include "rtp_llm/cpp/cache/BatchKVCacheResource.h"
+#include "rtp_llm/cpp/cache/connector/KVCacheConnectorCoordinator.h"
 #include "rtp_llm/cpp/cache/KVCacheHashUtil.h"
 #include "rtp_llm/cpp/engine_base/stream/CompleteTokenIds.h"
 #include "rtp_llm/cpp/devices/DeviceBase.h"
@@ -46,6 +47,7 @@ KVCacheManager::~KVCacheManager() {
         metrics_reporter_thread_.join();
     }
     allocator_.reset();
+    coordinator_.reset();
 }
 
 bool KVCacheManager::init() {
@@ -76,11 +78,13 @@ bool KVCacheManager::init() {
             stop_.store(false, std::memory_order_relaxed);
             metrics_reporter_thread_ = std::thread(&KVCacheManager::reportMetricsLoop, this);
         }
-        return true;
     } else {
         RTP_LLM_CHECK_WITH_INFO(false, "SingleTypeKVCacheAllocator only support Full Attention");
         return false;
     }
+
+    initConnectorCoordinator();
+    return true;
 }
 
 size_t KVCacheManager::availableTokensNum() const {
@@ -341,6 +345,26 @@ KVCacheBuffer KVCacheManager::getMTPModuleKVCacheBuffer(int mtp_module_id) const
 
 const CacheConfig& KVCacheManager::getMTPModuleCacheConfig(int mtp_module_id) const {
     return *config_.mtp_sub_configs[mtp_module_id];
+}
+
+void KVCacheManager::initConnectorCoordinator() {
+    coordinator_ = std::make_shared<KVCacheConnectorCoordinator>(
+        config_, kv_cache_config_, runtime_config_, allocator_, device_, metrics_reporter_);
+    RTP_LLM_CHECK_WITH_INFO(coordinator_->init(), "connector coordinator init failed");
+}
+
+std::shared_ptr<AsyncContext>
+KVCacheManager::asyncLoadCache(const std::shared_ptr<KVCacheConnectorReadWriteContext>& connector_context) {
+    return coordinator_->asyncRead(connector_context);
+}
+
+std::shared_ptr<AsyncContext>
+KVCacheManager::asyncStoreCache(const std::shared_ptr<KVCacheConnectorReadWriteContext>& connector_context) {
+    return coordinator_->asyncWrite(connector_context);
+}
+
+bool KVCacheManager::executeFunction(const FunctionRequestPB& request, FunctionResponsePB& response) {
+    return coordinator_->executeFunction(request, response);
 }
 
 }  // namespace rtp_llm
---
+++ rtp_llm/cpp/cache/connector/KVCacheConnectorCoordinator.h
@@ -0,0 +1,68 @@
+#pragma once
+
+#include <memory>
+#include <mutex>
+#include <vector>
+
+#include "autil/LoopThread.h"
+#include "rtp_llm/cpp/cache/BatchKVCacheResource.h"
+#include "rtp_llm/cpp/cache/CacheConfig.h"
+#include "rtp_llm/cpp/cache/connector/AsyncContext.h"
+#include "rtp_llm/cpp/cache/connector/KVCacheConnector.h"
+#include "rtp_llm/cpp/cache/KVCacheAllocator.h"
+#include "rtp_llm/cpp/config/ConfigModules.h"
+#include "rtp_llm/cpp/model_rpc/proto/model_rpc_service.grpc.pb.h"
+
+namespace rtp_llm {
+
+class DeviceBase;
+class KVCacheAllocator;
+class KVCacheConnectorReadWriteContext;
+
+class KVCacheConnectorCoordinator: public std::enable_shared_from_this<KVCacheConnectorCoordinator> {
+public:
+    KVCacheConnectorCoordinator(const CacheConfig&                       cache_config,
+                                const KVCacheConfig&                     kv_cache_config,
+                                const RuntimeConfig&                     runtime_config,
+                                const std::shared_ptr<KVCacheAllocator>& allocator,
+                                rtp_llm::DeviceBase*                     device,
+                                const kmonitor::MetricsReporterPtr&      metrics_reporter = nullptr);
+    virtual ~KVCacheConnectorCoordinator();
+
+public:
+    bool init();
+    // virtual for test
+    virtual std::shared_ptr<AsyncContext>
+    asyncRead(const std::shared_ptr<KVCacheConnectorReadWriteContext>& connector_context);
+    virtual std::shared_ptr<AsyncContext>
+    asyncWrite(const std::shared_ptr<KVCacheConnectorReadWriteContext>& connector_context);
+    virtual std::shared_ptr<AsyncContext>
+    asyncWriteByLayer(int layer_id, const std::shared_ptr<KVCacheConnectorReadWriteContext>& connector_context);
+    virtual bool executeFunction(const FunctionRequestPB& request, FunctionResponsePB& response);
+
+private:
+    void initUpdateThread();
+    void updateOnce();
+    void processReadContexts();
+    void processWriteContexts();
+    void asyncReadAfterMatch(std::shared_ptr<FusedAsyncReadContext> fused_read_context);
+
+private:
+    const CacheConfig                 cache_config_;
+    const KVCacheConfig               kv_cache_config_;
+    const RuntimeConfig               runtime_config_;
+    std::shared_ptr<KVCacheAllocator> allocator_;
+    rtp_llm::DeviceBase*              device_{nullptr};
+    kmonitor::MetricsReporterPtr      metrics_reporter_;
+
+    std::vector<std::shared_ptr<KVCacheConnector>> connectors_;
+
+    mutable std::mutex                                update_mutex_;
+    std::list<std::shared_ptr<FusedAsyncReadContext>> fused_async_read_context_list_;
+    std::list<std::shared_ptr<FusedAsyncContext>>     fused_async_write_context_list_;
+    autil::LoopThreadPtr                              update_thread_;
+    const int                                         update_interval_ms_{1};
+    std::atomic<bool>                                 stop_{false};
+};
+
+}  // namespace rtp_llm
