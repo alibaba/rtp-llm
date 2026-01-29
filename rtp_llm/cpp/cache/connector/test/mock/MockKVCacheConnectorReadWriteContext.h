f60918b6668414844d9f5fec218f7439948f9176
lxq <lxq271332@alibaba-inc.com>
2026-01-29 16:30:25 +0800

    feat: add unit test for connector

---
+++ rtp_llm/cpp/cache/connector/test/mock/MockKVCacheConnectorReadWriteContext.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <gmock/gmock.h>
+
+#include "rtp_llm/cpp/cache/connector/KVCacheConnectorReadWriteContext.h"
+
+namespace rtp_llm {
+
+class MockKVCacheConnectorReadWriteContext: public KVCacheConnectorReadWriteContext {
+public:
+    MockKVCacheConnectorReadWriteContext()           = default;
+    ~MockKVCacheConnectorReadWriteContext() override = default;
+
+public:
+    MOCK_METHOD(const KVCacheResource&, kvCacheResource, (), (const, override));
+    MOCK_METHOD(const std::shared_ptr<Meta>&, meta, (), (const, override));
+};
+
+}  // namespace rtp_llm
