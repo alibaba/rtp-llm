22e79bc919dda1d3e69cf7bd8f4139e2ba668657
wangyin <wangyin.yx@alibaba-inc.com>
2026-02-03 12:03:10 +0800

    fix: non_blocking

--- rtp_llm/models_py/modules/factory/attention/cuda_impl/trtllm_gen.py
+++ rtp_llm/models_py/modules/factory/attention/cuda_impl/trtllm_gen.py
@@ -184,6 +184,7 @@ class FlashInferTRTLLMDecodeOp(object):
         return not attention_inputs.is_prefill
 
     def prepare(self, attention_inputs: PyAttentionInputs) -> FlashInferTRTLLMParams:
+        non_blocking = torch.cuda.is_current_stream_capturing()
         if not attention_inputs.is_prefill:
             # need transfer to cuda, cuda graph can capture the add
             sequence_lengths = torch.ones_like(
