8cddbdf033f89346997ab492ca6f978295f29b36
weike.chw <caihaowen.chw@alibaba-inc.com>
2026-02-06 17:39:29 +0800

    fix waitPredicate logic

--- rtp_llm/cpp/engine_base/schedulers/GatherBatchScheduler.h
+++ rtp_llm/cpp/engine_base/schedulers/GatherBatchScheduler.h
@@ -17,15 +17,22 @@ struct GatherBatchSchedulerConfigLocal: public autil::legacy::Jsonizable {
 // Currently it is only used in CI with prompt_batch input, which may occur unstable result
 class GatherBatchScheduler: virtual public FIFOScheduler {
 public:
-    explicit GatherBatchScheduler(const RuntimeConfig&                 runtime_config,
-                                  const ModelConfig&                   model_config,
-                                  const PDSepConfig&                  pd_sep_config,
-                                  const ParallelismConfig&            parallelism_config,
-                                  const ModelSpecificConfig&          model_specific_config,
+    explicit GatherBatchScheduler(const RuntimeConfig&                   runtime_config,
+                                  const ModelConfig&                     model_config,
+                                  const PDSepConfig&                     pd_sep_config,
+                                  const ParallelismConfig&               parallelism_config,
+                                  const ModelSpecificConfig&             model_specific_config,
                                   const std::shared_ptr<KVCacheManager>& cache_manager,
-                                  const kmonitor::MetricsReporterPtr   metrics_reporter,
-                                  const int                            max_score_len = 1):
-        FIFOScheduler(runtime_config, model_config, pd_sep_config, parallelism_config, model_specific_config, cache_manager, metrics_reporter, max_score_len) {
+                                  const kmonitor::MetricsReporterPtr     metrics_reporter,
+                                  const int                              max_score_len = 1):
+        FIFOScheduler(runtime_config,
+                      model_config,
+                      pd_sep_config,
+                      parallelism_config,
+                      model_specific_config,
+                      cache_manager,
+                      metrics_reporter,
+                      max_score_len) {
         RTP_LLM_LOG_INFO("GatherBatchScheduler init");
         gather_batch_size_ = 1;
     }
@@ -41,7 +48,7 @@ protected:
     void reportMetrics() override {
         if (metrics_reporter_) {
             RtpLLMSchedulerMetricsCollector collector;
-            int64_t waiting_size = 0;
+            int64_t                         waiting_size = 0;
             // Lock is held by caller (schedule)
             for (const auto& group : waiting_groups_) {
                 waiting_size += group.streams.size();
@@ -69,7 +76,7 @@ protected:
     std::atomic<bool> schedule_trigger_ = false;
 
     bool waitPredicate() override {
-        return stop_ || !waiting_groups_.empty() || !running_streams_.empty() || !remote_running_streams_.empty() || schedule_trigger_;
+        return stop_ || !running_streams_.empty() || !remote_running_streams_.empty() || schedule_trigger_;
     }
 
 public:
@@ -81,7 +88,7 @@ public:
         } else {
             cond_.wait(lock, [this] { return waitPredicate(); });
         }
-        
+
         schedule_trigger_ = false;
 
         evaluateRunningRemote();
@@ -106,19 +113,19 @@ public:
     absl::Status enqueue(const GenerateStreamPtr& stream) override {
         {
             std::lock_guard<std::mutex> lock(lock_);
-            int64_t gid = stream->batchGroupId();
-            auto it = group_map_.find(gid);
+            int64_t                     gid = stream->batchGroupId();
+            auto                        it  = group_map_.find(gid);
             if (it == group_map_.end()) {
                 ScheduleGroup group;
-                group.batch_group_id = gid;
-                group.force_batch = stream->generateConfig()->force_batch;
-                group.expected_size = stream->batchGroupSize();
-                group.timeout_us = (int64_t)stream->batchGroupTimeout() * 1000;
+                group.batch_group_id     = gid;
+                group.force_batch        = stream->generateConfig()->force_batch;
+                group.expected_size      = stream->batchGroupSize();
+                group.timeout_us         = (int64_t)stream->batchGroupTimeout() * 1000;
                 group.first_enqueue_time = stream->enqueueTime();
-                
+
                 waiting_groups_.emplace_back(std::move(group));
                 group_map_[gid] = std::prev(waiting_groups_.end());
-                it = group_map_.find(gid);
+                it              = group_map_.find(gid);
             }
             it->second->streams.push_back(stream);
             schedule_trigger_ = true;
@@ -132,19 +139,19 @@ public:
             std::lock_guard<std::mutex> lock(lock_);
             for (const auto& stream : streams) {
                 int64_t gid = stream->batchGroupId();
-                auto it = group_map_.find(gid);
+                auto    it  = group_map_.find(gid);
                 if (it == group_map_.end()) {
                     ScheduleGroup group;
-                    group.batch_group_id = gid;
-                    group.force_batch = stream->generateConfig()->force_batch;
-                    group.expected_size = stream->batchGroupSize();
-                    group.timeout_us = (int64_t)stream->batchGroupTimeout() * 1000;
+                    group.batch_group_id     = gid;
+                    group.force_batch        = stream->generateConfig()->force_batch;
+                    group.expected_size      = stream->batchGroupSize();
+                    group.timeout_us         = (int64_t)stream->batchGroupTimeout() * 1000;
                     group.first_enqueue_time = stream->enqueueTime();
-                    
+
                     waiting_groups_.emplace_back(std::move(group));
                     group_map_[gid] = std::prev(waiting_groups_.end());
-                    it = group_map_.find(gid);
-                }                
+                    it              = group_map_.find(gid);
+                }
                 it->second->streams.push_back(stream);
             }
             schedule_trigger_ = true;
@@ -160,7 +167,7 @@ public:
 
     int64_t waitingStreamsSize() override {
         std::lock_guard<std::mutex> lock(lock_);
-        int64_t size = 0;
+        int64_t                     size = 0;
         for (const auto& group : waiting_groups_) {
             size += group.streams.size();
         }
@@ -169,7 +176,7 @@ public:
 
     int64_t onflightStreams() override {
         std::lock_guard<std::mutex> lock(lock_);
-        int64_t waiting_size = 0;
+        int64_t                     waiting_size = 0;
         for (const auto& group : waiting_groups_) {
             waiting_size += group.streams.size();
         }
@@ -177,7 +184,7 @@ public:
     }
 
     std::vector<EngineScheduleInfo::TaskInfo> waitingTaskList() override {
-        std::lock_guard<std::mutex> lock(lock_);
+        std::lock_guard<std::mutex>               lock(lock_);
         std::vector<EngineScheduleInfo::TaskInfo> task_list;
         task_list.reserve(waiting_groups_.size());
         for (const auto& group : waiting_groups_) {
@@ -196,17 +203,18 @@ protected:
     void evictDoneStreams(std::list<GenerateStreamPtr>& streams) override {
         if (&streams == &waiting_streams_) {
             // Evict done streams in waiting groups
-            for (auto git = waiting_groups_.begin(); git != waiting_groups_.end(); ) {
-                for (auto sit = git->streams.begin(); sit != git->streams.end(); ) {
-                     (*sit)->checkTimeout();
-                     if ((*sit)->stopped() || (*sit)->finished()) {
-                         (*sit)->releaseResource();
-                         RTP_LLM_LOG_DEBUG("evict waiting stream [%ld]", (*sit)->streamId());
-                         sit = git->streams.erase(sit);
-                         if (git->expected_size > 0) git->expected_size--;
-                     } else {
-                         ++sit;
-                     }
+            for (auto git = waiting_groups_.begin(); git != waiting_groups_.end();) {
+                for (auto sit = git->streams.begin(); sit != git->streams.end();) {
+                    (*sit)->checkTimeout();
+                    if ((*sit)->stopped() || (*sit)->finished()) {
+                        (*sit)->releaseResource();
+                        RTP_LLM_LOG_DEBUG("evict waiting stream [%ld]", (*sit)->streamId());
+                        sit = git->streams.erase(sit);
+                        if (git->expected_size > 0)
+                            git->expected_size--;
+                    } else {
+                        ++sit;
+                    }
                 }
                 if (git->streams.empty()) {
                     group_map_.erase(git->batch_group_id);
@@ -220,7 +228,7 @@ protected:
         }
     }
 
-    bool canFitGroupInMemory(const std::list<GenerateStreamPtr>& current_new_streams, 
+    bool canFitGroupInMemory(const std::list<GenerateStreamPtr>& current_new_streams,
                              const std::list<GenerateStreamPtr>& group_streams) const {
         std::list<GenerateStreamPtr> temp_streams = current_new_streams;
         for (const auto& s : group_streams) {
@@ -244,25 +252,25 @@ protected:
                              gather_batch_size_);
             return {};
         }
-        
+
         // reset gather_batch_size_ to 1
         gather_batch_size_ = 1;
 
         std::list<GenerateStreamPtr> new_streams;
-        int64_t current_time_us = autil::TimeUtility::currentTimeInMicroSeconds();
+        int64_t                      current_time_us = autil::TimeUtility::currentTimeInMicroSeconds();
 
-        for (auto git = waiting_groups_.begin(); git != waiting_groups_.end(); ) {
+        for (auto git = waiting_groups_.begin(); git != waiting_groups_.end();) {
             auto& group = *git;
-            
-            bool is_timeout = (current_time_us - group.first_enqueue_time) > group.timeout_us;
+
+            bool is_timeout     = (current_time_us - group.first_enqueue_time) > group.timeout_us;
             bool is_size_enough = (int)group.streams.size() >= group.expected_size;
-            
+
             if (!is_timeout && !is_size_enough) {
                 ++git;
                 continue;
             }
 
-            // Atomic Pre-check: 
+            // Atomic Pre-check:
             // Only for Force Batch groups that haven't timed out.
             // If they fit, great. If not, we skip them to wait for more resources (try to run atomically).
             // Unless the system is empty (deadlock prevention), then we fall through to split.
@@ -276,39 +284,42 @@ protected:
                 }
             }
 
-            for (auto sit = group.streams.begin(); sit != group.streams.end(); ) {
+            for (auto sit = group.streams.begin(); sit != group.streams.end();) {
                 auto stream = *sit;
                 if (evaluateNewStream(new_streams, stream, reserve_step)) {
                     RTP_LLM_LOG_DEBUG("stream [%ld] add to new queue", stream->streamId());
                     if (stream->setRunning()) {
                         new_streams.emplace_back(stream);
                         sit = group.streams.erase(sit);
-                        if (group.expected_size > 0) group.expected_size--;
+                        if (group.expected_size > 0)
+                            group.expected_size--;
                     } else {
                         RTP_LLM_LOG_WARNING("stream [%ld] set running failed", stream->streamId());
                         stream->releaseResource();
                         sit = group.streams.erase(sit);
-                        if (group.expected_size > 0) group.expected_size--;
+                        if (group.expected_size > 0)
+                            group.expected_size--;
                     }
                 } else if (running_streams_.empty() && new_streams.empty() && remote_running_streams_.empty()) {
-                     RTP_LLM_LOG_WARNING("stream [%ld] can not add to new queue", stream->streamId());
-                     if (stream->inputLength() > cache_manager_->maxAvailableTokensNum()) {
+                    RTP_LLM_LOG_WARNING("stream [%ld] can not add to new queue", stream->streamId());
+                    if (stream->inputLength() > cache_manager_->maxAvailableTokensNum()) {
                         stream->stopAndRelease(ErrorCode::EXCEEDS_KV_CACHE_MAX_LEN,
                                                "input len " + std::to_string(stream->inputLength())
                                                    + " is greater than kv cache max available tokens num "
                                                    + std::to_string(cache_manager_->maxAvailableTokensNum()));
                     } else if ((size_t)stream->inputLength() * stream->currentBatchSize() > max_batch_tokens_size_) {
-                        auto error_info =
-                            autil::StringUtil::formatString("input len [%d] * batch size [%d] > max_batch_tokens_size [%d]",
-                                                            stream->inputLength(),
-                                                            stream->currentBatchSize(),
-                                                            max_batch_tokens_size_);
+                        auto error_info = autil::StringUtil::formatString(
+                            "input len [%d] * batch size [%d] > max_batch_tokens_size [%d]",
+                            stream->inputLength(),
+                            stream->currentBatchSize(),
+                            max_batch_tokens_size_);
                         stream->stopAndRelease(ErrorCode::MALLOC_FAILED, error_info);
                     } else {
                         stream->stopAndRelease(ErrorCode::MALLOC_FAILED, "LACK MEM");
                     }
                     sit = group.streams.erase(sit);
-                    if (group.expected_size > 0) group.expected_size--;
+                    if (group.expected_size > 0)
+                        group.expected_size--;
                 } else {
                     return new_streams;
                 }
