516b981a5719279ce1183a55e31dfb554600e129
JackTan25 <tanboyu.tby@alibaba-inc.com>
2026-02-11 13:22:17 +0800

    fix: split BaseRotaryEmbeddingOp from common

--- rtp_llm/models_py/modules/factory/attention/__init__.py
+++ rtp_llm/models_py/modules/factory/attention/__init__.py
@@ -1,5 +1,7 @@
 """Attention factory module - handles different attention implementations."""
 
+import logging
+
 # Import the factory after lists are defined to avoid circular imports
 from rtp_llm.models_py.modules.factory.attention.attn_factory import AttnImplFactory
 from rtp_llm.models_py.modules.factory.attention.fmha_impl_base import FMHAImplBase
@@ -21,71 +23,74 @@ from rtp_llm.models_py.modules.factory.attention.attn_factory import (
 )
 
 device_type = get_device().get_device_type()
-if device_type == DeviceType.ROCm:
-    # Import to register ROCm FMHA implementations
-    from rtp_llm.models_py.modules.factory.attention.rocm_impl.aiter import (
-        AiterDecodeImplAsm,
-        AiterDecodeImplNonAsm,
-        AiterPrefillImplAsm,
-        AiterPrefillImplNonAsm,
-    )
+try:
+    if device_type == DeviceType.ROCm:
+        # Import to register ROCm FMHA implementations
+        from rtp_llm.models_py.modules.factory.attention.rocm_impl.aiter import (
+            AiterDecodeImplAsm,
+            AiterDecodeImplNonAsm,
+            AiterPrefillImplAsm,
+            AiterPrefillImplNonAsm,
+        )
 
-    PREFILL_MHA_IMPS.append(AiterPrefillImplAsm)
-    PREFILL_MHA_IMPS.append(AiterPrefillImplNonAsm)
-    DECODE_MHA_IMPS.append(AiterDecodeImplAsm)
-    DECODE_MHA_IMPS.append(AiterDecodeImplNonAsm)
-else:
-    # currently append early means impl has higher priority
-    if device_type == DeviceType.Cuda:
+        PREFILL_MHA_IMPS.append(AiterPrefillImplAsm)
+        PREFILL_MHA_IMPS.append(AiterPrefillImplNonAsm)
+        DECODE_MHA_IMPS.append(AiterDecodeImplAsm)
+        DECODE_MHA_IMPS.append(AiterDecodeImplNonAsm)
+    else:
+        # currently append early means impl has higher priority
+        if device_type == DeviceType.Cuda:
+            from rtp_llm.models_py.modules.factory.attention.cuda_impl.py_flashinfer_mha import (
+                PyFlashinferDecodeImpl,
+                PyFlashinferPagedPrefillImpl,
+                PyFlashinferPrefillImpl,
+            )
+            from rtp_llm.models_py.modules.factory.attention.cuda_impl.trt import (
+                TRTMHAImpl,
+                TRTPagedMHAImpl,
+            )
+            from rtp_llm.models_py.modules.factory.attention.cuda_impl.trtllm_gen import (
+                FlashInferTRTLLMDecodeImpl,
+                FlashInferTRTLLMPrefillImpl,
+                FlashInferTRTLLMSpecDecodeImpl,
+            )
+            from rtp_llm.models_py.modules.factory.attention.cuda_impl.xqa import (
+                get_xqa_impl,
+            )
+
+            PREFILL_MHA_IMPS.extend(
+                [
+                    TRTMHAImpl,
+                    PyFlashinferPrefillImpl,
+                    PyFlashinferPagedPrefillImpl,
+                    FlashInferTRTLLMSpecDecodeImpl,
+                    FlashInferTRTLLMPrefillImpl,
+                    TRTPagedMHAImpl,
+                ]
+            )
+            DECODE_MHA_IMPS.extend([FlashInferTRTLLMDecodeImpl])
+            DECODE_MHA_IMPS.append(get_xqa_impl())
+
+            from rtp_llm.models_py.modules.factory.attention.cuda_mla_impl.flashinfer_mla_wrapper import (
+                MlaFlashInferDecodeImpl,
+                MlaFlashInferPrefillImpl,
+            )
+
+            DECODE_MLA_IMPS.append(MlaFlashInferDecodeImpl)
+            PREFILL_MLA_IMPS.append(MlaFlashInferPrefillImpl)
+            from rtp_llm.models_py.modules.factory.attention.cuda_impl.flash_infer import (
+                FlashInferDecodeImpl,
+                FlashInferPrefillImpl,
+            )
+
+            PREFILL_MHA_IMPS.append(FlashInferPrefillImpl)
+            DECODE_MHA_IMPS.append(FlashInferDecodeImpl)
         from rtp_llm.models_py.modules.factory.attention.cuda_impl.py_flashinfer_mha import (
             PyFlashinferDecodeImpl,
-            PyFlashinferPagedPrefillImpl,
             PyFlashinferPrefillImpl,
         )
-        from rtp_llm.models_py.modules.factory.attention.cuda_impl.trt import (
-            TRTMHAImpl,
-            TRTPagedMHAImpl,
-        )
-        from rtp_llm.models_py.modules.factory.attention.cuda_impl.trtllm_gen import (
-            FlashInferTRTLLMDecodeImpl,
-            FlashInferTRTLLMPrefillImpl,
-            FlashInferTRTLLMSpecDecodeImpl,
-        )
-        from rtp_llm.models_py.modules.factory.attention.cuda_impl.xqa import (
-            get_xqa_impl,
-        )
 
-        PREFILL_MHA_IMPS.extend(
-            [
-                TRTMHAImpl,
-                PyFlashinferPrefillImpl,
-                PyFlashinferPagedPrefillImpl,
-                FlashInferTRTLLMSpecDecodeImpl,
-                FlashInferTRTLLMPrefillImpl,
-                TRTPagedMHAImpl,
-            ]
-        )
-        DECODE_MHA_IMPS.extend([FlashInferTRTLLMDecodeImpl])
-        DECODE_MHA_IMPS.append(get_xqa_impl())
-
-        from rtp_llm.models_py.modules.factory.attention.cuda_mla_impl.flashinfer_mla_wrapper import (
-            MlaFlashInferDecodeImpl,
-            MlaFlashInferPrefillImpl,
-        )
-
-        DECODE_MLA_IMPS.append(MlaFlashInferDecodeImpl)
-        PREFILL_MLA_IMPS.append(MlaFlashInferPrefillImpl)
-        from rtp_llm.models_py.modules.factory.attention.cuda_impl.flash_infer import (
-            FlashInferDecodeImpl,
-            FlashInferPrefillImpl,
-        )
-
-        PREFILL_MHA_IMPS.append(FlashInferPrefillImpl)
-        DECODE_MHA_IMPS.append(FlashInferDecodeImpl)
-    from rtp_llm.models_py.modules.factory.attention.cuda_impl.py_flashinfer_mha import (
-        PyFlashinferDecodeImpl,
-        PyFlashinferPrefillImpl,
-    )
-
-    PREFILL_MHA_IMPS.append(PyFlashinferPrefillImpl)
-    DECODE_MHA_IMPS.append(PyFlashinferDecodeImpl)
+        PREFILL_MHA_IMPS.append(PyFlashinferPrefillImpl)
+        DECODE_MHA_IMPS.append(PyFlashinferDecodeImpl)
+except Exception as e:
+    logging.warning(f"Failed to import Attention implementation: {e}")
