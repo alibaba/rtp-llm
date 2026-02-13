8a4001257ae547ec74c634e01994299a04cc8c14
guojing <gj01883648@alibaba-inc.com>
2026-02-13 17:54:26 +0800

    feat: 更新脚本参数并添加仓库URL字段

--- .github/scripts/trigger-CI.sh
+++ .github/scripts/trigger-CI.sh
@@ -3,7 +3,7 @@
 set -x
 # Check if two arguments are provided
 if [ $# -ne 5 ]; then
-    echo "Usage: $0 <COMMIT_ID> <SECURITY> <GITHUB_SOURCE_REPO> <GITHUB_PR_ID> <GITHUB_RUN_ID>"
+    echo "Usage: $0 <COMMIT_ID> <SECURITY> <GITHUB_SOURCE_REPO> <GITHUB_PR_ID> <CURRENT_GITHUB_RUN_ID>"
     exit 1
 fi
 
@@ -23,7 +23,7 @@ CURRENT_INTERNAL_COMMITID="UNKNOWN"
 CURRENT_GITHUB_RUN_ID=$5
 
 # Call get-branch-info.sh to get CURRENT_INTERNAL_COMMITID
-BRANCH_INFO=$(sh ./get-branch-info.sh "${BRANCH_NAME}")
+BRANCH_INFO=$(sh ./get-branch-info.sh "${BRANCH_NAME}" "${GITHUB_REPOSITORY}" "${COMMIT_ID}")
 if [ $? -eq 0 ]; then
     CURRENT_INTERNAL_COMMITID=$(echo "$BRANCH_INFO" | jq -r '.commit.id')
 fi
@@ -49,6 +49,7 @@ curl -v -H "Content-Type: application/json" \
             \"commitId\": \"${COMMIT_ID}\",
             \"currentInternalCommitId\": \"${CURRENT_INTERNAL_COMMITID}\",
             \"repositoryUrl\": \"${REPO_URL}\",
+            \"prId\": \"${GITHUB_PR_ID}\",
             \"aone\": { \"projectId\": \"${PROJECT_ID}\", \"pipelineId\": \"${PIPELINE_ID}\"},
             \"newBranch\": { \"name\": \"${BRANCH_NAME}\", \"ref\": \"${BRANCH_REF}\", \"head\": \"UNKNOWN\" },
             \"params\": {\"cancel-in-progress\": \"${CANCEL_IN_PROGRESS}\", \"github_commit\":\"${GITHUB_COMMIT_ID}\", \"github_source_repo\": \"${GITHUB_SOURCE_REPO}\",\"github_run_id\": \"${CURRENT_GITHUB_RUN_ID}\",\"aone_branch_name\": \"${BRANCH_NAME}\",\"aone_branch_ref\": \"${BRANCH_REF}\"}
