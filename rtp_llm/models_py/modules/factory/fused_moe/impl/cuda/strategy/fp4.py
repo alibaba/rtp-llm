5be4986a154c27ffccde5dd9eb72d8561977c349
xingjunna.xjn <xingjunna.xjn@alibaba-inc.com>
2026-02-24 13:56:46 +0800

    modify fp4 moe strategy and executor

--- rtp_llm/models_py/modules/factory/fused_moe/impl/cuda/strategy/fp4.py
+++ rtp_llm/models_py/modules/factory/fused_moe/impl/cuda/strategy/fp4.py
@@ -8,47 +8,34 @@ from rtp_llm.models_py.modules.factory.fused_moe.defs.config_adapter import MoEC
 from rtp_llm.models_py.modules.factory.fused_moe.defs.priority_attributes import (
     StrategyAttributes,
 )
+from rtp_llm.models_py.modules.factory.fused_moe.defs.quant_config import (
+    FusedMoEQuantConfig,
+)
 from rtp_llm.models_py.modules.factory.fused_moe.defs.strategy_base import MoeStrategy
-from rtp_llm.config.model_config import ModelConfig
+from rtp_llm.models_py.modules.factory.fused_moe.utils.config_resolver import (
+    MoeConfigResolver,
+)
 
 
 class CudaFp4NoDPStrategy(MoeStrategy):
     """CUDA FP4 PerGroup single GPU strategy"""
 
-    def create_router(self, config: MoEConfigAdapter) -> Any:
-        from rtp_llm.models_py.modules.factory.fused_moe.impl.cuda.routers.deepgeemm_coutinous_router import (
-            PureTpRouter,
-        )
-
-        return PureTpRouter(config, False)
-
-    def create_executor(
-        self, config: MoEConfigAdapter, weights: Dict[str, torch.Tensor]
-    ) -> Any:
-        # maybe use DeepGemmMaskedExecutor with reorder for small token size
-        from rtp_llm.models_py.modules.factory.fused_moe.impl.cuda.executors.trtllm_fp4_executor import (
-            TrtllmFp4Executor,
-        )
-        from rtp_llm.models_py.modules.factory.fused_moe.defs.quant_config import (
-            FusedMoEQuantConfig,
-        )
-        
-        quant_config = FusedMoEQuantConfig(
-            quant_dtype=torch.uint8, block_shape=[16, 16]
-        )
-        return TrtllmFp4Executor(config, weights, quant_config)
-
     def get_attributes(self) -> StrategyAttributes:
         from rtp_llm.models_py.modules.factory.fused_moe.impl.cuda.executors.trtllm_fp4_executor import (
             TrtllmFp4Executor,
         )
-        from rtp_llm.models_py.modules.factory.fused_moe.impl.cuda.routers.deepgeemm_coutinous_router import (
-            PureTpRouter,
+        from rtp_llm.models_py.modules.factory.fused_moe.impl.cuda.routers.pure_tp_router import (
+            PureTpRouterFp4PerGroup,
         )
 
+        quant_config = FusedMoEQuantConfig(
+            quant_dtype=torch.uint8,
+            block_shape=[16, 16],
+        )
         return StrategyAttributes(
-            router_class=PureTpRouter,
+            router_class=PureTpRouterFp4PerGroup,
             executor_class=TrtllmFp4Executor,
+            quant_config=quant_config,
         )
 
 
@@ -57,46 +44,9 @@ class CudaFp4EpLowLatencyStrategy(MoeStrategy):
 
     @classmethod
     def check_conditions(cls, checker: Any, config: MoEConfigAdapter) -> None:
-        from rtp_llm.models_py.modules.factory.fused_moe.utils.config_resolver import (
-            MoeConfigResolver,
-        )
-
         resolver = MoeConfigResolver()
         quant_method = resolver.get_quant_method(config)
         checker.check(quant_method == "modelopt_fp4")
-
-
-    def create_router(self, config: MoEConfigAdapter) -> Any:
-        from rtp_llm.models_py.modules.factory.fused_moe.impl.cuda.routers.deepep_low_latency_router import (
-            DeepEpLowLatencyRouter,
-        )
-
-        return DeepEpLowLatencyRouter(
-            config,
-            use_fp8_dispatch=False,
-            zero_copy=False,
-            async_finish=False,
-            return_recv_hook=False,
-        )
-
-    def create_executor(
-        self, config: MoEConfigAdapter, weights: Dict[str, torch.Tensor]
-    ) -> Any:
-        from rtp_llm.models_py.modules.factory.fused_moe.defs.quant_config import (
-            FusedMoEQuantConfig,
-        )
-        from rtp_llm.models_py.modules.factory.fused_moe.impl.cuda.executors.cutedsl_fp4_executor import (
-            CutedslFp4Executor,
-        )
-
-        quant_config = FusedMoEQuantConfig(
-            quant_dtype=torch.uint8, block_shape=[16, 16]
-        )
-        return CutedslFp4Executor(
-            config,
-            weights,
-            quant_config=quant_config,
-        )
 
     def get_attributes(self) -> StrategyAttributes:
         from rtp_llm.models_py.modules.factory.fused_moe.impl.cuda.executors.cutedsl_fp4_executor import (
@@ -106,46 +56,34 @@ class CudaFp4EpLowLatencyStrategy(MoeStrategy):
             DeepEpLowLatencyRouter,
         )
 
+        quant_config = FusedMoEQuantConfig(
+            quant_dtype=torch.uint8,
+            block_shape=[16, 16],
+        )
         return StrategyAttributes(
             router_class=DeepEpLowLatencyRouter,
             executor_class=CutedslFp4Executor,
+            quant_config=quant_config,
         )
 
 
 class CudaFp4EpNormalStrategy(MoeStrategy):
     """CUDA FP4 PerGroup EP normal mode strategy"""
     
-    def create_router(self, config: MoEConfigAdapter) -> Any:
-        from rtp_llm.models_py.modules.factory.fused_moe.impl.cuda.routers.deepep_normal_router import (
-            DeepepNormalRouter,
-        )
-
-        return DeepepNormalRouter(config, use_fp8=False)
-
-    def create_executor(
-        self, config: MoEConfigAdapter, weights: Dict[str, torch.Tensor]
-    ) -> Any:
-        from rtp_llm.models_py.modules.factory.fused_moe.impl.cuda.executors.trtllm_fp4_executor import (
-            TrtllmFp4Executor,
-        )
-        from rtp_llm.models_py.modules.factory.fused_moe.defs.quant_config import (
-            FusedMoEQuantConfig,
-        )
-        
-        quant_config = FusedMoEQuantConfig(
-            quant_dtype=torch.uint8, block_shape=[16, 16]
-        )
-        return TrtllmFp4Executor(config, weights, quant_config)
-
     def get_attributes(self) -> StrategyAttributes:
         from rtp_llm.models_py.modules.factory.fused_moe.impl.cuda.executors.trtllm_fp4_executor import (
             TrtllmFp4Executor,
         )
         from rtp_llm.models_py.modules.factory.fused_moe.impl.cuda.routers.deepep_normal_router import (
-            DeepepNormalRouter,
+            DeepepNormalRouterFp4PerGroup,
         )
 
+        quant_config = FusedMoEQuantConfig(
+            quant_dtype=torch.uint8,
+            block_shape=[16, 16],
+        )
         return StrategyAttributes(
-            router_class=DeepepNormalRouter,
+            router_class=DeepepNormalRouterFp4PerGroup,
             executor_class=TrtllmFp4Executor,
+            quant_config=quant_config,
         )
