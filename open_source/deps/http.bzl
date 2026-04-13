06e7b1ad39270f6ff0a3938f01a96b370f5ef386
zhoushipei.zsp <zhoushipei.zsp@alibaba-inc.com>
2026-04-13 16:08:57 +0800

    update kvcm server && client

--- open_source/deps/http.bzl
+++ open_source/deps/http.bzl
@@ -134,17 +134,17 @@ def http_deps():
     http_file(
         name = "remote_kv_cache_manager_client_rpm",
         urls = [
-            "https://rtp-opensource.oss-cn-hangzhou.aliyuncs.com/package/kvcm/kv-cache-manager-client-2026_02_28_11_34.rpm",
+            "http://search-ad.oss-cn-hangzhou-zmf-internal.aliyuncs.com/kv_cache_manager%2Fclient%2Fkv-cache-manager-client-2026_04_16_14_54.rpm",
         ],
-        sha256 = "14ed1759d596bb049a3cc2b68b91797b1d8b4d1b13577304e5ab916e5a4fe159",
+        sha256 = "1b292f07a3c226d34ddd89da605e7a90025153016898ba1ac9f83ab34e6e79ec",
     )
 
     http_archive(
         name = "remote_kv_cache_manager_server",
         urls = [
-            "https://rtp-opensource.oss-cn-hangzhou.aliyuncs.com/package/kvcm/kv_cache_manager_server_2026_02_28_11_36.tar.gz",
+            "http://search-ad.oss-cn-hangzhou-zmf-internal.aliyuncs.com/kv_cache_manager%2Fserver%2Fkv_cache_manager_server_2026_04_16_14_56.tar.gz",
         ],
-        sha256 = "757eaec92b45a156ae02bae2000db54d767538c572276269ebc803c1513bb3f2",
+        sha256 = "5a1c30ff610d5074bdcef0cfa0b39d598f6e0edb5773b7ffa8a3963a15deeb0e",
         build_file_content = """
 exports_files(["bin/kv_cache_manager_bin"])
         """,
--- rtp_llm/cpp/cache/connector/remote_connector/GroupPolicy.cc
+++ rtp_llm/cpp/cache/connector/remote_connector/GroupPolicy.cc
@@ -142,13 +142,8 @@ bool DefaultLayerGroupPolicy::getNeedWriteGroups(const std::shared_ptr<KVCacheRe
 bool DefaultLayerGroupPolicy::genBlockBuffers(const std::vector<int32_t>&     group_ids,
                                               const std::vector<int32_t>&     block_ids,
                                               kv_cache_manager::BlockBuffers& block_buffers) const {
-    // yemu : pace_enable_gs is ugly adapter code, delete in the future
-    static bool pace_enable_gs = autil::EnvUtil::getEnv("PACE_ENABLE_GATHERSCATTER", std::string("OFF")) == "ON";
-    static auto push_iov       = [](std::vector<kv_cache_manager::Iov>& iovs, const BlockInfo& block_info) {
+    static auto push_iov = [](std::vector<kv_cache_manager::Iov>& iovs, const BlockInfo& block_info) {
         iovs.push_back({kv_cache_manager::MemoryType::GPU, block_info.addr, block_info.size_bytes, false});
-    };
-    static auto push_iov_raw = [](std::vector<kv_cache_manager::Iov>& iovs, void* addr, size_t sz) {
-        iovs.push_back({kv_cache_manager::MemoryType::GPU, addr, sz, false});
     };
     block_buffers.reserve(block_ids.size());
     for (size_t i = 0; i < block_ids.size(); ++i) {
@@ -156,8 +151,6 @@ bool DefaultLayerGroupPolicy::genBlockBuffers(const std::vector<int32_t>&     gr
         const auto& layer_ids = group_to_layer_ids_.at(group_ids[i]);
         auto&       iovs      = block_buffers.back().iovs;
         iovs.reserve(layer_ids.size() * 2);
-        size_t iov_size = 0;
-
         for (size_t j = 0; j < layer_ids.size(); ++j) {
             // if support scale, block_infos: {kv_info, scale_info}
             const auto& block_infos = allocator_->convertIndexToBuffer(layer_ids[j], block_ids[i]);
@@ -165,40 +158,13 @@ bool DefaultLayerGroupPolicy::genBlockBuffers(const std::vector<int32_t>&     gr
                 RTP_LLM_LOG_WARNING(
                     "convertIndexToBuffer returned empty for layer_id [%d] block_id[%d]", layer_ids[j], block_ids[i]);
             }
-            if (!pace_enable_gs) {
-                for (size_t idx = 0; idx < block_infos.size(); ++idx) {
-                    CHECK_BLOCK_INFO_VALID(
-                        block_infos[idx],
-                        "convertIndexToBuffer failed layer_id [%d] block_id[%d], block_info.addr or block_info.size_bytes is invalid",
-                        j,
-                        i);
-                    push_iov(iovs, block_infos[idx]);
-                }
-            } else {
-                for (size_t idx = 0; idx < block_infos.size(); ++idx) {
-                    CHECK_BLOCK_INFO_VALID(
-                        block_infos[idx],
-                        "convertIndexToBuffer failed layer_id [%d] block_id[%d], block_info.addr or block_info.size_bytes is invalid",
-                        j,
-                        i);
-                    if (iov_size == 0) {
-                        iov_size = block_infos[idx].size_bytes;
-                        push_iov(iovs, block_infos[0]);
-                    } else if (iov_size == block_infos[idx].size_bytes) {
-                        push_iov(iovs, block_infos[idx]);
-                    } else if ((block_infos[idx].size_bytes % iov_size) == 0) {
-                        for (size_t offset = 0; offset < block_infos[idx].size_bytes; offset += iov_size) {
-                            void* addr = static_cast<char*>(block_infos[idx].addr) + offset;
-                            push_iov_raw(iovs, addr, iov_size);
-                        }
-                    } else {
-                        RTP_LLM_LOG_ERROR(
-                            "PACE_ENABLE_GATHERSCATTER must have same iov size now, real [%zu], iov_size [%zu]",
-                            block_infos[idx].size_bytes,
-                            iov_size);
-                        return false;
-                    }
-                }
+            for (size_t idx = 0; idx < block_infos.size(); ++idx) {
+                CHECK_BLOCK_INFO_VALID(
+                    block_infos[idx],
+                    "convertIndexToBuffer failed layer_id [%d] block_id[%d], block_info.addr or block_info.size_bytes is invalid",
+                    j,
+                    i);
+                push_iov(iovs, block_infos[idx]);
             }
         }
     }
