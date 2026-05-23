608db984d887ddccfa9f2f13ec1fc5b1a074097b
luoli.hn <luoli.hn@taobao.com>
2026-05-23 23:34:11 +0800

    fix(xgrammar): track stateful processor commits

--- rtp_llm/cpp/models/logits_processor/BaseLogitsProcessor.h
+++ rtp_llm/cpp/models/logits_processor/BaseLogitsProcessor.h
@@ -12,9 +12,15 @@ public:
     static const float neg_inf;
 
 public:
-    virtual void  process(const SamplerInputs& inputs, size_t start_idx, size_t finish_idx) = 0;
-    virtual void  updateMultiSeqStatus(const std::vector<int>& src_batch_indices)           = 0;
-    virtual void  updateStatus(const torch::Tensor& new_tokens, int32_t num_new_tokens)     = 0;
+    virtual void process(const SamplerInputs& inputs, size_t start_idx, size_t finish_idx) = 0;
+    virtual void updateMultiSeqStatus(const std::vector<int>& src_batch_indices)           = 0;
+    virtual void updateStatus(const torch::Tensor& new_tokens, int32_t num_new_tokens)     = 0;
+    virtual bool isStateful() const {
+        return false;
+    }
+    virtual int64_t acceptedTokenLen() const {
+        return 0;
+    }
     void          memFill(const torch::Tensor& new_tokens_logits, size_t vocab_size, size_t index);
     void          maskLogits(torch::Tensor& new_token_logits, const torch::Tensor& vocab_mask);
     torch::Tensor generateVocabMask(size_t                                  batch_size,
