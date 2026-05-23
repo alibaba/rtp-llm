791eda88d91bfdabd98307b6f317319b2bb88f50
luoli.hn <luoli.hn@taobao.com>
2026-05-23 12:00:54 +0800

    test(logits): cover zero think budget end mask

--- rtp_llm/cpp/models/logits_processor/test/ThinkModeLogitsProcessorTest.cc
+++ rtp_llm/cpp/models/logits_processor/test/ThinkModeLogitsProcessorTest.cc
@@ -413,6 +413,29 @@ TEST_F(SamplerTest, testNoThinkingMasksThinkEndTokenWithoutBeginTokenConfig) {
     EXPECT_EQ(0, sampler_inputs.logits[0][271].item<float>());
 }
 
+TEST_F(SamplerTest, testZeroThinkBudgetMasksThinkEndTokenWithoutBeginTokenConfig) {
+    SamplerDataBuilder builder;
+
+    auto generate_input                                  = std::make_shared<GenerateInput>();
+    generate_input->generate_config                      = std::make_shared<GenerateConfig>();
+    generate_input->generate_config->in_think_mode       = true;
+    generate_input->generate_config->max_thinking_tokens = 0;
+    generate_input->generate_config->end_think_token_ids = {201, 128822, 271};
+    generate_input->input_ids                            = torch::tensor({1, 2, 3}, torch::kInt32);
+
+    auto processor = ThinkModeLogitsProcessor::fromGenerateInput(generate_input, 1);
+    ASSERT_NE(processor, nullptr);
+    EXPECT_EQ(1, processor->size());
+
+    SamplerInputs sampler_inputs = builder.allocate({1, 128900, 8}, {}, {});
+    processor->process(sampler_inputs, 0, 1);
+
+    float neg_inf = -std::numeric_limits<float>::max();
+    EXPECT_EQ(0, sampler_inputs.logits[0][201].item<float>());
+    EXPECT_EQ(neg_inf, sampler_inputs.logits[0][128822].item<float>());
+    EXPECT_EQ(0, sampler_inputs.logits[0][271].item<float>());
+}
+
 TEST_F(SamplerTest, testThinkingBudgetEnforceStartsWhenCloseTokensNeedRemainingBudget) {
     SamplerDataBuilder builder;
 
