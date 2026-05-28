5abd7c19c56325ad6d506bcbba2936c43a2d942b
nwk_bince <ningweikang@huawei.com>
2026-05-28 17:02:32 +0800

    feat: add simplified fusedCopy

--- rtp_llm/models_py/bindings/common/FusedCopyOp.cc
+++ rtp_llm/models_py/bindings/common/FusedCopyOp.cc
@@ -11,6 +11,11 @@
 #include "rtp_llm/models_py/bindings/rocm/cuda_shims.h"
 #include <hip/hip_runtime.h>
 #endif
+#if USING_ASCEND
+#include <acl/acl.h>
+#include <torch_npu/csrc/core/npu/NPUStream.h>
+#include "rtp_llm/models_py/bindings/ascend/ascend_types_hdr.h"
+#endif
 
 namespace rtp_llm {
 
@@ -21,8 +26,14 @@ void fusedCopy(const FusedD2DCopyParams& params) {
 #elif USING_ROCM
     hipStream_t stream = at::hip::getCurrentHIPStream();
     invokeFusedCopy(params, stream);
+#elif USING_ASCEND
+    aclrtStream stream = c10_npu::getCurrentNPUStream().stream();
+    for (int i = 0; i < params.num_copies; ++i) {
+        ASCEND_CHECK(aclrtMemcpyAsync(params.dst[i], params.size[i],
+                                       params.src[i], params.size[i],
+                                       ACL_MEMCPY_HOST_TO_DEVICE, stream));
+    }
 #else
-    // TODO: Ascend - Add Ascend support
     throw std::runtime_error("No supported GPU backend found for fusedCopy");
 #endif
 }
@@ -34,6 +45,19 @@ void fusedStridedCopy(const FusedStridedCopyParams& params) {
 #elif USING_ROCM
     hipStream_t stream = at::hip::getCurrentHIPStream();
     invokeFusedStridedCopy(params, stream);
+#elif USING_ASCEND
+    aclrtStream stream = c10_npu::getCurrentNPUStream().stream();
+    for (int i = 0; i < params.num_copies; ++i) {
+        const char* src_base = static_cast<const char*>(params.src[i]);
+        char*       dst_base = static_cast<char*>(params.dst[i]);
+        for (size_t row = 0; row < params.num_rows[i]; ++row) {
+            const void* src_ptr = src_base + row * params.src_row_stride[i];
+            void*       dst_ptr = dst_base + row * params.dst_row_stride[i];
+            ASCEND_CHECK(aclrtMemcpyAsync(dst_ptr, params.row_bytes[i],
+                                           src_ptr, params.row_bytes[i],
+                                           ACL_MEMCPY_HOST_TO_DEVICE, stream));
+        }
+    }
 #else
     throw std::runtime_error("No supported GPU backend found for fusedStridedCopy");
 #endif
