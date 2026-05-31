b563f24bf18694bb2b1e7b3eec46e40fcb75604f
JackTan25 <jacktby@gmail.com>
2026-05-31 12:21:55 +0800

    fix(dsv4): split SWA KV CP slice transfer

--- rtp_llm/server/server_args/parallel_group_args.py
+++ rtp_llm/server/server_args/parallel_group_args.py
@@ -114,6 +114,14 @@ def init_parallel_group_args(
         help="显式指定 prefill CP size，供 decode 侧 fixed/SWA cache ring sizing 使用。",
     )
     parallel_group.add_argument(
+        "--prefill_cp_size",
+        env_name="PREFILL_CP_SIZE",
+        bind_to=(prefill_cp_config, "prefill_cp_size"),
+        type=int,
+        default=0,
+        help="显式指定 prefill CP size，供 decode 侧 fixed/SWA cache ring sizing 使用；decode 开启 PREFILL_CP 且 prefill_cp_kv_cache_sharded 时必须设置。",
+    )
+    parallel_group.add_argument(
         "--use_ub_comm",
         env_name="USE_UB_COMM",
         bind_to=(parallelism_config, "use_ub_comm"),
