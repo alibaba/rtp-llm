324d521945fb13730138b7e575ac1d779a416df7
xinfei.sxf <xinfei.sxf@alibaba-inc.com>
2026-06-01 20:50:07 +0800

    fix: enable core dump on exception by default

--- rtp_llm/cpp/config/StaticConfig.cc
+++ rtp_llm/cpp/config/StaticConfig.cc
@@ -1,6 +1,6 @@
 #include "rtp_llm/cpp/config/StaticConfig.h"
 
 namespace rtp_llm {
-bool StaticConfig::user_ft_core_dump_on_exception = false;
+bool StaticConfig::user_ft_core_dump_on_exception = true;
 bool StaticConfig::user_disable_pdl               = true;
 }  // namespace rtp_llm
