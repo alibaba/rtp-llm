dede34d14f25bd2c1f8d7f0f4a791a61fd9787c7
zhenyun.yzy <zhenyun.yzy@alibaba-inc.com>
2026-06-03 07:17:37 +0000

    fix: normalize qwen35 tool call arguments
    (cherry picked from commit 01f64c8a7e212bfa86b3494a80851086b5e78ae4)
    (cherry picked from commit 49f51197c8c14fefd1e14a0507a7c5f2fdb85bb9)

--- rtp_llm/test/template_test.py
+++ rtp_llm/test/template_test.py
@@ -6,6 +6,7 @@ from abc import ABC, abstractmethod
 from typing import Any, List
 from unittest import TestCase, main
 
+from jinja2 import Environment
 from transformers import AutoTokenizer
 from typing_extensions import override
 
@@ -15,6 +16,9 @@ from rtp_llm.frontend.tokenizer_factory.tokenizers import (
     QWenTokenizer,
     QWenV2Tokenizer,
 )
+from rtp_llm.frontend.tokenizer_factory.tokenizers.qwen_tokenizer import (
+    _QWEN35_DEFAULT_CHAT_TEMPLATE,
+)
 from rtp_llm.openai.api_datatype import (
     ChatCompletionRequest,
     ChatMessage,
@@ -23,9 +27,54 @@ from rtp_llm.openai.api_datatype import (
     RoleEnum,
 )
 from rtp_llm.openai.renderer_factory import ChatRendererFactory, RendererParams
+from rtp_llm.openai.renderer_factory_register import _renderer_factory
+from rtp_llm.openai.renderers.qwen35_renderer import Qwen35Renderer
 from rtp_llm.openai.renderers.qwen_agent_renderer import QwenAgentRenderer
 from rtp_llm.openai.renderers.qwen_agent_tool_renderer import QwenAgentToolRenderer
 from rtp_llm.openai.renderers.qwen_renderer import QwenRenderer
+
+
+class _Qwen35ItemsTemplateTokenizer:
+    path = ""
+    chat_template = """
+{%- for message in messages -%}
+{%- for tool_call in message.tool_calls or [] -%}
+{%- for key, value in tool_call.function.arguments|items -%}
+{{ tool_call.function.name }}:{{ key }}={{ value }}
+{%- endfor -%}
+{%- endfor -%}
+{%- endfor -%}
+"""
+
+    def apply_chat_template(self, messages, **kwargs):
+        template = Environment().from_string(self.chat_template)
+        return template.render(messages=messages, **kwargs)
+
+    def encode(self, prompt):
+        return list(range(len(prompt)))
+
+    def decode(self, token_ids):
+        return ""
+
+
+class _Qwen35DefaultTemplateTokenizer:
+    path = ""
+    chat_template = _QWEN35_DEFAULT_CHAT_TEMPLATE
+
+    def apply_chat_template(self, messages, **kwargs):
+        env = Environment()
+        env.globals["raise_exception"] = self._raise_exception
+        template = env.from_string(self.chat_template)
+        return template.render(messages=messages, **kwargs)
+
+    def encode(self, prompt):
+        return list(range(len(prompt)))
+
+    def decode(self, token_ids):
+        return ""
+
+    def _raise_exception(self, message):
+        raise ValueError(message)
 
 
 class BaseRendererTestMixin(ABC):
@@ -243,6 +292,75 @@ class TemplateTest(TestCase):
     def __init__(self, *args: Any, **kwargs: Any):
         super().__init__(*args, **kwargs)
         self.test_data_path = os.path.join(os.getcwd(), "rtp_llm/test")
+
+    def test_qwen35_parses_tool_call_arguments_for_items_filter(self):
+        tokenizer = _Qwen35ItemsTemplateTokenizer()
+        renderer = Qwen35Renderer(
+            tokenizer,
+            RendererParams(
+                model_type="qwen35_dense",
+                max_seq_len=1024,
+                eos_token_id=0,
+                stop_word_ids_list=[],
+            ),
+            GenerateEnvConfig(),
+            RenderConfig(),
+        )
+        request = ChatCompletionRequest(
+            messages=[
+                ChatMessage(role=RoleEnum.user, content="What's the weather?"),
+                ChatMessage(
+                    **{
+                        "role": RoleEnum.assistant,
+                        "content": None,
+                        "tool_calls": [
+                            {
+                                "id": "call_1",
+                                "type": "function",
+                                "function": {
+                                    "name": "get_current_temperature",
+                                    "arguments": '{"location": "San Francisco, CA, USA"}',
+                                },
+                            }
+                        ],
+                    }
+                ),
+            ],
+        )
+
+        rendered_prompt = renderer.render_chat(request).rendered_prompt
+
+        assert (
+            rendered_prompt == "get_current_temperature:location=San Francisco, CA, USA"
+        )
+
+    def test_qwen35_moe_mtp_uses_qwen35_renderer(self):
+        assert _renderer_factory["qwen35_moe_mtp"] is Qwen35Renderer
+
+    def test_qwen35_passes_chat_template_kwargs_to_template(self):
+        tokenizer = _Qwen35DefaultTemplateTokenizer()
+        renderer = Qwen35Renderer(
+            tokenizer,
+            RendererParams(
+                model_type="qwen35_dense",
+                max_seq_len=1024,
+                eos_token_id=0,
+                stop_word_ids_list=[],
+            ),
+            GenerateEnvConfig(),
+            RenderConfig(),
+        )
+        request = ChatCompletionRequest(
+            messages=[ChatMessage(role=RoleEnum.user, content="hello")],
+            chat_template_kwargs={"enable_thinking": False},
+        )
+
+        rendered_prompt = renderer.render_chat(request).rendered_prompt
+
+        assert rendered_prompt == (
+            "<|im_start|>user\nhello<|im_end|>\n"
+            "<|im_start|>assistant\n<think>\n\n</think>\n\n"
+        )
 
     def test_qwen_agent(self):
         tokenizer = QWenTokenizer(
