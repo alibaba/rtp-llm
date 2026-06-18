6c44cc879c72815c4362291b4f77d397a3274165
huzetao.hzt <huzetao.hzt@alibaba-inc.com>
2026-06-18 14:11:00 +0800

    chore: remove redundant error handling

--- rtp_llm/cpp/utils/DevicePin.h
+++ rtp_llm/cpp/utils/DevicePin.h
@@ -17,34 +17,17 @@ inline void pinThreadToDeviceOnce(int device_id) {
         return;
     }
 
+    thread_local int pinned_device = -1;
+    if (pinned_device == device_id) {
+        return;
+    }
+
 #if USING_CUDA
-    thread_local int pinned_device = -1;
-    if (pinned_device == device_id) {
-        return;
-    }
-    const auto rc = cudaSetDevice(device_id);
-    if (rc != cudaSuccess) {
-        RTP_LLM_LOG_WARNING("cudaSetDevice(%d) failed: %s", device_id, cudaGetErrorString(rc));
-        return;
-    }
     at::cuda::set_device(device_id);
-    at::cuda::setCurrentCUDAStream(at::cuda::getDefaultCUDAStream(device_id));
-    pinned_device = device_id;
 #elif USING_ROCM
-    thread_local int pinned_device = -1;
-    if (pinned_device == device_id) {
-        return;
-    }
-    const auto rc = hipSetDevice(device_id);
-    if (rc != hipSuccess) {
-        RTP_LLM_LOG_WARNING("hipSetDevice(%d) failed: %s", device_id, hipGetErrorString(rc));
-        return;
-    }
     at::hip::set_device(device_id);
-    pinned_device = device_id;
-#else
-    (void)device_id;
 #endif
+    pinned_device = device_id;
 }
 
 }  // namespace rtp_llm
