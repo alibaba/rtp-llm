619b8dd674821a7c44fa2e670796bcf1330cbab1
zw193905 <zw193905@alibaba-inc.com>
2026-06-26 13:52:48 +0800

    fix - add trip stop token id

--- rtp_llm/server/test/backend_rpc_server_visitor_test.py
+++ rtp_llm/server/test/backend_rpc_server_visitor_test.py
@@ -1,10 +1,13 @@
 import unittest
 from unittest.mock import patch
 
+import torch
+
 from rtp_llm.config.exceptions import ExceptionType, FtRuntimeException
 from rtp_llm.server.backend_rpc_server_visitor import BackendRPCServerVisitor
 from rtp_llm.server.cache_key_routing import route_cache_keys_for_page_rr
 from rtp_llm.server.master_client import FlexlbResponse
+from rtp_llm.utils.base_model_datatypes import GenerateOutput, GenerateOutputs
 
 
 class _FakeTokenIds:
@@ -199,6 +202,60 @@ class BackendRPCServerVisitorRetryTest(unittest.IsolatedAsyncioTestCase):
 
         self.assertEqual(outputs, ["partial-output-from-failed-attempt"])
         self.assertEqual(client.attempts, 1)
+
+
+class BackendRPCServerVisitorFrontendStopIdsTest(unittest.TestCase):
+    def test_set_frontend_stop_word_ids_merges_eos_and_renderer_stops(self):
+        visitor = BackendRPCServerVisitor.__new__(BackendRPCServerVisitor)
+
+        visitor.set_frontend_stop_word_ids(
+            eos_token_id=151329,
+            stop_word_ids_list=[[151336], [151338]],
+        )
+
+        self.assertEqual(
+            visitor.frontend_stop_word_ids_list,
+            [[151329], [151336], [151338]],
+        )
+
+    def test_strip_finished_output_ids_at_stop_word(self):
+        output = GenerateOutput(
+            output_ids=torch.tensor([[10, 151338]], dtype=torch.int32),
+            finished=True,
+        )
+
+        BackendRPCServerVisitor.strip_frontend_stop_word_ids(
+            GenerateOutputs(generate_outputs=[output]),
+            [[151338]],
+            {},
+        )
+
+        self.assertEqual(output.output_ids.tolist(), [[10]])
+
+    def test_strip_streaming_multi_token_stop_across_chunks(self):
+        pending = {}
+        first = GenerateOutput(
+            output_ids=torch.tensor([[7, 8]], dtype=torch.int32),
+            finished=False,
+        )
+        second = GenerateOutput(
+            output_ids=torch.tensor([[9]], dtype=torch.int32),
+            finished=True,
+        )
+
+        BackendRPCServerVisitor.strip_frontend_stop_word_ids(
+            GenerateOutputs(generate_outputs=[first]),
+            [[8, 9]],
+            pending,
+        )
+        BackendRPCServerVisitor.strip_frontend_stop_word_ids(
+            GenerateOutputs(generate_outputs=[second]),
+            [[8, 9]],
+            pending,
+        )
+
+        self.assertEqual(first.output_ids.tolist(), [[7]])
+        self.assertEqual(second.output_ids.tolist(), [[]])
 
 
 if __name__ == "__main__":
