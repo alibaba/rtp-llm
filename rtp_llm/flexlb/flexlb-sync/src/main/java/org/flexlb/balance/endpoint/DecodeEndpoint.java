32f711c89db8405a662e172dbd91d3e1101febdc
luoli.hn <luoli.hn@taobao.com>
2026-07-21 17:23:21 +0800

    refactor(flexlb): remove obsolete metric lease lifecycle

--- rtp_llm/flexlb/flexlb-sync/src/main/java/org/flexlb/balance/endpoint/DecodeEndpoint.java
+++ rtp_llm/flexlb/flexlb-sync/src/main/java/org/flexlb/balance/endpoint/DecodeEndpoint.java
@@ -6,7 +6,6 @@ import org.flexlb.dao.master.WorkerStatus;
 import org.flexlb.dao.master.WorkerStatusResponse;
 import org.flexlb.dao.route.RoleType;
 import org.flexlb.enums.TaskPhase;
-import org.flexlb.metric.MetricLease;
 import org.flexlb.service.monitor.BatchSchedulerReporter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -26,11 +25,7 @@ public class DecodeEndpoint extends WorkerEndpoint {
     private final InflightEvictor<Long, RequestInflight> requestEvictor;
 
     public DecodeEndpoint(WorkerStatus status) {
-        this(status, MetricLease.noop());
-    }
-
-    DecodeEndpoint(WorkerStatus status, MetricLease metricLease) {
-        super(status, metricLease);
+        super(status);
         this.requestEvictor = new InflightEvictor<>(inflightRequests,
                 req -> inflightKvReservedTotal.addAndGet(-req.kvTokens()));
     }
