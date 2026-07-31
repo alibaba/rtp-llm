1bde7f33506b85c22715b8ffba3eb364e68a4b3c
xinfei.sxf <xinfei.sxf@alibaba-inc.com>
2026-07-31 11:45:43 +0800

    refactor(cache): centralize KV cache spec type

--- rtp_llm/cpp/cache/KVCacheSpecType.h
+++ rtp_llm/cpp/cache/KVCacheSpecType.h
@@ -3,9 +3,11 @@
 namespace rtp_llm {
 
 enum KVCacheSpecType {
-    MultiHeadAttention,
-    MultiHeadLatentAttention,
-    LinearAttention,
+    MultiHeadAttention,        // MHAKVCacheSpec: standard multi-head attention KV cache
+    MultiHeadLatentAttention,  // MLAKVCacheSpec: MLA compressed latent KV cache
+    LinearAttention,           // LinearKVCacheSpec: linear / SSM attention state cache
+    OpaqueKV,                  // Byte-addressed opaque paged KV pool
+    OpaqueState,               // Fixed-allocation opaque state cache
 };
 
 inline const char* KVCacheSpecTypeToString(KVCacheSpecType t) {
@@ -16,6 +18,10 @@ inline const char* KVCacheSpecTypeToString(KVCacheSpecType t) {
             return "MultiHeadLatentAttention";
         case KVCacheSpecType::LinearAttention:
             return "LinearAttention";
+        case KVCacheSpecType::OpaqueKV:
+            return "OpaqueKV";
+        case KVCacheSpecType::OpaqueState:
+            return "OpaqueState";
         default:
             return "Unknown";
     }
