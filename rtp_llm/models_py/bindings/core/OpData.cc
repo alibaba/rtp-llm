45128eed52e2b6213a602d1d61fc2ab57e13a41c
baowending.bwd <baowending.bwd@alibaba-inc.com>
2026-08-08 22:25:54 +0800

    chore(model): include hidden states in input diagnostics

--- rtp_llm/models_py/bindings/core/OpData.cc
+++ rtp_llm/models_py/bindings/core/OpData.cc
@@ -51,6 +51,9 @@ std::string GptModelInputs::debugString(bool force) const {
     if (combo_position_ids.defined()) {
         debug_string << ", combo_position_ids: " << tb(combo_position_ids);
     }
+    if (last_hidden_states.defined()) {
+        debug_string << ", last_hidden_states: " << tb(last_hidden_states);
+    }
     if (kv_cache_kernel_block_id.defined()) {
         debug_string << ", kv_cache_kernel_block_id: " << tb(kv_cache_kernel_block_id);
     }
