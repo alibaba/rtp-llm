c3f8bae0fdf6b38fd2379af04cfa3cbad26bd2fb
tanhongmin.thm <tanhongmin.thm@alibaba-inc.com>
2026-08-14 10:47:07 +0800

    feat: import vLLM FA4 CuTeDSL

---
+++ 3rdparty/vllm_flash_attention/vllm_flash_attention/__init__.py
@@ -0,0 +1,4 @@
+"""Runtime namespace for vendored vLLM FlashAttention CuTeDSL."""
+
+UPSTREAM_COMMIT = "ed4b7342bc8f0489dd9b649d5288867e35fc6a32"
+UPSTREAM_REPOSITORY = "https://github.com/vllm-project/flash-attention"
