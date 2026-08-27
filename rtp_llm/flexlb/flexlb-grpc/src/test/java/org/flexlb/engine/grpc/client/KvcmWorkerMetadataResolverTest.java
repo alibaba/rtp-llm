9eb57b17328b88a6ccb77f1e2e0dd3f7b32dcf5b
saichen.sm <saichen.sm@alibaba-inc.com>
2026-08-27 14:27:14 +0800

    fix(flexlb): restore KVCM worker metadata resolution

--- rtp_llm/flexlb/flexlb-grpc/src/test/java/org/flexlb/engine/grpc/client/KvcmWorkerMetadataResolverTest.java
+++ rtp_llm/flexlb/flexlb-grpc/src/test/java/org/flexlb/engine/grpc/client/KvcmWorkerMetadataResolverTest.java
@@ -1,48 +1,111 @@
 package org.flexlb.engine.grpc.client;
 
 import org.flexlb.config.CacheMatchConfiguration;
+import org.flexlb.config.ModelMetaConfig;
+import org.flexlb.dao.master.WorkerStatus;
 import org.flexlb.dao.master.WorkerStatusProvider;
+import org.flexlb.dao.route.Endpoint;
+import org.flexlb.dao.route.GroupRoleEndPoint;
 import org.flexlb.dao.route.KvcmConfig;
 import org.flexlb.dao.route.RoleType;
+import org.flexlb.dao.route.ServiceRoute;
+import org.flexlb.enums.KvCacheGroupMode;
 import org.flexlb.kvcm.grpc.QueryType;
 import org.junit.jupiter.api.Test;
 
+import java.util.Collection;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 class KvcmWorkerMetadataResolverTest {
 
     @Test
-    void resolvesConfiguredNamespaceAndDefaultQueryType() {
-        CacheMatchConfiguration configuration = mock(CacheMatchConfiguration.class);
-        KvcmConfig config = new KvcmConfig();
-        config.setNamespace("deployment-a");
-        when(configuration.isKvcmEnabled()).thenReturn(true);
-        when(configuration.getKvcmConfig()).thenReturn(config);
-
+    void resolvesNamespaceAndQueryTypeFromOneWorkerStatusRefresh() {
+        WorkerStatus workerStatus = workerStatus(
+                "prefill-deployment", KvCacheGroupMode.WITH_MAMBA);
         KvcmWorkerMetadataResolver resolver = new KvcmWorkerMetadataResolver(
-                configuration, mock(WorkerStatusProvider.class));
+                configuration(null), provider(List.of(workerStatus)));
 
-        assertTrue(resolver.usesConfiguredNamespace());
-        assertEquals(
-                "deployment-a_2192",
+        resolver.refreshNamespacesAndQueryTypes();
+
+        assertEquals("prefill-deployment_2192",
                 resolver.resolveNamespace(RoleType.PREFILL, "default", 2192));
-        assertEquals(
-                QueryType.QT_PREFIX_MATCH,
+        assertEquals(QueryType.QT_PREFIX_MATCH_WITH_MAMBA,
+                resolver.resolveQueryType(RoleType.PREFILL, "default"));
+        assertEquals(QueryType.QT_PREFIX_MATCH_WITH_MAMBA,
+                resolver.resolveQueryType(RoleType.PREFILL, null));
+    }
+
+    @Test
+    void configuredNamespaceTakesPriorityWhileQueryTypeStillUsesWorkerStatus() {
+        WorkerStatus workerStatus = workerStatus(
+                "ignored-deployment", KvCacheGroupMode.FULL_ATTENTION_ONLY);
+        KvcmWorkerMetadataResolver resolver = new KvcmWorkerMetadataResolver(
+                configuration("configured-namespace"), provider(List.of(workerStatus)));
+
+        resolver.refreshNamespacesAndQueryTypes();
+
+        assertEquals("configured-namespace_1024",
+                resolver.resolveNamespace(RoleType.PREFILL, "default", 1024));
+        assertEquals(QueryType.QT_PREFIX_MATCH,
                 resolver.resolveQueryType(RoleType.PREFILL, "default"));
     }
 
     @Test
-    void returnsNoNamespaceWhenKvcmIsDisabled() {
-        CacheMatchConfiguration configuration = mock(CacheMatchConfiguration.class);
-        when(configuration.isKvcmEnabled()).thenReturn(false);
-
+    void unspecifiedModeDoesNotOverwriteLastTrustedQueryType() {
+        WorkerStatus workerStatus = workerStatus(
+                "prefill-deployment", KvCacheGroupMode.WITH_MAMBA);
         KvcmWorkerMetadataResolver resolver = new KvcmWorkerMetadataResolver(
-                configuration, mock(WorkerStatusProvider.class));
+                configuration(null), provider(List.of(workerStatus)));
+        resolver.refreshNamespacesAndQueryTypes();
 
-        assertNull(resolver.resolveNamespace(RoleType.PREFILL, "default", 2192));
+        workerStatus.setKvCacheGroupMode(KvCacheGroupMode.UNSPECIFIED);
+        resolver.refreshNamespacesAndQueryTypes();
+
+        assertEquals(QueryType.QT_PREFIX_MATCH_WITH_MAMBA,
+                resolver.resolveQueryType(RoleType.PREFILL, "default"));
+    }
+
+    private WorkerStatus workerStatus(String deploymentName, KvCacheGroupMode mode) {
+        WorkerStatus workerStatus = new WorkerStatus();
+        workerStatus.setDeploymentName(deploymentName);
+        workerStatus.setKvCacheGroupMode(mode);
+        return workerStatus;
+    }
+
+    private WorkerStatusProvider provider(Collection<WorkerStatus> workerStatuses) {
+        return new WorkerStatusProvider() {
+            @Override
+            public Collection<WorkerStatus> getWorkerStatuses(RoleType roleType, String group) {
+                return workerStatuses;
+            }
+        };
+    }
+
+    private ModelMetaConfig modelMetaConfig(String namespace) {
+        Endpoint endpoint = new Endpoint();
+        endpoint.setAddress("v-prefill");
+
+        GroupRoleEndPoint group = new GroupRoleEndPoint();
+        group.setGroup("default");
+        group.setPrefillEndpoint(endpoint);
+
+        KvcmConfig kvcm = new KvcmConfig();
+        kvcm.setEnabled(true);
+        kvcm.setNamespace(namespace);
+
+        ServiceRoute route = new ServiceRoute();
+        route.setServiceId("test-service");
+        route.setKvcm(kvcm);
+        route.setRoleEndpoints(List.of(group));
+
+        ModelMetaConfig modelMetaConfig = new ModelMetaConfig();
+        modelMetaConfig.putServiceRoute(route.getServiceId(), route);
+        return modelMetaConfig;
+    }
+
+    private CacheMatchConfiguration configuration(String namespace) {
+        return new CacheMatchConfiguration(modelMetaConfig(namespace));
     }
 }
