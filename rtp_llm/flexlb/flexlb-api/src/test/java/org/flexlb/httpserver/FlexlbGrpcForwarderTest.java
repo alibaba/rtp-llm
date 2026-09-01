572255824d693e22efbb55481d3f1fc03bd86ebc
zhangpeiwen <zhangpeiwen.zhangp@alibaba-inc.com>
2026-09-01 17:45:40 +0800

    test(flexlb): keep explicit Mockito import

--- rtp_llm/flexlb/flexlb-api/src/test/java/org/flexlb/httpserver/FlexlbGrpcForwarderTest.java
+++ rtp_llm/flexlb/flexlb-api/src/test/java/org/flexlb/httpserver/FlexlbGrpcForwarderTest.java
@@ -32,6 +32,7 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.when;
 
 class FlexlbGrpcForwarderTest {
