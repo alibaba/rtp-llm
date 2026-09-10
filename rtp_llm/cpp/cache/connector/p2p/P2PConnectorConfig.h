7cea606587f7c6e01ba44abb5ca93f0d08c98e2d
hengcang.wyd <hengcang.wyd@alibaba-inc.com>
2026-09-10 17:51:12 +0800

    fix(loader): close remaining routing and quantization gaps

--- rtp_llm/cpp/cache/connector/p2p/P2PConnectorConfig.h
+++ rtp_llm/cpp/cache/connector/p2p/P2PConnectorConfig.h
@@ -4,7 +4,6 @@
 #include "rtp_llm/cpp/config/ConfigModules.h"
 #include <cstdint>
 #include <string>
-#include <utility>
 #include <vector>
 
 namespace rtp_llm {
@@ -79,9 +78,8 @@ struct P2PConnectorWorkerConfig {
 };
 
 struct P2PConnectorConfig {
-    RoleType                 role_type = RoleType::PDFUSION;
-    int                      tp_rank   = 0;
-    std::vector<std::string> cache_group_tags;
+    RoleType role_type = RoleType::PDFUSION;
+    int      tp_rank   = 0;
 
     P2PConnectorSchedulerConfig scheduler_config;
     P2PConnectorWorkerConfig    worker_config;
@@ -90,12 +88,10 @@ struct P2PConnectorConfig {
                                      const CacheStoreConfig&  cache_store_config,
                                      const ParallelismConfig& parallelism_config,
                                      const PDSepConfig&       pd_sep_config,
-                                     uint32_t                 layer_all_num,
-                                     std::vector<std::string> cache_group_tags = {}) {
+                                     uint32_t                 layer_all_num) {
         P2PConnectorConfig config;
-        config.role_type        = pd_sep_config.role_type;
-        config.tp_rank          = parallelism_config.tp_rank;
-        config.cache_group_tags = std::move(cache_group_tags);
+        config.role_type = pd_sep_config.role_type;
+        config.tp_rank   = parallelism_config.tp_rank;
         config.scheduler_config =
             P2PConnectorSchedulerConfig::create(runtime_config, cache_store_config, pd_sep_config);
         config.worker_config =
