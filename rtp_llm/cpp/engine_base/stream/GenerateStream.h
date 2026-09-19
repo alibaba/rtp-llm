448d438896e9a73abf4b7025d89783d62938cd0f
zw193905 <zw193905@alibaba-inc.com>
2026-09-19 15:56:26 +0800

    fix: retain stream cache resources until async completion

--- rtp_llm/cpp/engine_base/stream/GenerateStream.h
+++ rtp_llm/cpp/engine_base/stream/GenerateStream.h
@@ -606,7 +606,7 @@ public:
     void decPendingAsyncBookkeepingAndMaybeRelease();
     bool hasPendingAsyncBookkeeping() const;
     void waitPendingAsyncBookkeeping();
-    void markDeferredRelease();
+    bool markDeferredRelease();
     bool isDeferredReleasePending() const;
 
     // Per-stream CUDA state used to prepare the next MTP decode step while host
